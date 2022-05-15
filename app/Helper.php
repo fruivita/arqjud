b54673a8efa6ee295383d8d072c9e8162cdfcd46
FCNO <fcno.dev@gmail.com>
2022-05-15 19:58:25 -0300

    feat: add helpers

---
+++ app/Helper.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+if (! function_exists('App\maxSafeInteger')) {
+    /**
+     * The maximum integer acceptable by JavaScript. Especially useful for
+     * applications that use Livewire.
+     *
+     * @return int
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
+     * @see https://github.com/livewire/livewire/discussions/4788
+     */
+    function maxSafeInteger()
+    {
+        return pow(2, 53) - 1;
+    }
+}
+
+if (! function_exists('App\stringToArrayAssoc')) {
+    /**
+     * Splits a string based on the given delimiter and returns it as an
+     * associative array using the braces for each value extracted from the
+     * string.
+     *
+     * The extracted values must be numerically compatible with the number of
+     * keys informed, otherwise it will return null.
+     * It will also return null if any of the parameters is a false value to
+     * php.
+     *
+     * @param string[] $keys      keys that will be used to index the return
+     *                            array
+     * @param string   $str       string to be exploded
+     * @param string   $delimiter delimiter to explode the string
+     *
+     * @return array<string, string>|null
+     *
+     * @see https://www.php.net/manual/en/language.types.boolean.php
+     */
+    function stringToArrayAssoc(array $keys, string $delimiter, string $str)
+    {
+        if (! $keys || ! $delimiter || ! $str) {
+            return null;
+        }
+
+        try {
+            return
+                array_combine(
+                    $keys,
+                    explode($delimiter, $str)
+                );
+        } catch (\Throwable $exception) {
+            return null;
+        }
+    }
+}
---
+++ tests/Unit/App/HelperTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use function App\maxSafeInteger;
+use function App\reportMaxDate;
+use function App\reportMaxYear;
+use function App\reportMinDate;
+use function App\reportMinYear;
+use function App\stringToArrayAssoc;
+
+// Invalid
+test('stringToArrayAssoc returns null if invalid values are given', function ($keys, $delimiter, $string) {
+    expect(stringToArrayAssoc($keys, $delimiter, $string))->toBeNull();
+})->with([
+    [
+        ['name', 'age', 'nationality', 'excess_key'], // qty of keys incompatible with string
+        ',',
+        'foo,18,bar',
+    ],
+    [
+        [], // unspecified keys (empty array)
+        ',',
+        'foo,18,bar',
+    ],
+    [
+        ['name', 'age', 'nationality'],
+        ',',
+        '', // unreported string (false boolean)
+    ],
+    [
+        ['name', 'age', 'nationality'],
+        '', // delimiter not informed (false boolean)
+        'foo,18,bar',
+    ],
+]);
+
+// Happy path
+test('maxSafeInteger returns the value of the largest safe integer, i.e. not subject to truncation, for javascript work', function () {
+    expect(maxSafeInteger())->toBe(9007199254740991);
+});
+
+test('stringToArrayAssoc explodes the string based on the delimiter and returns an associative array', function () {
+    $keys = ['name', 'age', 'nationality'];
+    $string = 'foo,18,bar';
+    $delimiter = ',';
+    $expected = [
+        'name' => 'foo',
+        'age' => '18',
+        'nationality' => 'bar',
+    ];
+
+    expect(stringToArrayAssoc($keys, $delimiter, $string))->toMatchArray($expected);
+});
