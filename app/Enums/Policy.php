60b5a505c3dd157ff3c5e5312c5902381591816d
FCNO <fcno.dev@gmail.com>
2022-05-15 17:08:44 -0300

    feat(core): add policy enum

---
+++ app/Enums/Policy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Enums;
+
+/*
+ * Types of actions/methods handled by a policy.
+ *
+ * @see https://www.php.net/manual/en/language.enumerations.php
+ * @see https://laravel.com/docs/authorization
+ */
+enum Policy: string
+{
+    case ViewAny = 'view-any';
+    case View = 'view';
+    case Create = 'create';
+    case Update = 'update';
+    case Restore = 'restore';
+    case Delete = 'delete';
+    case ForceDelete = 'force-delete';
+    case DelegationViewAny = 'delegation-view-any';
+    case DelegationCreate = 'delegation-create';
+    case DelegationDelete = 'delegation-delete';
+    case ImportationCreate = 'importation-create';
+    case LogViewAny = 'log-view-any';
+    case LogDownload = 'log-download';
+    case LogDelete = 'log-delete';
+    case SimulationCreate = 'simulation-create';
+    case SimulationDelete = 'simulation-delete';
+}
---
+++ tests/Feature/Enums/PolicyTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Enums\Policy;
+
+// Happy path
+test('Policy enum correctly defined', function () {
+    expect(Policy::ViewAny->value)->toBe('view-any')
+    ->and(Policy::View->value)->toBe('view')
+    ->and(Policy::Create->value)->toBe('create')
+    ->and(Policy::Update->value)->toBe('update')
+    ->and(Policy::Restore->value)->toBe('restore')
+    ->and(Policy::Delete->value)->toBe('delete')
+    ->and(Policy::ForceDelete->value)->toBe('force-delete')
+    ->and(Policy::DelegationViewAny->value)->toBe('delegation-view-any')
+    ->and(Policy::DelegationCreate->value)->toBe('delegation-create')
+    ->and(Policy::DelegationDelete->value)->toBe('delegation-delete')
+    ->and(Policy::ImportationCreate->value)->toBe('importation-create')
+    ->and(Policy::LogViewAny->value)->toBe('log-view-any')
+    ->and(Policy::LogDelete->value)->toBe('log-delete')
+    ->and(Policy::LogDownload->value)->toBe('log-download')
+    ->and(Policy::SimulationCreate->value)->toBe('simulation-create')
+    ->and(Policy::SimulationDelete->value)->toBe('simulation-delete');
+});
