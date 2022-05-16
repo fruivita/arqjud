35b8fb443c7d6d142fe03d29d742d5f08399836f
FCNO <fcno.dev@gmail.com>
2022-05-16 03:20:43 -0300

    test: test if corporate file is readable

---
+++ tests/Feature/Importer/DiskPermissionTest.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+test('can read the corporate file', function () {
+    $full_path = config('company.corporate_file');
+
+    expect((new \SplFileInfo($full_path))->isReadable())->toBeTrue();
+})->group('integration');
