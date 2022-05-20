c5f28dcc5646300bacb05a26008dce6352bc737e
FCNO <fcno.dev@gmail.com>
2022-05-20 00:16:06 -0300

    refactor(view): menu icon/text align using flex

--- resources/views/components/menu/logout.blade.php
+++ resources/views/components/menu/logout.blade.php
@@ -18,13 +18,13 @@
 
 
         <button
-            class="block border-primary-500 outline-none pl-3 space-x-3 focus:border-l-4 hover:border-l-4"
+            class="flex items-center border-primary-500 outline-none pl-3 space-x-3 w-full focus:border-l-4 hover:border-l-4"
             formaction="{{ route('logout') }}"
             formmethod="POST"
             title="{{ __('Exit the application') }}"
             type="submit">
 
-            <x-icon name="door-open" class="inline"/>
+            <x-icon name="door-open"/>
 
 
             <span>{{ __('Logout') }}</span>
