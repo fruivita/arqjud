9d85a5daec580d939e4a298c5a84e7caf9b24a0a
FCNO <fcno.dev@gmail.com>
2022-06-05 02:07:31 -0300

    feat(view): add actions to show site view

--- resources/views/livewire/archiving/register/site/show.blade.php
+++ resources/views/livewire/archiving/register/site/show.blade.php
@@ -63,51 +63,6 @@
                 :value="$site->description"/>
 
 
-            <div class="overflow-x-auto">
-
-                <x-perpage
-                    wire:key="per-page"
-                    wire:model="per_page"
-                    class="mb-3"
-                    :error="$errors->first('per_page')"/>
-
-
-                <x-table wire:key="table-buildings" wire:loading.delay.class="opacity-25">
-
-                    <x-slot name="head">
-
-                        <x-table.heading>{{ __('Buildings') }}</x-table.heading>
-
-                    </x-slot>
-
-
-                    <x-slot name="body">
-
-                        @forelse ( $buildings ?? [] as $building )
-
-                            <x-table.row>
-
-                                <x-table.cell>{{ $building->name }}</x-table.cell>
-
-                            </x-table.row>
-
-                        @empty
-
-                            <x-table.row>
-
-                                <x-table.cell colspan="1">{{ __('No record found') }}</x-table.cell>
-
-                            </x-table.row>
-
-                        @endforelse
-
-                    </x-slot>
-
-                </x-table>
-
-            </div>
-
-
             <x-button-group>
 
                 @can(\App\Enums\Policy::Update->value, \App\Models\Site::class)
@@ -130,6 +85,92 @@
                     :title="__('Show all records')"/>
 
             </x-button-group>
+
+
+            <div class="overflow-x-auto">
+
+                <x-perpage
+                    wire:key="per-page"
+                    wire:model="per_page"
+                    class="mb-3"
+                    :error="$errors->first('per_page')"/>
+
+
+                <x-table wire:key="table-buildings" wire:loading.delay.class="opacity-25">
+
+                    <x-slot name="head">
+
+                        <x-table.heading>{{ __('Buildings') }}</x-table.heading>
+
+
+                        <x-table.heading>{{ __('Floors') }}</x-table.heading>
+
+
+                        <x-table.heading class="w-10">{{ __('Actions') }}</x-table.heading>
+
+                    </x-slot>
+
+
+                    <x-slot name="body">
+
+                        @forelse ( $buildings ?? [] as $building )
+
+                            <x-table.row>
+
+                                <x-table.cell>{{ $building->name }}</x-table.cell>
+
+
+                                <x-table.cell>{{ $building->floors_count }}</x-table.cell>
+
+
+                                <x-table.cell>
+
+                                    <x-action-button-group>
+
+                                        @can(\App\Enums\Policy::View->value, \App\Models\Building::class)
+
+                                            <x-link-button
+                                                class="btn-do"
+                                                icon="eye"
+                                                :href="route('archiving.register.building.show', $building)"
+                                                :text="__('Show')"
+                                                :title="__('Show the record')"/>
+
+                                        @endcan
+
+
+                                        @can(\App\Enums\Policy::Update->value, \App\Models\Building::class)
+
+                                            <x-link-button
+                                                class="btn-do"
+                                                icon="pencil-square"
+                                                :href="route('archiving.register.building.edit', $building)"
+                                                :text="__('Edit')"
+                                                :title="__('Edit the record')"/>
+
+                                        @endcan
+
+                                    </x-action-button-group>
+
+                                </x-table.cell>
+
+                            </x-table.row>
+
+                        @empty
+
+                            <x-table.row>
+
+                                <x-table.cell colspan="1">{{ __('No record found') }}</x-table.cell>
+
+                            </x-table.row>
+
+                        @endforelse
+
+                    </x-slot>
+
+                </x-table>
+
+            </div>
 
         </div>
 
