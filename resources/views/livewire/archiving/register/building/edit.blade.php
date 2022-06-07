a4daa901961f9825c9b03114b85aa8209a36e056
FCNO <fcno.dev@gmail.com>
2022-06-07 01:31:23 -0300

    feat(view): add create button on parent edit page

--- resources/views/livewire/archiving/register/building/edit.blade.php
+++ resources/views/livewire/archiving/register/building/edit.blade.php
@@ -110,11 +110,30 @@
 
         <div class="overflow-x-auto">
 
-            <x-perpage
-                wire:key="per-page"
-                wire:model="per_page"
-                class="mb-3"
-                :error="$errors->first('per_page')"/>
+            <div class="flex items-center justify-between mb-3">
+
+                @can(\App\Enums\Policy::Create->value, \App\Models\Floor::class)
+
+                    <x-link-button
+                        class="btn-do"
+                        icon="plus-circle"
+                        :href="route('archiving.register.floor.create', $building)"
+                        :text="__('New')"
+                        :title="__('Create a new record')"/>
+
+                @else
+
+                    <div></div>
+
+                @endcan
+
+
+                <x-perpage
+                    wire:key="per-page"
+                    wire:model="per_page"
+                    :error="$errors->first('per_page')"/>
+
+            </div>
 
 
             <x-table wire:key="table-floors" wire:loading.delay.class="opacity-25">
