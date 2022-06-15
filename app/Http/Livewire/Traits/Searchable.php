f29e51db498ff26f61e1c958e57d6975eacfd4f8
FCNO <fcno.dev@gmail.com>
2022-06-15 14:46:37 -0300

    refactor(core): moves search core to its own trait

---
+++ app/Http/Livewire/Traits/Searchable.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Livewire\Traits;
+
+use Illuminate\Support\Facades\Validator;
+
+/**
+ * Trait designed group search behaviour.
+ *
+ * @see https://www.php.net/manual/en/language.oop5.traits.php
+ * @see https://laravel-livewire.com/docs/2.x/traits
+ */
+trait Searchable
+{
+    /**
+     * Searchable term entered by the user.
+     *
+     * @var string
+     */
+    public $term;
+
+    /**
+     * Get custom attributes for query strings.
+     *
+     * @return array<string, mixed>
+     */
+    protected function queryString()
+    {
+        return [
+            'term' => [
+                'except' => '',
+                'as' => 's',
+            ],
+        ];
+    }
+
+    /**
+     * Returns the pagination to the initial pagination.
+     *
+     * Runs before a property called $term is updated.
+     *
+     * @param mixed $value
+     *
+     * @return void
+     */
+    public function updatingTerm($value)
+    {
+        Validator::make(
+            data: ['term' => $value],
+            rules: ['term' => ['nullable', 'string', 'max:50']],
+            customAttributes: ['term' => __('Searchable term')]
+        )->validate();
+
+        $this->resetPage();
+    }
+}
