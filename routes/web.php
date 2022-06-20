a9399fa25608679445b47636e6eddc7f0b17d30f
FCNO <fcno.dev@gmail.com>
2022-06-19 21:10:03 -0300

    refactor(route): use id for archiving models route

--- routes/web.php
+++ routes/web.php
@@ -133,9 +133,9 @@ Route::middleware('auth')->group(function () {
         Route::prefix('cadastro')->name('register.')->group(function () {
             Route::prefix('caixa')->name('box.')->group(function () {
                 Route::get('/', BoxLivewireIndex::class)->name('index')->can(Policy::ViewAny->value, Box::class);
-                Route::get('show/{box}', BoxLivewireShow::class)->name('show')->can(Policy::View->value, Box::class);
-                Route::get('prateleira/{shelf}/create', BoxLivewireCreate::class)->name('create')->can(Policy::Create->value, Box::class);
-                Route::get('edit/{box}', BoxLivewireUpdate::class)->name('edit')->can(Policy::Update->value, Box::class);
+                Route::get('show/{id}', BoxLivewireShow::class)->name('show')->can(Policy::View->value, Box::class);
+                Route::get('prateleira/{id}/create', BoxLivewireCreate::class)->name('create')->can(Policy::Create->value, Box::class);
+                Route::get('edit/{id}', BoxLivewireUpdate::class)->name('edit')->can(Policy::Update->value, Box::class);
             });
 
             Route::prefix('localidade')->name('site.')->group(function () {
@@ -147,37 +147,37 @@ Route::middleware('auth')->group(function () {
 
             Route::prefix('predio')->name('building.')->group(function () {
                 Route::get('/', BuildingLivewireIndex::class)->name('index')->can(Policy::ViewAny->value, Building::class);
-                Route::get('show/{building}', BuildingLivewireShow::class)->name('show')->can(Policy::View->value, Building::class);
-                Route::get('localidade/{site}/create', BuildingLivewireCreate::class)->name('create')->can(Policy::Create->value, Building::class);
-                Route::get('edit/{building}', BuildingLivewireUpdate::class)->name('edit')->can(Policy::Update->value, Building::class);
+                Route::get('show/{id}', BuildingLivewireShow::class)->name('show')->can(Policy::View->value, Building::class);
+                Route::get('localidade/{id}/create', BuildingLivewireCreate::class)->name('create')->can(Policy::Create->value, Building::class);
+                Route::get('edit/{id}', BuildingLivewireUpdate::class)->name('edit')->can(Policy::Update->value, Building::class);
             });
 
             Route::prefix('andar')->name('floor.')->group(function () {
                 Route::get('/', FloorLivewireIndex::class)->name('index')->can(Policy::ViewAny->value, Floor::class);
-                Route::get('show/{floor}', FloorLivewireShow::class)->name('show')->can(Policy::View->value, Floor::class);
-                Route::get('predio/{building}/create', FloorLivewireCreate::class)->name('create')->can(Policy::Create->value, Floor::class);
-                Route::get('edit/{floor}', FloorLivewireUpdate::class)->name('edit')->can(Policy::Update->value, Floor::class);
+                Route::get('show/{id}', FloorLivewireShow::class)->name('show')->can(Policy::View->value, Floor::class);
+                Route::get('predio/{id}/create', FloorLivewireCreate::class)->name('create')->can(Policy::Create->value, Floor::class);
+                Route::get('edit/{id}', FloorLivewireUpdate::class)->name('edit')->can(Policy::Update->value, Floor::class);
             });
 
             Route::prefix('sala')->name('room.')->group(function () {
                 Route::get('/', RoomLivewireIndex::class)->name('index')->can(Policy::ViewAny->value, Room::class);
-                Route::get('show/{room}', RoomLivewireShow::class)->name('show')->can(Policy::View->value, Room::class);
-                Route::get('andar/{floor}/create', RoomLivewireCreate::class)->name('create')->can(Policy::Create->value, Room::class);
-                Route::get('edit/{room}', RoomLivewireUpdate::class)->name('edit')->can(Policy::Update->value, Room::class);
+                Route::get('show/{id}', RoomLivewireShow::class)->name('show')->can(Policy::View->value, Room::class);
+                Route::get('andar/{id}/create', RoomLivewireCreate::class)->name('create')->can(Policy::Create->value, Room::class);
+                Route::get('edit/{id}', RoomLivewireUpdate::class)->name('edit')->can(Policy::Update->value, Room::class);
             });
 
             Route::prefix('estante')->name('stand.')->group(function () {
                 Route::get('/', StandLivewireIndex::class)->name('index')->can(Policy::ViewAny->value, Stand::class);
-                Route::get('show/{stand}', StandLivewireShow::class)->name('show')->can(Policy::View->value, Stand::class);
-                Route::get('sala/{room}/create', StandLivewireCreate::class)->name('create')->can(Policy::Create->value, Stand::class);
-                Route::get('edit/{stand}', StandLivewireUpdate::class)->name('edit')->can(Policy::Update->value, Stand::class);
+                Route::get('show/{id}', StandLivewireShow::class)->name('show')->can(Policy::View->value, Stand::class);
+                Route::get('sala/{id}/create', StandLivewireCreate::class)->name('create')->can(Policy::Create->value, Stand::class);
+                Route::get('edit/{id}', StandLivewireUpdate::class)->name('edit')->can(Policy::Update->value, Stand::class);
             });
 
             Route::prefix('prateleira')->name('shelf.')->group(function () {
                 Route::get('/', ShelfLivewireIndex::class)->name('index')->can(Policy::ViewAny->value, Shelf::class);
-                Route::get('show/{shelf}', ShelfLivewireShow::class)->name('show')->can(Policy::View->value, Shelf::class);
-                Route::get('estante/{stand}/create', ShelfLivewireCreate::class)->name('create')->can(Policy::Create->value, Shelf::class);
-                Route::get('edit/{shelf}', ShelfLivewireUpdate::class)->name('edit')->can(Policy::Update->value, Shelf::class);
+                Route::get('show/{id}', ShelfLivewireShow::class)->name('show')->can(Policy::View->value, Shelf::class);
+                Route::get('estante/{id}/create', ShelfLivewireCreate::class)->name('create')->can(Policy::Create->value, Shelf::class);
+                Route::get('edit/{id}', ShelfLivewireUpdate::class)->name('edit')->can(Policy::Update->value, Shelf::class);
             });
         });
     });
