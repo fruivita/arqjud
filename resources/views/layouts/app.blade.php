c29ee7cb5069f74eacace274ae5ca8fe68bb5d50
FCNO <fcno.dev@gmail.com>
2022-06-27 04:51:28 -0300

    feat(view): add efeito hover na logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,7 +70,7 @@
 
                         @endauth
 
-                        class="bg-primary-500 flex font-extrabold items-center h-24 justify-center outline-none rounded-full text-primary-50 w-24 focus:ring focus:ring-primary-300"
+                        class="bg-primary-500 flex font-extrabold items-center h-24 justify-center outline-none rounded-full text-primary-50 transition w-24 hover:bg-primary-700 focus:ring focus:ring-primary-300"
                     >
 
                         {{ config('app.name') }}
