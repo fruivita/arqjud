a740733d43cc3b4b8e329f021d44d3d9dbb95be2
FCNO <fcno.dev@gmail.com>
2022-11-29 01:45:31 -0300

    feat: cria a aplicação inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,51 @@
-import './bootstrap';
+/**
+ * @link https://laravel.com/docs/9.x/vite
+ */
+
+import Layout from '@/Shared/Layout.vue';
+import { createInertiaApp, Link } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createPinia } from 'pinia';
+import { createApp, h } from 'vue';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+import '../css/app.css';
+
+createInertiaApp({
+    resolve: async (name) => {
+        let page = (
+            await resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue'))
+        ).default;
+
+        if (page.layout === undefined) {
+            page.layout = Layout;
+        }
+
+        return page;
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(createPinia())
+            .use(Toast)
+            .mixin({ methods: { route } })
+            .component('InertiaLink', Link)
+            .mount(el);
+    },
+});
+
+InertiaProgress.init({
+    // The delay after which the progress bar will
+    // appear during navigation, in milliseconds.
+    delay: 250,
+
+    // The color of the progress bar.
+    color: 'red',
+
+    // Whether to include the default NProgress styles.
+    includeCSS: true,
+
+    // Whether the NProgress spinner will be shown.
+    showSpinner: true,
+});
