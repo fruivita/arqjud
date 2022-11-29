45703fb42e9f182560f1bf3a2d150690ae2af617
FCNO <fcno.dev@gmail.com>
2022-11-29 14:01:15 -0300

    feat(model): add models
    - Cargo;
    - Função de confiança;
    - Lotação.

---
+++ app/Models/Cargo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use FruiVita\Corporativo\Models\Cargo as CargoCorporativo;
+
+/**
+ * @see https://laravel.com/docs/9.x/eloquent
+ */
+class Cargo extends CargoCorporativo
+{
+    //
+}
---
+++ app/Models/FuncaoConfianca.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use FruiVita\Corporativo\Models\FuncaoConfianca as FuncaoConfiancaCorporativo;
+
+/**
+ * @see https://laravel.com/docs/9.x/eloquent
+ */
+class FuncaoConfianca extends FuncaoConfiancaCorporativo
+{
+    //
+}
