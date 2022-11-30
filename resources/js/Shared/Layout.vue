00b17d0420c54c179e084595d0d0bb82f9d20f00
FCNO <fcno.dev@gmail.com>
2022-11-29 21:10:27 -0300

    feat(view): WIP componente layout padrão

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,14 @@
+<!--
+    Layout padrão da aplicação destinado aos usuários autenticados.
+
+    @see https://vuejs.org/guide/introduction.html
+    @see https://tailwindcss.com/docs
+    @see https://inertiajs.com/
+    @see https://vueuse.org/
+ -->
+
+<script setup></script>
+
+<template>
+    <slot></slot>
+</template>
