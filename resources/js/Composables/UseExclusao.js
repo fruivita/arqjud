2c9919da5ab2d88ca58043eedf44e88c10711c8b
FCNO <fcno.dev@gmail.com>
2022-12-01 02:43:59 -0300

    feat(js): add composable de exclusão de elemento

---
+++ resources/js/Composables/UseExclusao.js
@@ -0,0 +1,51 @@
+/**
+ * Lógica comum da exclusão de items.
+ *
+ * COMPOSABLE SEM TESTE UNITÁRIO. CUIDADO AO ALTERÁ-LO.
+ *
+ * @see https://vuejs.org/guide/introduction.html
+ * @see https://jsdoc.app/index.html
+ * @see https://vue-toastification.maronato.dev/
+ */
+
+import { flash } from '@/Composables/UseFlash';
+import { exibirModalKey, fecharModalKey } from '@/keys.js';
+import { useTranslationsStore } from '@/Stores/TranslationsStore';
+import { Inertia } from '@inertiajs/inertia';
+import { provide, readonly, ref } from 'vue';
+
+export const useExclusao = () => {
+    const __ = useTranslationsStore().__;
+    const item = ref('');
+    const exibirModal = ref(false);
+    const urlDelete = ref('');
+
+    const fecharModal = () => reset();
+    provide(exibirModalKey, readonly(exibirModal));
+    provide(fecharModalKey, fecharModal);
+
+    const titulo = () => __('Excluir :attribute', { attribute: item.value });
+    const confirmarExclusao = (url, valor) => {
+        exibirModal.value = true;
+        urlDelete.value = url;
+        item.value = valor;
+    };
+
+    const excluir = () => {
+        Inertia.delete(urlDelete.value, {
+            preserveScroll: true,
+            preserveState: true,
+
+            onSuccess: () => flash(),
+            onFinish: () => reset(),
+        });
+    };
+
+    const reset = () => {
+        exibirModal.value = false;
+        urlDelete.value = '';
+        item.value = '';
+    };
+
+    return { confirmarExclusao, excluir, titulo };
+};
