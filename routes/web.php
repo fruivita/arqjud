13f93472ce3b163a8e40821516e050804c0e83ce
FCNO <fcno.dev@gmail.com>
2022-12-01 20:13:29 -0300

    feat(route): add rotas crud da localidade

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Enums\Policy;
+use App\Http\Controllers\Cadastro\Localidade\LocalidadeController;
 use App\Http\Controllers\HomeController;
+use App\Models\Localidade;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 
@@ -29,5 +32,16 @@ Route::middleware('auth')->group(function () {
     Route::prefix('home')->name('home.')->group(function () {
         Route::get('/', [HomeController::class, 'show'])->name('show');
         Route::post('/', [HomeController::class, 'show']);
+    });
+
+    Route::prefix('cadastro')->name('cadastro.')->group(function () {
+        Route::prefix('localidade')->name('localidade.')->group(function () {
+            Route::get('/', [LocalidadeController::class, 'index'])->name('index')->can(Policy::ViewAny->value, Localidade::class);
+            Route::get('create', [LocalidadeController::class, 'create'])->name('create')->can(Policy::Create->value, Localidade::class);
+            Route::post('/', [LocalidadeController::class, 'store'])->name('store')->can(Policy::Create->value, Localidade::class);
+            Route::get('{localidade}/edit', [LocalidadeController::class, 'edit'])->name('edit')->can(Policy::ViewOrUpdate->value, Localidade::class);
+            Route::patch('{localidade}', [LocalidadeController::class, 'update'])->name('update')->can(Policy::Update->value, Localidade::class);
+            Route::delete('{localidade}', [LocalidadeController::class, 'destroy'])->name('destroy')->can(Policy::Delete->value, 'localidade');
+        });
     });
 });
