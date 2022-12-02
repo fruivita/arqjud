0acf04b2ea67fc57b2b4566b9d2250d16a73702d
FCNO <fcno.dev@gmail.com>
2022-12-02 01:16:32 -0300

    feat(controller): add LocalidadeOnlyResource

---
+++ tests/Feature/App/Http/Resources/Localidade/LocalidadeOnlyResourceTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://inertiajs.com/testing
+ */
+
+use App\Http\Resources\Localidade\LocalidadeOnlyResource;
+use App\Models\Localidade;
+use Database\Seeders\PerfilSeeder;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+    login();
+
+    $this->localidade = Localidade::factory()->create();
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Caminho feliz
+test('retorna os campos principais do modelo', function () {
+    $resource = LocalidadeOnlyResource::make($this->localidade);
+
+    expect($resource->response(request())->getData(true))->toBe([
+        'data' => $this->localidade->only(['id', 'nome'])
+    ]);
+});
