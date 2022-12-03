dc176cfd1c97b854d4e7387c4d08928b6ad7ddf1
FCNO <fcno.dev@gmail.com>
2022-12-02 23:41:16 -0300

    feat(policy): add PredioPolicy

---
+++ app/Policies/PredioPolicy.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Permissao;
+use App\Models\Predio;
+use App\Models\Usuario;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+/**
+ * @see https://laravel.com/docs/9.x/authorization
+ */
+class PredioPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::PREDIO_VIEW_ANY);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::PREDIO_VIEW);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::PREDIO_CREATE);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::PREDIO_UPDATE);
+    }
+
+    /**
+     * Determina se o usuário pode visualizar ou atualizar um modelo.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewOrUpdate(Usuario $usuario)
+    {
+        return
+            $this->view($usuario)
+            || $this->update($usuario);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @param  \App\Models\Predio  $predio
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(Usuario $usuario, Predio $predio)
+    {
+        return $usuario->possuiPermissao(Permissao::PREDIO_DELETE);
+    }
+}
---
+++ tests/Feature/App/Policies/PredioPolicyTest.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Models\Permissao;
+use App\Enums\Policy;
+use App\Models\Andar;
+use App\Models\Predio;
+use Database\Seeders\LotacaoSeeder;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Support\Facades\Auth;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $this->usuario = login();
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Proibido
+test('usuário sem permissão não pode listar os prédios', function () {
+    expect(Auth::user()->can(Policy::ViewAny->value, Predio::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode visualizar um prédio', function () {
+    expect(Auth::user()->can(Policy::View->value, Predio::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode criar um prédio', function () {
+    expect(Auth::user()->can(Policy::Create->value, Predio::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode atualizar um prédio', function () {
+    expect(Auth::user()->can(Policy::Update->value, Predio::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode visualizar ou atualizar um prédio', function () {
+    expect(Auth::user()->can(Policy::ViewOrUpdate->value, Predio::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode excluir um prédio', function () {
+    $predio = Predio::factory()->create();
+
+    expect(Auth::user()->can(Policy::Delete->value, $predio))->toBeFalse();
+});
+
+// test('prédio com andares não pode ser excluído, independente de permissão', function () {
+//     concederPermissao(Permissao::PredioDelete->value);
+
+//     $predio = Predio::factory()->has(Andar::factory(2), 'andares')->create();
+
+//     expect(Auth::user()->can(Policy::Delete->value, $predio))->toBeFalse();
+// });
+
+// Caminho feliz
+test('usuário com permissão pode listar os prédios', function () {
+    concederPermissao(Permissao::PREDIO_VIEW_ANY);
+
+    expect(Auth::user()->can(Policy::ViewAny->value, Predio::class))->toBeTrue();
+});
+
+test('usuário com permissão pode visualizar um prédio', function () {
+    concederPermissao(Permissao::PREDIO_VIEW);
+
+    expect(Auth::user()->can(Policy::View->value, Predio::class))->toBeTrue();
+});
+
+test('usuário com permissão pode criar um prédio', function () {
+    concederPermissao(Permissao::PREDIO_CREATE);
+
+    expect(Auth::user()->can(Policy::Create->value, Predio::class))->toBeTrue();
+});
+
+test('usuário com permissão pode atualizar um prédio', function () {
+    concederPermissao(Permissao::PREDIO_UPDATE);
+
+    expect(Auth::user()->can(Policy::Update->value, Predio::class))->toBeTrue();
+});
+
+test('usuário com permissão pode visualizar um prédio por meio da policy viewOrUpdate', function () {
+    concederPermissao(Permissao::PREDIO_VIEW);
+
+    expect(Auth::user()->can(Policy::ViewOrUpdate->value, Predio::class))->toBeTrue();
+});
+
+test('usuário com permissão pode atualizar um prédio por meio da policy viewOrUpdate', function () {
+    concederPermissao(Permissao::PREDIO_UPDATE);
+
+    expect(Auth::user()->can(Policy::ViewOrUpdate->value, Predio::class))->toBeTrue();
+});
+
+test('usuário com permissão pode excluir um prédio sem andares', function () {
+    concederPermissao(Permissao::PREDIO_DELETE);
+
+    $predio = Predio::factory()->create();
+
+    expect(Auth::user()->can(Policy::Delete->value, $predio))->toBeTrue();
+});
