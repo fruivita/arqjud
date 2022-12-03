536a48d39102554e828d7551e8229d76ef1740a3
FCNO <fcno.dev@gmail.com>
2022-12-03 04:54:40 -0300

    feat(database): add migration e factory solicitação

---
+++ database/factories/SolicitacaoFactory.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Guia;
+use App\Models\Lotacao;
+use App\Models\Processo;
+use App\Models\Solicitacao;
+use App\Models\Usuario;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Solicitacao>
+ *
+ * @see https://laravel.com/docs/9.x/database-testing
+ * @see https://fakerphp.github.io/
+ */
+class SolicitacaoFactory extends Factory
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $model = Solicitacao::class;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function definition()
+    {
+        return [
+            'solicitada_em' => now()->subWeeks(rand(21, 30)),
+            'entregue_em' => now()->subWeeks(rand(11, 20)),
+            'devolvida_em' => now()->subWeeks(rand(1, 10)),
+            'por_guia' => fake()->boolean(),
+            'descricao' => fake()->optional()->sentence(),
+            'processo_id' => Processo::factory(),
+            'solicitante_id' => Usuario::factory()->comNome(),
+            'recebedor_id' => Usuario::factory()->comNome(),
+            'remetente_id' => Usuario::factory()->comNome(),
+            'rearquivador_id' => Usuario::factory()->comNome(),
+            'lotacao_destinataria_id' => Lotacao::factory(),
+            'guia_id' => Guia::factory(),
+        ];
+    }
+
+    /**
+     * Gera uma solicitação no status solicitada.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function solicitada()
+    {
+        return $this->state(function () {
+            $lotacao = Lotacao::factory()->create();
+
+            return [
+                'entregue_em' => null,
+                'devolvida_em' => null,
+                'por_guia' => false,
+                'solicitante_id' => Usuario::factory()->comNome()->for($lotacao, 'lotacao'),
+                'recebedor_id' => null,
+                'remetente_id' => null,
+                'rearquivador_id' => null,
+                'lotacao_destinataria_id' => $lotacao->id,
+                'guia_id' => null,
+            ];
+        });
+    }
+
+    /**
+     * Gera uma solicitação no status entregue, isto é, ainda não devolvida.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function entregue()
+    {
+        return $this->state(function () {
+            $lotacao = Lotacao::factory()->create();
+
+            return [
+                'devolvida_em' => null,
+                'solicitante_id' => Usuario::factory()->comNome()->for($lotacao, 'lotacao'),
+                'recebedor_id' => Usuario::factory()->comNome()->for($lotacao, 'lotacao'),
+                'rearquivador_id' => null,
+                'lotacao_destinataria_id' => $lotacao->id,
+            ];
+        });
+    }
+
+    /**
+     * Gera uma solicitação no status devolvida.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function devolvida()
+    {
+        return $this->state(function () {
+            $lotacao = Lotacao::factory()->create();
+
+            return [
+                'solicitante_id' => Usuario::factory()->comNome()->for($lotacao, 'lotacao'),
+                'recebedor_id' => Usuario::factory()->comNome()->for($lotacao, 'lotacao'),
+                'lotacao_destinataria_id' => $lotacao->id,
+            ];
+        });
+    }
+}
---
+++ database/migrations/2022_09_04_120000_create_solicitacoes_table.php
@@ -0,0 +1,89 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * @see https://laravel.com/docs/9.x/migrations
+ * @see https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
+ */
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solicitacoes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('processo_id');
+            $table->unsignedBigInteger('solicitante_id');
+            $table->unsignedBigInteger('recebedor_id')->nullable();
+            $table->unsignedBigInteger('remetente_id')->nullable();
+            $table->unsignedBigInteger('rearquivador_id')->nullable();
+            $table->unsignedBigInteger('lotacao_destinataria_id');
+            $table->unsignedBigInteger('guia_id')->nullable();
+            $table->dateTime('solicitada_em');
+            $table->dateTime('entregue_em')->nullable();
+            $table->dateTime('devolvida_em')->nullable();
+            $table->boolean('por_guia');
+            $table->string('descricao', 255)->nullable();
+            $table->timestamps();
+
+            $table
+                ->foreign('processo_id')
+                ->references('id')
+                ->on('processos')
+                ->onUpdate('cascade');
+
+            $table
+                ->foreign('solicitante_id')
+                ->references('id')
+                ->on('usuarios')
+                ->onUpdate('cascade');
+
+            $table
+                ->foreign('recebedor_id')
+                ->references('id')
+                ->on('usuarios')
+                ->onUpdate('cascade');
+
+            $table
+                ->foreign('remetente_id')
+                ->references('id')
+                ->on('usuarios')
+                ->onUpdate('cascade');
+
+            $table
+                ->foreign('rearquivador_id')
+                ->references('id')
+                ->on('usuarios')
+                ->onUpdate('cascade');
+
+            $table
+                ->foreign('lotacao_destinataria_id')
+                ->references('id')
+                ->on('lotacoes')
+                ->onUpdate('cascade');
+
+            $table
+                ->foreign('guia_id')
+                ->references('id')
+                ->on('guias')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solicitacoes');
+    }
+};
