2e8729661462a274cefa71154934b8620a52e856
FCNO <fcno.dev@gmail.com>
2022-12-03 15:35:23 -0300

    feat(policy): add PrateleiraPolicy

---
+++ app/Policies/PrateleiraPolicy.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Permissao;
+use App\Models\Prateleira;
+use App\Models\Usuario;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+/**
+ * @see https://laravel.com/docs/9.x/authorization
+ */
+class PrateleiraPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::PRATELEIRA_VIEW_ANY);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::PRATELEIRA_VIEW);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::PRATELEIRA_CREATE);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::PRATELEIRA_UPDATE);
+    }
+
+    /**
+     * Determina se o usuário pode visualizar ou atualizar um modelo.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewOrUpdate(Usuario $usuario)
+    {
+        return
+            $this->view($usuario)
+            || $this->update($usuario);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @param  \App\Models\Prateleira  $prateleira
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(Usuario $usuario, Prateleira $prateleira)
+    {
+        if (isset($prateleira->caixas_count) !== true) {
+            $prateleira->loadCount('caixas');
+        }
+
+        return
+            $prateleira->caixas_count === 0
+            && $usuario->possuiPermissao(Permissao::PRATELEIRA_DELETE);
+    }
+}
---
+++ tests/Feature/App/Policies/PrateleiraPolicyTest.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Models\Permissao;
+use App\Enums\Policy;
+use App\Models\Caixa;
+use App\Models\Prateleira;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Support\Facades\Auth;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $this->usuario = login();
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Proibido
+test('usuário sem permissão não pode listar as prateleiras', function () {
+    expect(Auth::user()->can(Policy::ViewAny->value, Prateleira::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode visualizar uma prateleira', function () {
+    expect(Auth::user()->can(Policy::View->value, Prateleira::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode criar uma prateleira', function () {
+    expect(Auth::user()->can(Policy::Create->value, Prateleira::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode atualizar uma prateleira', function () {
+    expect(Auth::user()->can(Policy::Update->value, Prateleira::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode visualizar ou atualizar uma prateleira', function () {
+    expect(Auth::user()->can(Policy::ViewOrUpdate->value, Prateleira::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode excluir uma prateleira', function () {
+    $prateleira = Prateleira::factory()->create();
+
+    expect(Auth::user()->can(Policy::Delete->value, $prateleira))->toBeFalse();
+});
+
+test('prateleira com caixas não pode ser excluída, independente de permissão', function () {
+    concederPermissao(Permissao::PRATELEIRA_DELETE);
+
+    $prateleira = Prateleira::factory()->has(Caixa::factory(2), 'caixas')->create();
+
+    expect(Auth::user()->can(Policy::Delete->value, $prateleira))->toBeFalse();
+});
+
+// Caminho feliz
+test('usuário com permissão pode listar as prateleiras', function () {
+    concederPermissao(Permissao::PRATELEIRA_VIEW_ANY);
+
+    expect(Auth::user()->can(Policy::ViewAny->value, Prateleira::class))->toBeTrue();
+});
+
+test('usuário com permissão pode visualizar uma prateleira', function () {
+    concederPermissao(Permissao::PRATELEIRA_VIEW);
+
+    expect(Auth::user()->can(Policy::View->value, Prateleira::class))->toBeTrue();
+});
+
+test('usuário com permissão pode criar uma prateleira', function () {
+    concederPermissao(Permissao::PRATELEIRA_CREATE);
+
+    expect(Auth::user()->can(Policy::Create->value, Prateleira::class))->toBeTrue();
+});
+
+test('usuário com permissão pode atualizar uma prateleira', function () {
+    concederPermissao(Permissao::PRATELEIRA_UPDATE);
+
+    expect(Auth::user()->can(Policy::Update->value, Prateleira::class))->toBeTrue();
+});
+
+test('usuário com permissão pode visualizar uma prateleira por meio da policy viewOrUpdate', function () {
+    concederPermissao(Permissao::PRATELEIRA_VIEW);
+
+    expect(Auth::user()->can(Policy::ViewOrUpdate->value, Prateleira::class))->toBeTrue();
+});
+
+test('usuário com permissão pode atualizar uma prateleira por meio da policy viewOrUpdate', function () {
+    concederPermissao(Permissao::PRATELEIRA_UPDATE);
+
+    expect(Auth::user()->can(Policy::ViewOrUpdate->value, Prateleira::class))->toBeTrue();
+});
+
+test('usuário com permissão pode excluir uma prateleira sem caixas', function () {
+    concederPermissao(Permissao::PRATELEIRA_DELETE);
+
+    $prateleira = Prateleira::factory()->create();
+
+    expect(Auth::user()->can(Policy::Delete->value, $prateleira))->toBeTrue();
+});
