c157b91b8698407a2937ffef2486bb529093bf9e
FCNO <fcno.dev@gmail.com>
2022-12-03 15:38:10 -0300

    feat(policy): add CaixaPolicy

---
+++ app/Policies/CaixaPolicy.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Permissao;
+use App\Models\Caixa;
+use App\Models\Usuario;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+/**
+ * @see https://laravel.com/docs/9.x/authorization
+ */
+class CaixaPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::CAIXA_VIEW_ANY);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::CAIXA_VIEW);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::CAIXA_CREATE);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::CAIXA_UPDATE);
+    }
+
+    /**
+     * Determina se o usuário pode visualizar ou atualizar um modelo.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewOrUpdate(Usuario $usuario)
+    {
+        return
+            $this->view($usuario)
+            || $this->update($usuario);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @param  \App\Models\Caixa  $caixa
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(Usuario $usuario, Caixa $caixa)
+    {
+        if (isset($caixa->volumes_count) !== true) {
+            $caixa->loadCount('volumes');
+        }
+
+        return
+            $caixa->volumes_count === 0
+            && $usuario->possuiPermissao(Permissao::CAIXA_DELETE);
+    }
+}
---
+++ tests/Feature/App/Policies/CaixaPolicyTest.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Models\Permissao;
+use App\Enums\Policy;
+use App\Models\Caixa;
+use App\Models\VolumeCaixa;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Support\Facades\Auth;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $this->usuario = login();
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Proibido
+test('usuário sem permissão não pode listar as caixas', function () {
+    expect(Auth::user()->can(Policy::ViewAny->value, Caixa::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode visualizar uma caixa', function () {
+    expect(Auth::user()->can(Policy::View->value, Caixa::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode criar uma caixa', function () {
+    expect(Auth::user()->can(Policy::Create->value, Caixa::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode atualizar uma caixa', function () {
+    expect(Auth::user()->can(Policy::Update->value, Caixa::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode visualizar ou atualizar uma caixa', function () {
+    expect(Auth::user()->can(Policy::ViewOrUpdate->value, Caixa::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode excluir uma caixa', function () {
+    $caixa = Caixa::factory()->create();
+
+    expect(Auth::user()->can(Policy::Delete->value, $caixa))->toBeFalse();
+});
+
+test('caixa com volumes não pode ser excluída, independente de permissão', function () {
+    concederPermissao(Permissao::CAIXA_DELETE);
+
+    $caixa = Caixa::factory()->has(VolumeCaixa::factory(2), 'volumes')->create();
+
+    expect(Auth::user()->can(Policy::Delete->value, $caixa))->toBeFalse();
+});
+
+// Caminho feliz
+test('usuário com permissão pode listar as caixas', function () {
+    concederPermissao(Permissao::CAIXA_VIEW_ANY);
+
+    expect(Auth::user()->can(Policy::ViewAny->value, Caixa::class))->toBeTrue();
+});
+
+test('usuário com permissão pode visualizar uma caixa', function () {
+    concederPermissao(Permissao::CAIXA_VIEW);
+
+    expect(Auth::user()->can(Policy::View->value, Caixa::class))->toBeTrue();
+});
+
+test('usuário com permissão pode criar uma caixa', function () {
+    concederPermissao(Permissao::CAIXA_CREATE);
+
+    expect(Auth::user()->can(Policy::Create->value, Caixa::class))->toBeTrue();
+});
+
+test('usuário com permissão pode atualizar uma caixa', function () {
+    concederPermissao(Permissao::CAIXA_UPDATE);
+
+    expect(Auth::user()->can(Policy::Update->value, Caixa::class))->toBeTrue();
+});
+
+test('usuário com permissão pode visualizar uma caixa por meio da policy viewOrUpdate', function () {
+    concederPermissao(Permissao::CAIXA_VIEW);
+
+    expect(Auth::user()->can(Policy::ViewOrUpdate->value, Caixa::class))->toBeTrue();
+});
+
+test('usuário com permissão pode atualizar uma caixa por meio da policy viewOrUpdate', function () {
+    concederPermissao(Permissao::CAIXA_UPDATE);
+
+    expect(Auth::user()->can(Policy::ViewOrUpdate->value, Caixa::class))->toBeTrue();
+});
+
+test('usuário com permissão pode excluir uma caixa sem volumes', function () {
+    concederPermissao(Permissao::CAIXA_DELETE);
+
+    $caixa = Caixa::factory()->create();
+
+    expect(Auth::user()->can(Policy::Delete->value, $caixa))->toBeTrue();
+});
