8c424308549d7ac67eca479e1bcaeec174240f9a
FCNO <fcno.dev@gmail.com>
2022-12-03 17:10:41 -0300

    feat(model): add esqueleto do model Guia

---
+++ app/Models/Guia.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\AsCollection;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @see https://laravel.com/docs/9.x/eloquent
+ */
+class Guia extends Model
+{
+    use HasFactory;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $table = 'guias';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $casts = [
+        'remetente' => AsCollection::class,
+        'recebedor' => AsCollection::class,
+        'lotacao_destinataria' => AsCollection::class,
+        'processos' => AsCollection::class,
+    ];
+
+    /**
+     * Relacionamento guia (1:N) solicitações.
+     *
+     * Solicitações registradas na guia.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function solicitacoes()
+    {
+        return $this->hasMany(Solicitacao::class, 'guia_id', 'id');
+    }
+}
