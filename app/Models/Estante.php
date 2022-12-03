d3d4b953face1ba3749edf0ef8d88c1494df5112
FCNO <fcno.dev@gmail.com>
2022-12-03 17:10:35 -0300

    feat(model): add esqueleto do model Estante

---
+++ app/Models/Estante.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+/**
+ * @see https://laravel.com/docs/9.x/eloquent
+ */
+class Estante extends Model
+{
+    use HasFactory;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $table = 'estantes';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $fillable = ['numero'];
+
+    /**
+     * Relacionamento estante (N:1) sala.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function sala()
+    {
+        return $this->belongsTo(Sala::class, 'sala_id', 'id');
+    }
+
+    /**
+     * Relacionamento estante (1:N) prateleiras.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function prateleiras()
+    {
+        return $this->hasMany(Prateleira::class, 'estante_id', 'id');
+    }
+}
