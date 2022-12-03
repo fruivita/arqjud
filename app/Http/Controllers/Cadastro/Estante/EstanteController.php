027886fecd1b14bb1c0d3f45d7c21ce8eb7b622b
FCNO <fcno.dev@gmail.com>
2022-12-03 17:07:43 -0300

    feat(controller): add esqueleto da EstanteController

---
+++ app/Http/Controllers/Cadastro/Estante/EstanteController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Cadastro\Estante;
+
+use App\Http\Controllers\Controller;
+use App\Models\Estante;
+use Illuminate\Http\Request;
+
+class EstanteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Estante  $estante
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Estante $estante)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Estante  $estante
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Estante $estante)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Estante  $estante
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Estante $estante)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Estante  $estante
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Estante $estante)
+    {
+        //
+    }
+}
