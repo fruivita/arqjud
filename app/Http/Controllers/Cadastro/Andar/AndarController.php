a1b251c7d160ee5ae86dd5961f9d2bee43b41921
FCNO <fcno.dev@gmail.com>
2022-12-03 17:07:21 -0300

    feat(controller): add esqueleto do AndarController

---
+++ app/Http/Controllers/Cadastro/Andar/AndarController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Cadastro\Andar;
+
+use App\Http\Controllers\Controller;
+use App\Models\Andar;
+use Illuminate\Http\Request;
+
+class AndarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Andar  $andar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Andar $andar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Andar  $andar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Andar $andar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Andar  $andar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Andar $andar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Andar  $andar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Andar $andar)
+    {
+        //
+    }
+}
