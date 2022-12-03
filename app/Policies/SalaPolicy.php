2504f17a3c214dd7b9947939229c844068b0f056
FCNO <fcno.dev@gmail.com>
2022-12-03 15:28:47 -0300

    feat(policy): add SalaPolicy

---
+++ app/Policies/SalaPolicy.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Permissao;
+use App\Models\Sala;
+use App\Models\Usuario;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+/**
+ * @see https://laravel.com/docs/9.x/authorization
+ */
+class SalaPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::SALA_VIEW_ANY);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::SALA_VIEW);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::SALA_CREATE);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(Usuario $usuario)
+    {
+        return $usuario->possuiPermissao(Permissao::SALA_UPDATE);
+    }
+
+    /**
+     * Determina se o usuário pode visualizar ou atualizar um modelo.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewOrUpdate(Usuario $usuario)
+    {
+        return
+            $this->view($usuario)
+            || $this->update($usuario);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @param  \App\Models\Sala  $sala
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(Usuario $usuario, Sala $sala)
+    {
+        if (isset($sala->estantes_count) !== true) {
+            $sala->loadCount('estantes');
+        }
+
+        return
+            $sala->estantes_count === 0
+            && $usuario->possuiPermissao(Permissao::SALA_DELETE);
+    }
+}
---
+++ tests/Feature/App/Policies/SalaPolicyTest.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Models\Permissao;
+use App\Enums\Policy;
+use App\Models\Estante;
+use App\Models\Sala;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Support\Facades\Auth;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $this->usuario = login();
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Proibido
+test('usuário sem permissão não pode listar as salas', function () {
+    expect(Auth::user()->can(Policy::ViewAny->value, Sala::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode visualizar uma sala', function () {
+    expect(Auth::user()->can(Policy::View->value, Sala::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode criar uma sala', function () {
+    expect(Auth::user()->can(Policy::Create->value, Sala::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode atualizar uma sala', function () {
+    expect(Auth::user()->can(Policy::Update->value, Sala::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode visualizar ou atualizar uma sala', function () {
+    expect(Auth::user()->can(Policy::ViewOrUpdate->value, Sala::class))->toBeFalse();
+});
+
+test('usuário sem permissão não pode excluir uma sala', function () {
+    $sala = Sala::factory()->create();
+
+    expect(Auth::user()->can(Policy::Delete->value, $sala))->toBeFalse();
+});
+
+test('sala com estantes não pode ser excluída, independente de permissão', function () {
+    concederPermissao(Permissao::SALA_DELETE);
+
+    $sala = Sala::factory()->has(Estante::factory(2), 'estantes')->create();
+
+    expect(Auth::user()->can(Policy::Delete->value, $sala))->toBeFalse();
+});
+
+// Caminho feliz
+test('usuário com permissão pode listar as salas', function () {
+    concederPermissao(Permissao::SALA_VIEW_ANY);
+
+    expect(Auth::user()->can(Policy::ViewAny->value, Sala::class))->toBeTrue();
+});
+
+test('usuário com permissão pode visualizar uma sala', function () {
+    concederPermissao(Permissao::SALA_VIEW);
+
+    expect(Auth::user()->can(Policy::View->value, Sala::class))->toBeTrue();
+});
+
+test('usuário com permissão pode criar uma sala', function () {
+    concederPermissao(Permissao::SALA_CREATE);
+
+    expect(Auth::user()->can(Policy::Create->value, Sala::class))->toBeTrue();
+});
+
+test('usuário com permissão pode atualizar uma sala', function () {
+    concederPermissao(Permissao::SALA_UPDATE);
+
+    expect(Auth::user()->can(Policy::Update->value, Sala::class))->toBeTrue();
+});
+
+test('usuário com permissão pode visualizar uma sala por meio da policy viewOrUpdate', function () {
+    concederPermissao(Permissao::SALA_VIEW);
+
+    expect(Auth::user()->can(Policy::ViewOrUpdate->value, Sala::class))->toBeTrue();
+});
+
+test('usuário com permissão pode atualizar uma sala por meio da policy viewOrUpdate', function () {
+    concederPermissao(Permissao::SALA_UPDATE);
+
+    expect(Auth::user()->can(Policy::ViewOrUpdate->value, Sala::class))->toBeTrue();
+});
+
+test('usuário com permissão pode excluir uma sala sem estantes', function () {
+    concederPermissao(Permissao::SALA_DELETE);
+
+    $sala = Sala::factory()->create();
+
+    expect(Auth::user()->can(Policy::Delete->value, $sala))->toBeTrue();
+});
