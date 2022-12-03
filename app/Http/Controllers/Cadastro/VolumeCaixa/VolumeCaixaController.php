89a6348caebe34aa32b26bc903ea0712ad426919
FCNO <fcno.dev@gmail.com>
2022-12-03 17:09:09 -0300

    feat(controller): add esqueleto do VolumeCaixaController

---
+++ app/Http/Controllers/Cadastro/VolumeCaixa/VolumeCaixaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Cadastro\VolumeCaixa;
+
+use App\Http\Controllers\Controller;
+use App\Models\VolumeCaixa;
+use Illuminate\Http\Request;
+
+class VolumeCaixaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\VolumeCaixa  $volumeCaixa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(VolumeCaixa $volumeCaixa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\VolumeCaixa  $volumeCaixa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(VolumeCaixa $volumeCaixa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\VolumeCaixa  $volumeCaixa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, VolumeCaixa $volumeCaixa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\VolumeCaixa  $volumeCaixa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(VolumeCaixa $volumeCaixa)
+    {
+        //
+    }
+}
