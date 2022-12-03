36d445f561df984ebffc2dbd71d7b6bdfb3b0612
FCNO <fcno.dev@gmail.com>
2022-12-03 17:11:03 -0300

    feat(model): add esqueleto do model Sala

---
+++ app/Models/Sala.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+/**
+ * @see https://laravel.com/docs/9.x/eloquent
+ */
+class Sala extends Model
+{
+    use HasFactory;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $table = 'salas';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $fillable = ['numero'];
+
+    /**
+     * Relacionamento sala (N:1) andar.
+     *
+     * Andar da sala.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function andar()
+    {
+        return $this->belongsTo(Andar::class, 'andar_id', 'id');
+    }
+
+    /**
+     * Relacionamento sala (1:N) estantes.
+     *
+     * Estantes da sala.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function estantes()
+    {
+        return $this->hasMany(Estante::class, 'sala_id', 'id');
+    }
+}
