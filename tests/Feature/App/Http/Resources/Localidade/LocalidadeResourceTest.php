588694456263aea280d100a8ee085ce7ef284708
FCNO <fcno.dev@gmail.com>
2022-12-04 01:01:33 -0300

    style(api): altera estilo de codificação

--- tests/Feature/App/Http/Resources/Localidade/LocalidadeResourceTest.php
+++ tests/Feature/App/Http/Resources/Localidade/LocalidadeResourceTest.php
@@ -28,12 +28,13 @@ test('retorna os campos principais e as rotas autorizadas do modelo', function (
     $resource = LocalidadeResource::make($this->localidade);
 
     expect($resource->response(request())->getData(true))->toBe([
-        'data' => $this->localidade->only(['id', 'nome']) + [
-            'links' => [
-                'view_or_update' => route('cadastro.localidade.edit', $this->localidade),
-                'delete' => route('cadastro.localidade.destroy', $this->localidade),
+        'data' => $this->localidade->only(['id', 'nome'])
+            + [
+                'links' => [
+                    'view_or_update' => route('cadastro.localidade.edit', $this->localidade),
+                    'delete' => route('cadastro.localidade.destroy', $this->localidade),
+                ]
             ]
-        ]
     ]);
 });
 
@@ -41,15 +42,16 @@ test('retorna a quantidade de filhos se houver o eager load da propriedade', fun
     $resource = LocalidadeResource::make($this->localidade->loadCount(['predios', 'caixasCriadas']));
 
     expect($resource->response(request())->getData(true))->toBe([
-        'data' => $this->localidade->only(['id', 'nome', 'predios_count', 'caixas_criadas_count']) + [
-            'links' => []
-        ]
+        'data' => $this->localidade->only(['id', 'nome', 'predios_count', 'caixas_criadas_count'])
+            + ['links' => []]
     ]);
 });
 
 test('retorna apenas os campos principais se não houver rota autorizada para o modelo', function () {
     $resource = LocalidadeResource::make($this->localidade);
 
-    expect($resource->response(request())->getData(true))
-        ->toBe(['data' => $this->localidade->only(['id', 'nome']) + ['links' => []]]);
+    expect($resource->response(request())->getData(true))->toBe([
+        'data' => $this->localidade->only(['id', 'nome'])
+            + ['links' => []]
+    ]);
 });
