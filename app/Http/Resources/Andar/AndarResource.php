414c1f1d4ade58962c99d323ed64111188f554a5
FCNO <fcno.dev@gmail.com>
2022-12-04 03:41:14 -0300

    feat(api): add AndarResource

---
+++ app/Http/Resources/Andar/AndarResource.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Resources\Andar;
+
+use App\Enums\Policy;
+use App\Http\Resources\Localidade\LocalidadeOnlyResource;
+use App\Http\Resources\Predio\PredioOnlyResource;
+use App\Models\Andar;
+use App\Models\Sala;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @see https://laravel.com/docs/9.x/eloquent-resources
+ */
+class AndarResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'numero' => $this->numero,
+            'apelido' => $this->apelido,
+            'predio_id' => $this->predio_id,
+            'predio' => PredioOnlyResource::make($this->whenLoaded('predio')),
+            'salas_count' => $this->whenCounted('salas'),
+            'links' => [
+                'create_sala' => $this->when(
+                    $request->user()->can(Policy::Create->value, Sala::class),
+                    fn () => route('cadastro.sala.create', $this->id),
+                ),
+                'view_or_update' => $this->when(
+                    $request->user()->can(Policy::ViewOrUpdate->value, Andar::class),
+                    fn () => route('cadastro.andar.edit', $this->id),
+                ),
+                'delete' => $this->when(
+                    $request->user()->can(Policy::Delete->value, $this->resource),
+                    fn () => route('cadastro.andar.destroy', $this->id),
+                ),
+            ],
+        ];
+    }
+}
---
+++ tests/Feature/App/Http/Resources/Andar/AndarResourceTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://inertiajs.com/testing
+ */
+
+use App\Http\Resources\Andar\AndarResource;
+use App\Models\Andar;
+use App\Models\Permissao;
+use Database\Seeders\PerfilSeeder;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+    login();
+
+    $this->andar = Andar::factory()->create();
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Caminho feliz
+test('retorna os campos principais e as rotas autorizadas do modelo', function () {
+    concederPermissao([Permissao::SALA_CREATE, Permissao::ANDAR_DELETE, Permissao::ANDAR_VIEW]);
+
+    $resource = AndarResource::make($this->andar);
+
+    expect($resource->response(request())->getData(true))->toBe([
+        'data' => $this->andar->only(['id', 'numero', 'apelido', 'predio_id'])
+            + [
+                'links' => [
+                    'create_sala' => route('cadastro.sala.create', $this->andar),
+                    'view_or_update' => route('cadastro.andar.edit', $this->andar),
+                    'delete' => route('cadastro.andar.destroy', $this->andar),
+                ]
+            ]
+    ]);
+});
+
+test('retorna o prédio pai se houver o eager load da propriedade', function () {
+    $resource = AndarResource::make($this->andar->load('predio'));
+
+    expect($resource->response(request())->getData(true))->toBe([
+        'data' => $this->andar->only(['id', 'numero', 'apelido', 'predio_id'])
+            + ['predio' => $this->andar->predio->only(['id', 'nome', 'localidade_id'])]
+            + ['links' => []]
+    ]);
+});
+
+test('retorna a quantidade de filhos se houver o eager load da propriedade', function () {
+    $resource = AndarResource::make($this->andar->loadCount('salas'));
+
+    expect($resource->response(request())->getData(true))->toBe([
+        'data' => $this->andar->only(['id', 'numero', 'apelido', 'predio_id', 'salas_count'])
+            + ['links' => []]
+    ]);
+});
+
+test('retorna apenas os campos principais se não houver rota autorizada para o modelo', function () {
+    $resource = AndarResource::make($this->andar);
+
+    expect($resource->response(request())->getData(true))->toBe([
+        'data' => $this->andar->only(['id', 'numero', 'apelido', 'predio_id'])
+            + ['links' => []]
+    ]);
+});
