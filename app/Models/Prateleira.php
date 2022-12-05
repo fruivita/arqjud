680a443c3fc1ec9cccb809f9ea6b71d2aa271dff
FCNO <fcno.dev@gmail.com>
2022-12-04 22:32:30 -0300

    feat(model): add escopo search à prateleira

--- app/Models/Prateleira.php
+++ app/Models/Prateleira.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -40,5 +41,42 @@ class Prateleira extends Model
     public function caixas()
     {
         return $this->hasMany(Caixa::class, 'prateleira_id', 'id');
+    }
+
+    /**
+     * Pesquisa utilizando o termo informado com o operador like no seguinte
+     * formato: `termo%`
+     *
+     * Pressupõe join com as tabelas:
+     * - Localidades;
+     * - Prédios;
+     * - Andares;
+     * - Salas;
+     * - Estantes.
+     *
+     * Colunas pesquisadas:
+     * - nome da localidade;
+     * - nome do prédio;
+     * - número do andar;
+     * - apelido do andar;
+     * - número da sala;
+     * - número da estante;
+     * - número da prateleira.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string|null  $termo
+     * @return void
+     */
+    public function scopeSearch(Builder $query, string $termo = null)
+    {
+        $termo = "{$termo}%";
+
+        $query->where('localidades.nome', 'like', $termo)
+            ->orWhere('predios.nome', 'like', $termo)
+            ->orWhere('andares.numero', 'like', $termo)
+            ->orWhere('andares.apelido', 'like', $termo)
+            ->orWhere('salas.numero', 'like', $termo)
+            ->orWhere('estantes.numero', 'like', $termo)
+            ->orWhere('prateleiras.numero', 'like', $termo);
     }
 }
---
+++ tests/Feature/App/Models/PrateleiraTest.php
@@ -0,0 +1,227 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Models\Andar;
+use App\Models\Caixa;
+use App\Models\Estante;
+use App\Models\Localidade;
+use App\Models\Prateleira;
+use App\Models\Predio;
+use App\Models\Sala;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Str;
+
+// Exceptions
+test('lança exception ao tentar criar prateleiras duplicadas, isto é, com mesmo número e estante', function () {
+    $estante = Estante::factory()->create();
+
+    expect(
+        fn () => Prateleira::factory(2)->create([
+            'numero' => 100,
+            'estante_id' => $estante->id,
+        ])
+    )->toThrow(QueryException::class, 'Duplicate entry');
+});
+
+test('lança exception ao tentar criar prateleira com campo inválido', function ($campo, $valor, $mensagem) {
+    expect(
+        fn () => Prateleira::factory()->create([$campo => $valor])
+    )->toThrow(QueryException::class, $mensagem);
+})->with([
+    ['numero',    null,             'cannot be null'],           // obrgatório
+    ['numero',    Str::random(51),  'Data too long for column'], // máximo 51 caracteres
+    ['descricao', Str::random(256), 'Data too long for column'], // máximo 255 caracteres
+]);
+
+test('lança exception ao tentar definir relacionamento inválido', function ($campo, $valor, $mensagem) {
+    expect(
+        fn () => Prateleira::factory()->create([$campo => $valor])
+    )->toThrow(QueryException::class, $mensagem);
+})->with([
+    ['estante_id', 99999999, 'Cannot add or update a child row'], // não existente
+    ['estante_id', null,     'cannot be null'],                   // obrigatório
+]);
+
+// Caminho feliz
+test('aceita campos em seus tamanhos máximos', function () {
+    Prateleira::factory()->create([
+        'numero' => Str::random(50),
+        'descricao' => Str::random(255),
+    ]);
+
+    expect(Prateleira::count())->toBe(1);
+});
+
+test('campos opcionais estão definidos', function () {
+    Prateleira::factory()->create([
+        'descricao' => null,
+    ]);
+
+    expect(Prateleira::count())->toBe(1);
+});
+
+test('zero é válido para o número da prateleira', function () {
+    Prateleira::factory()->create(['numero' => 0]);
+
+    $prateleira = Prateleira::first();
+
+    expect($prateleira->numero)->toBe('0');
+});
+
+test('uma prateleira pertence a uma estante', function () {
+    $prateleira = Prateleira::factory()->for(Estante::factory(), 'estante')->create();
+
+    $prateleira->load(['estante']);
+
+    expect($prateleira->estante)->toBeInstanceOf(Estante::class);
+});
+
+test('uma prateleira possui muitas caixas', function () {
+    Prateleira::factory()->has(Caixa::factory(3), 'caixas')->create();
+
+    $prateleira = Prateleira::with('caixas')->first();
+
+    expect($prateleira->caixas)->toHaveCount(3);
+});
+
+test('retorna as prateleiras pelo escopo search que busca a partir do início do texto no número do prateleira', function (string $termo, int $quantidade) {
+    Prateleira::factory()->create(['numero' => 'aaaaaaaa']);
+    Prateleira::factory()->create(['numero' => 'ccccbbbb']);
+    Prateleira::factory()->create(['numero' => 'cccccccc']);
+    Prateleira::factory()->create(['numero' => 'dddddddd']);
+
+    $query = Prateleira::query()
+        ->join('estantes', 'estantes.id', 'prateleiras.estante_id')
+        ->join('salas', 'salas.id', 'estantes.sala_id')
+        ->join('andares', 'andares.id', 'salas.andar_id')
+        ->join('predios', 'predios.id', 'andares.predio_id')
+        ->join('localidades', 'localidades.id', 'predios.localidade_id');
+
+    expect($query->search($termo)->count())->toBe($quantidade);
+})->with([
+    ['', 4],
+    ['ccc', 2],
+    ['ddd', 1],
+    ['ccbb', 0],
+]);
+
+test('retorna as prateleiras pelo escopo search que busca a partir do início do texto no número da estante', function (string $termo, int $quantidade) {
+    Estante::factory()->hasPrateleiras(2)->create(['numero' => 'aaaaaaaa']);
+    Estante::factory()->hasPrateleiras(3)->create(['numero' => 'bbbbbbbb']);
+
+    $query = Prateleira::query()
+        ->join('estantes', 'estantes.id', 'prateleiras.estante_id')
+        ->join('salas', 'salas.id', 'estantes.sala_id')
+        ->join('andares', 'andares.id', 'salas.andar_id')
+        ->join('predios', 'predios.id', 'andares.predio_id')
+        ->join('localidades', 'localidades.id', 'predios.localidade_id');
+
+    expect($query->search($termo)->count())->toBe($quantidade);
+})->with([
+    ['', 5],
+    ['aaaa', 2],
+    ['bbbb', 3],
+]);
+
+test('retorna as prateleiras pelo escopo search que busca a partir do início do texto no número da sala', function (string $termo, int $quantidade) {
+    Sala::factory()->has(Estante::factory()->hasPrateleiras(2))->create(['numero' => 'aaaaaaaa']);
+    Sala::factory()->has(Estante::factory()->hasPrateleiras(3))->create(['numero' => 'bbbbbbbb']);
+
+    $query = Prateleira::query()
+        ->join('estantes', 'estantes.id', 'prateleiras.estante_id')
+        ->join('salas', 'salas.id', 'estantes.sala_id')
+        ->join('andares', 'andares.id', 'salas.andar_id')
+        ->join('predios', 'predios.id', 'andares.predio_id')
+        ->join('localidades', 'localidades.id', 'predios.localidade_id');
+
+    expect($query->search($termo)->count())->toBe($quantidade);
+})->with([
+    ['', 5],
+    ['aaaa', 2],
+    ['bbbb', 3],
+]);
+
+test('retorna as prateleiras pelo escopo search que busca a partir do início do texto no número e apelido do andar', function (string $termo, int $quantidade) {
+    Andar::factory()
+        ->has(Sala::factory()
+            ->has(Estante::factory()->hasPrateleiras(2)))
+        ->create(['numero' => 99999999, 'apelido' => 'aaaaaaaa']);
+    Andar::factory()
+        ->has(Sala::factory()
+            ->has(Estante::factory()->hasPrateleiras(3)))
+        ->create(['numero' => 88888888, 'apelido' => 'bbbbbbbb']);
+
+    $query = Prateleira::query()
+        ->join('estantes', 'estantes.id', 'prateleiras.estante_id')
+        ->join('salas', 'salas.id', 'estantes.sala_id')
+        ->join('andares', 'andares.id', 'salas.andar_id')
+        ->join('predios', 'predios.id', 'andares.predio_id')
+        ->join('localidades', 'localidades.id', 'predios.localidade_id');
+
+    expect($query->search($termo)->count())->toBe($quantidade);
+})->with([
+    ['', 5],
+    ['aaaa', 2],
+    ['bbbb', 3],
+    [999999, 2],
+    [888888, 3],
+]);
+
+test('retorna as prateleiras pelo escopo search que busca a partir do início do texto no nome do prédio', function (string $termo, int $quantidade) {
+    Predio::factory()
+        ->has(Andar::factory()
+            ->has(Sala::factory()
+                ->has(Estante::factory()->hasPrateleiras(2))), 'andares')
+        ->create(['nome' => 'aaaaaaaa']);
+    Predio::factory()
+        ->has(Andar::factory()
+            ->has(Sala::factory()
+                ->has(Estante::factory()->hasPrateleiras(3))), 'andares')
+        ->create(['nome' => 'bbbbbbbb']);
+
+    $query = Prateleira::query()
+        ->join('estantes', 'estantes.id', 'prateleiras.estante_id')
+        ->join('salas', 'salas.id', 'estantes.sala_id')
+        ->join('andares', 'andares.id', 'salas.andar_id')
+        ->join('predios', 'predios.id', 'andares.predio_id')
+        ->join('localidades', 'localidades.id', 'predios.localidade_id');
+
+    expect($query->search($termo)->count())->toBe($quantidade);
+})->with([
+    ['', 5],
+    ['aaaa', 2],
+    ['bbbb', 3],
+]);
+
+
+test('retorna as prateleiras pelo escopo search que busca a partir do início do texto no nome da localidade', function (string $termo, int $quantidade) {
+    Localidade::factory()
+        ->has(Predio::factory()
+            ->has(Andar::factory()
+                ->has(Sala::factory()
+                    ->has(Estante::factory()->hasPrateleiras(2))), 'andares'))
+        ->create(['nome' => 'aaaaaaaa']);
+
+    Localidade::factory()
+        ->has(Predio::factory()
+            ->has(Andar::factory()
+                ->has(Sala::factory()
+                    ->has(Estante::factory()->hasPrateleiras(3))), 'andares'))
+        ->create(['nome' => 'bbbbbbbb']);
+
+    $query = Prateleira::query()
+        ->join('estantes', 'estantes.id', 'prateleiras.estante_id')
+        ->join('salas', 'salas.id', 'estantes.sala_id')
+        ->join('andares', 'andares.id', 'salas.andar_id')
+        ->join('predios', 'predios.id', 'andares.predio_id')
+        ->join('localidades', 'localidades.id', 'predios.localidade_id');
+
+    expect($query->search($termo)->count())->toBe($quantidade);
+})->with([
+    ['', 5],
+    ['aaaa', 2],
+    ['bbbb', 3],
+]);
