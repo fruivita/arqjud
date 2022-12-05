6b108c2832369aaf85ed3a54c66ed9d1ed0dc14a
FCNO <fcno.dev@gmail.com>
2022-12-05 12:50:19 -0300

    feat(api): add VolumeCaixaOnlyResource

---
+++ app/Http/Resources/VolumeCaixa/VolumeCaixaOnlyResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\VolumeCaixa;
+
+use App\Http\Resources\Caixa\CaixaOnlyResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @see https://laravel.com/docs/9.x/eloquent-resources
+ */
+class VolumeCaixaOnlyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'numero' => $this->numero,
+            'caixa_id' => $this->caixa_id,
+            'caixa' => CaixaOnlyResource::make($this->whenLoaded('caixa')),
+            'processos_count' => $this->whenCounted('processos'),
+        ];
+    }
+}
---
+++ tests/Feature/App/Http/Resources/VolumeCaixa/VolumeCaixaOnlyResourceTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://inertiajs.com/testing
+ */
+
+use App\Http\Resources\VolumeCaixa\VolumeCaixaOnlyResource;
+use App\Models\VolumeCaixa;
+use Database\Seeders\PerfilSeeder;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+    login();
+
+    $this->volume = VolumeCaixa::factory()->create();
+
+    $this->volume_api = [
+        'id' => $this->volume->id,
+        'numero' => $this->volume->numero,
+        'caixa_id' => $this->volume->caixa_id,
+    ];
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Caminho feliz
+test('retorna os campos principais do modelo', function () {
+    $resource = VolumeCaixaOnlyResource::make($this->volume);
+
+    expect($resource->response(request())->getData(true))->toBe(['data' => $this->volume_api]);
+});
+
+test('retorna a caixa pai se houver o eager load da propriedade', function () {
+    $resource = VolumeCaixaOnlyResource::make($this->volume->load(['caixa']));
+
+    $caixa_api = [
+        'id' => $this->volume->caixa->id,
+        'numero' => $this->volume->caixa->numero,
+        'ano' => $this->volume->caixa->ano,
+        'guarda_permanente' => $this->volume->caixa->guarda_permanente ? __('Sim') : __('Não'),
+        'complemento' => $this->volume->caixa->complemento,
+        'prateleira_id' => $this->volume->caixa->prateleira_id,
+        'localidade_criadora_id' => $this->volume->caixa->localidade_criadora_id,
+    ];
+
+    expect($resource->response(request())->getData(true))->toBe([
+        'data' => $this->volume_api
+            + ['caixa' => $caixa_api]
+    ]);
+});
+
+test('retorna a quantidade de filhos se houver o eager load da propriedade', function () {
+    $resource = VolumeCaixaOnlyResource::make($this->volume->loadCount('processos'));
+
+    expect($resource->response(request())->getData(true))->toBe([
+        'data' => $this->volume_api
+            + $this->volume->only('processos_count')
+    ]);
+});
