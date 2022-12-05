1feca3289435856a277d1f03197660f757c8f27b
FCNO <fcno.dev@gmail.com>
2022-12-04 22:44:09 -0300

    feat(core): add filtro order da prateleira

---
+++ app/Filters/Prateleira/Order.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Filters\Prateleira;
+
+use App\Filters\OrderBase;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Pressupõe join com as tabelas pais.
+ *
+ * @see https://www.youtube.com/watch?v=FByQN_d876c
+ */
+class Order extends OrderBase
+{
+    /**
+     * Aplica a ordenação pelo número da prateleira.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function numero(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('numero', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pela quantidade de caixas da prateleira.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function caixasCount(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('caixas_count', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo nome da localidade pai.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function localidadePaiNome(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('localidades.nome', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo nome do prédio pai.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function predioPaiNome(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('predios.nome', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo número do andar pai.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function andarPaiNumero(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('andares.numero', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo apelido do andar pai.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function andarPaiApelido(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('andares.apelido', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo número da sala pai.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function salaPaiNumero(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('salas.numero', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo número da estante pai.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function estantePaiNumero(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('estantes.numero', $direcao);
+    }
+}
