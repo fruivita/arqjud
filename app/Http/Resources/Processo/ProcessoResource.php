2efb9e81b7eca13b65646443d586e399d5e81a41
FCNO <fcno.dev@gmail.com>
2022-12-05 23:22:53 -0300

    feat(api): add ProcessoResource

---
+++ app/Http/Resources/Processo/ProcessoResource.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Resources\Processo;
+
+use App\Enums\Policy;
+use App\Http\Resources\Processo\ProcessoOnlyResource;
+use App\Http\Resources\VolumeCaixa\VolumeCaixaOnlyResource;
+use App\Models\Processo;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @see https://laravel.com/docs/9.x/eloquent-resources
+ */
+class ProcessoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'numero' => $this->numero,
+            'numero_antigo' => $this->numero_antigo,
+            'arquivado_em' => $this->arquivado_em,
+            'guarda_permanente' => $this->guarda_permanente ? __('Sim') : __('Não'),
+            'qtd_volumes' => $this->qtd_volumes,
+            'volume_caixa_id' => $this->volume_caixa_id,
+            'processo_pai_id' => $this->processo_pai_id,
+            'volume_caixa' => VolumeCaixaOnlyResource::make($this->whenLoaded('volumeCaixa')),
+            'processo_pai' => ProcessoOnlyResource::make($this->whenLoaded('processoPai')),
+            'processos_filho_count' => $this->whenCounted('processosFilho'),
+            'solicitacoes_count' => $this->whenCounted('solicitacoes'),
+            'links' => [
+                'view_or_update' => $this->when(
+                    $request->user()->can(Policy::ViewOrUpdate->value, Processo::class),
+                    fn () => route('cadastro.processo.edit', $this->id),
+                ),
+                'delete' => $this->when(
+                    $request->user()->can(Policy::Delete->value, $this->resource),
+                    fn () => route('cadastro.processo.destroy', $this->id),
+                ),
+            ],
+        ];
+    }
+}
---
+++ tests/Feature/App/Http/Resources/Processo/ProcessoResourceTest.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://inertiajs.com/testing
+ */
+
+use App\Http\Resources\Processo\ProcessoResource;
+use App\Models\Permissao;
+use App\Models\Processo;
+use Database\Seeders\PerfilSeeder;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+    login();
+
+    $this->processo = Processo::factory()->for(Processo::factory(), 'processoPai')->create();
+
+    $this->processo_api = [
+        'id' => $this->processo->id,
+        'numero' => $this->processo->numero,
+        'numero_antigo' => $this->processo->numero_antigo,
+        'arquivado_em' => $this->processo->arquivado_em,
+        'guarda_permanente' => $this->processo->guarda_permanente ? __('Sim') : __('Não'),
+        'qtd_volumes' => $this->processo->qtd_volumes,
+        'volume_caixa_id' => $this->processo->volume_caixa_id,
+        'processo_pai_id' => $this->processo->processo_pai_id,
+    ];
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Caminho feliz
+test('retorna os campos principais e as rotas autorizadas do modelo', function () {
+    concederPermissao([Permissao::PROCESSO_DELETE, Permissao::PROCESSO_VIEW]);
+
+    $resource = ProcessoResource::make($this->processo);
+
+    expect($resource->response(request())->getData(true))->toBe([
+        'data' => $this->processo_api
+            + [
+                'links' => [
+                    'view_or_update' => route('cadastro.processo.edit', $this->processo),
+                    'delete' => route('cadastro.processo.destroy', $this->processo),
+                ],
+            ],
+    ]);
+});
+
+test('retorna o volume da caixa pai e o processo pai se houver o eager load da propriedade', function () {
+    $resource = ProcessoResource::make($this->processo->load(['volumeCaixa', 'processoPai']));
+
+    $processo_pai_api = [
+        'id' => $this->processo->processoPai->id,
+        'numero' => $this->processo->processoPai->numero,
+        'numero_antigo' => $this->processo->processoPai->numero_antigo,
+        'arquivado_em' => $this->processo->processoPai->arquivado_em,
+        'guarda_permanente' => $this->processo->processoPai->guarda_permanente ? __('Sim') : __('Não'),
+        'qtd_volumes' => $this->processo->processoPai->qtd_volumes,
+        'volume_caixa_id' => $this->processo->processoPai->volume_caixa_id,
+        'processo_pai_id' => $this->processo->processoPai->processo_pai_id,
+    ];
+
+    expect($resource->response(request())->getData(true))->toBe([
+        'data' => $this->processo_api
+            + ['volume_caixa' => $this->processo->volumeCaixa->only(['id', 'numero', 'caixa_id'])]
+            + ['processo_pai' => $processo_pai_api]
+            + ['links' => []],
+    ]);
+});
+
+test('retorna a quantidade de filhos se houver o eager load da propriedade', function () {
+    $resource = ProcessoResource::make($this->processo->loadCount(['processosFilho', 'solicitacoes']));
+
+    expect($resource->response(request())->getData(true))->toBe([
+        'data' => $this->processo_api
+            + $this->processo->only('processos_filho_count')
+            + $this->processo->only('solicitacoes_count')
+            + ['links' => []],
+    ]);
+});
+
+test('retorna apenas os campos principais se não houver rota autorizada para o modelo', function () {
+    $resource = ProcessoResource::make($this->processo);
+
+    expect($resource->response(request())->getData(true))->toBe([
+        'data' => $this->processo_api
+            + ['links' => []],
+    ]);
+});
