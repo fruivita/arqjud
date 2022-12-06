3377febc289c2a2ec0d3272daecb3ffffda374a3
FCNO <fcno.dev@gmail.com>
2022-12-06 13:37:14 -0300

    feat(view): add componente Clipboard
    Permite a cópia para o componente clipboard do navegador (ctrl + c) do
    texto copiável.

---
+++ resources/js/Shared/Misc/Clipboard.vue
@@ -0,0 +1,38 @@
+<!--
+    Componente para cópia de valores para o clipboard.
+
+    Útil para que o usúario possa clicar no botão do componente e seu conteúdo
+    ser transferido para a memória, ou seja, um ctrl + c automatizado.
+
+    @link https://vuejs.org/guide/introduction.html
+    @link https://tailwindcss.com/docs
+    @link https://inertiajs.com/
+ -->
+
+<script setup>
+import Icone from '@/Shared/Icones/Icone.vue';
+import { useClipboard } from '@vueuse/core';
+
+const props = defineProps({
+    copiavel: { type: String },
+});
+
+const { copy, copied, isSupported } = useClipboard({
+    source: props.copiavel,
+});
+</script>
+
+<template>
+    <div class="flex items-center space-y-1">
+        <span>{{ copiavel }}</span>
+
+        <button
+            v-if="isSupported"
+            :disabled="copied"
+            @click="copy()"
+            class="rounded p-1 opacity-50 ring-primaria-500 transition duration-150 hover:opacity-100 dark:ring-secundaria-500"
+        >
+            <Icone :nome="copied ? 'check-circle' : 'clipboard'" class="h-5 w-5" />
+        </button>
+    </div>
+</template>
---
+++ resources/js/Shared/Misc/__tests__/Clipboard.test.js
@@ -0,0 +1,60 @@
+/**
+ * Testes para o componente Clipboard.
+ *
+ * @link https://vuejs.org/guide/scaling-up/testing.html
+ * @link https://test-utils.vuejs.org/guide/
+ * @link https://vitest.dev/
+ */
+
+import Clipboard from '@/Shared/Misc/Clipboard.vue';
+import { mount } from '@vue/test-utils';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
+
+let mountFunction;
+
+let clipboardContents = '';
+
+beforeEach(() => {
+    Object.assign(navigator, {
+        clipboard: {
+            writeText: vi.fn((text) => {
+                clipboardContents = text;
+            }),
+            readText: vi.fn(() => clipboardContents),
+        },
+    });
+
+    mountFunction = (options = {}) => {
+        return mount(Clipboard, { ...options });
+    };
+});
+
+describe('Clipboard', () => {
+    test('propriedades do componente estão definidas', () => {
+        expect(Clipboard.props).toMatchObject({
+            copiavel: { type: String },
+        });
+    });
+
+    test('renderiza o componente default respeitando o snapshot', () => {
+        expect(
+            mountFunction({
+                props: { copiavel: 'foo' },
+            }).html()
+        ).toMatchSnapshot();
+    });
+
+    test('copia o conteúdo para o Clipboard ao clicar no botão', async () => {
+        const wrapper = mountFunction({
+            props: { copiavel: 'foo' },
+        });
+
+        expect(clipboardContents).toBe('');
+
+        await wrapper.get('button').trigger('click');
+
+        expect(clipboardContents).toBe('foo');
+
+        clipboardContents = '';
+    });
+});
