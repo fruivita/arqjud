b2345b5647008569562aa61e8a5e1682ec96c075
FCNO <fcno.dev@gmail.com>
2022-12-06 00:24:56 -0300

    feat(core): add cast NumeroProcesso
    Aplica máscara no get e remove no set do número do processo.

---
+++ app/Casts/NumeroProcesso.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Casts;
+
+use App\Models\Processo;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+/**
+ * @see https://laravel.com/docs/9.x/eloquent-mutators
+ */
+class NumeroProcesso implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * Aplica máscara o número do processo.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return string|null
+     */
+    public function get($model, string $key, $value, array $attributes)
+    {
+        return Processo::aplicarMascaraProcesso($value);
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * Remove eventual máscara do número do processo.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return string|null
+     */
+    public function set($model, string $key, $value, array $attributes)
+    {
+        return apenasNumeros($value);
+    }
+}
