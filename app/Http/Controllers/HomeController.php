e6f599b216a5d93b1a2287e70a4e5b92785b0305
FCNO <fcno.dev@gmail.com>
2022-12-11 23:39:48 -0300

    feat(controller): add HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\Policy;
+use App\Http\Requests\ShowProcessoHomeRequest;
+use App\Http\Resources\Processo\ProcessoOnlyResource;
+use App\Http\Resources\Solicitacao\CounterResource;
+use App\Models\Processo;
+use App\Models\Solicitacao;
+use Illuminate\Support\Collection;
 use Inertia\Inertia;
 
 /**
@@ -13,10 +20,67 @@ class HomeController extends Controller
     /**
      * Display the specified resource.
      *
+     * @param \App\Http\Requests\ShowProcessoHomeRequest  $request
      * @return \Inertia\Response
      */
-    public function show()
+    public function show(ShowProcessoHomeRequest $request)
     {
-        return Inertia::render('Home/HomeProcesso');
+        return (auth()->user()->can(Policy::View->value, Processo::class))
+            ? $this->renderHomeProcesso($request->input('termo'))
+            : $this->renderHome();
+    }
+
+    /**
+     * Renderiza a página home para pesquisa de processo.
+     *
+     * @param  string|null  $processo número do processo
+     * @return \Inertia\Response
+     */
+    private function renderHomeProcesso(string $processo = null)
+    {
+        return Inertia::render('Home/HomeProcesso', [
+            'processo' => fn () => ProcessoOnlyResource::make(
+                $processo
+                    ? Processo::with([
+                        'solicitacoesAtivas.solicitante',
+                        'solicitacoesAtivas.recebedor',
+                        'solicitacoesAtivas.remetente',
+                        'solicitacoesAtivas.rearquivador',
+                        'solicitacoesAtivas.lotacaoDestinataria',
+                        'volumeCaixa.caixa.prateleira.estante.sala.andar.predio.localidade',
+                        'volumeCaixa.caixa.localidadeCriadora',
+                        'processoPai',
+                    ])->where('numero', $processo)
+                    ->orWhere('numero_antigo', $processo)
+                    ->first()
+                    : null
+            ),
+            'links' => fn () => ['search' => route('home.show')],
+        ]);
+    }
+
+    /**
+     * Renderiza a página home com dados sobre a lotação do usuário autenticado.
+     *
+     * @return \Inertia\Response
+     */
+    private function renderHome()
+    {
+        return Inertia::render('Home/Home', [
+            'solicitacoes' => fn () => CounterResource::make(
+                Solicitacao::countAll()
+                    ->where('lotacao_destinataria_id', auth()->user()->lotacao_id)
+                    ->toBase()
+                    ->first()
+            )->additional([
+                'links' => collect()
+                    ->when(auth()->user()->can(Policy::ExternoViewAny->value, Solicitacao::class), function (Collection $collection) {
+                        return $collection->put('view_any', route('solicitacao.index'));
+                    })
+                    ->when(auth()->user()->can(Policy::ExternoCreate->value, Solicitacao::class), function (Collection $collection) {
+                        return $collection->put('create', route('solicitacao.create'));
+                    })->toArray()
+            ])
+        ]);
     }
 }
---
+++ tests/Feature/App/Http/Controllers/Cadastro/HomeControllerTest.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://inertiajs.com/testing
+ * @see https://hofmannsven.com/2020/testing-laravel-form-requests
+ * @see https://github.com/jasonmccreary/laravel-test-assertions
+ */
+
+use App\Models\Permissao;
+use App\Http\Controllers\HomeController;
+use App\Http\Requests\ShowProcessoHomeRequest;
+use App\Models\Processo;
+use App\Models\Solicitacao;
+use Database\Seeders\PerfilSeeder;
+use Inertia\Testing\AssertableInertia as Assert;
+use function Pest\Laravel\get;
+use function Pest\Laravel\post;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $this->usuario = login();
+});
+
+// Autorização
+test('usuário sem autorização não consegue pesquisar processos na página home', function () {
+    post(route('home.show', ['termo' => 'abc']))->assertForbidden();
+});
+
+// Caminho feliz
+test('página home dispensa autorização específica', function () {
+    get(route('home.show'))->assertOk();
+});
+
+test('action do controller usa o form request', function (string $action, string $request) {
+    $this->assertActionUsesFormRequest(
+        HomeController::class,
+        $action,
+        $request
+    );
+})->with([
+    ['show', ShowProcessoHomeRequest::class],
+]);
+
+test('action show compartilha os dados esperados com a view/componente correto para usuários com autorização específica', function () {
+    Solicitacao::factory()->solicitada()->create(['lotacao_destinataria_id' => $this->usuario->lotacao_id]);
+    Solicitacao::factory(2)->entregue()->create(['lotacao_destinataria_id' => $this->usuario->lotacao_id]);
+    Solicitacao::factory(4)->devolvida()->create(['lotacao_destinataria_id' => $this->usuario->lotacao_id]);
+    Solicitacao::factory(3)->solicitada()->create();
+
+    concederPermissao(Permissao::SOLICITACAO_EXTERNA_VIEW_ANY);
+    concederPermissao(Permissao::SOLICITACAO_EXTERNA_CREATE);
+
+    get(route('home.show'))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Home/Home')
+                ->whereAll([
+                    'solicitacoes' => [
+                        'data' => [
+                            'solicitadas' => 1,
+                            'entregues' => 2,
+                            'devolvidas' => 4,
+                        ],
+                        'links' => [
+                            'create' => route('solicitacao.create'),
+                            'view_any' => route('solicitacao.index'),
+                        ]
+                    ],
+                ])
+        );
+});
+
+test('action show compartilha os dados esperados com a view/componente correto para usuários sem autorização específica', function () {
+    Solicitacao::factory()->solicitada()->create(['lotacao_destinataria_id' => $this->usuario->lotacao_id]);
+    Solicitacao::factory(2)->entregue()->create(['lotacao_destinataria_id' => $this->usuario->lotacao_id]);
+    Solicitacao::factory(4)->devolvida()->create(['lotacao_destinataria_id' => $this->usuario->lotacao_id]);
+    Solicitacao::factory(3)->solicitada()->create();
+
+    get(route('home.show'))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Home/Home')
+                ->whereAll([
+                    'solicitacoes' => [
+                        'data' => [
+                            'solicitadas' => 1,
+                            'entregues' => 2,
+                            'devolvidas' => 4,
+                        ],
+                        'links' => []
+                    ],
+                ])
+        );
+});
+
+test('action show compartilha os dados esperados com view/componente diverso para usuários com autorização específica', function () {
+    concederPermissao(Permissao::PROCESSO_VIEW);
+
+    get(route('home.show'))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Home/HomeProcesso')
+                ->has('processo')
+                ->where('links.search', route('home.show'))
+        );
+});
+
+test('pesquisa e retorna o processo detalhado e sua solicitação ativa', function () {
+    concederPermissao(Permissao::PROCESSO_VIEW);
+
+    $processo = Processo::factory()->create(['numero' => '02393484420224003909']);
+    $Solicitacao = Solicitacao::factory()->for($processo, 'processo')->solicitada()->create();
+    Solicitacao::factory()->create();
+
+    post(route('home.show', ['termo' => '02393484420224003909']))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Home/HomeProcesso')
+                ->whereAll([
+                    'processo.data.id' => $processo->id,
+                    'processo.data.solicitacao_ativa.0.id' => $Solicitacao->id
+                ])
+        );
+});
