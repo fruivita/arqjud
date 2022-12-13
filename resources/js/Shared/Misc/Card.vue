878a91ad94a026f4bf53ca972ba9b673b3bf26d0
FCNO <fcno.dev@gmail.com>
2022-12-12 21:16:08 -0300

    feat(view): add componente Card.vue

---
+++ resources/js/Shared/Misc/Card.vue
@@ -0,0 +1,45 @@
+<!--
+    Container para os cards de visualização de informação.
+
+    @link https://vuejs.org/guide/introduction.html
+    @link https://tailwindcss.com/docs
+    @link https://inertiajs.com/
+ -->
+
+<script setup>
+const props = defineProps({
+    titulo: { type: String, required: true },
+    texto: { type: String, required: true },
+    especie: {
+        type: String,
+        default: 'info',
+        validator(value) {
+            return ['info', 'alerta', 'perigo'].includes(value);
+        },
+    },
+});
+</script>
+
+<template>
+    <div
+        :class="{
+            'bg-green-500 p-3 text-green-50': especie === 'info',
+            'bg-yellow-500 p-3 text-yellow-50': especie === 'alerta',
+            'bg-red-500 p-3 text-red-50': especie === 'perigo',
+        }"
+        class="flex flex-col items-center justify-center space-y-3 rounded-lg"
+    >
+        <h3 class="font-mono text-2xl font-bold">{{ titulo }}</h3>
+
+        <p
+            :class="{
+                'bg-green-700': especie === 'info',
+                'bg-yellow-700': especie === 'alerta',
+                'bg-red-700': especie === 'perigo',
+            }"
+            class="rounded-full px-3 text-5xl"
+        >
+            {{ texto }}
+        </p>
+    </div>
+</template>
---
+++ resources/js/Shared/Misc/__tests__/Card.test.js
@@ -0,0 +1,50 @@
+/**
+ * Testes para o componente Card.
+ *
+ * @link https://vuejs.org/guide/scaling-up/testing.html
+ * @link https://test-utils.vuejs.org/guide/
+ * @link https://vitest.dev/
+ */
+
+import Card from '@/Shared/Misc/Card.vue';
+import { mount } from '@vue/test-utils';
+import { beforeEach, describe, expect, test } from 'vitest';
+
+let mountFunction;
+
+beforeEach(() => {
+    mountFunction = (options = {}) => {
+        return mount(Card, { ...options });
+    };
+});
+
+describe('Card', () => {
+    // Caminho feliz
+    const especies = ['info', 'alerta', 'perigo'];
+
+    test('aceita apenas as espécies definidas', () => {
+        const validator = Card.props.especie.validator;
+
+        especies.forEach((especie) => expect(validator(especie)).toBe(true));
+        expect(validator('foo')).toBe(false);
+    });
+
+    test('propriedades do componente estão definidas', () => {
+        expect(Card.props).toMatchObject({
+            titulo: { type: String, required: true },
+            especie: { type: String, default: 'info' },
+            texto: { type: String, required: true },
+        });
+    });
+
+    test.each(especies)(
+        'renderiza o componente das espécies específicas respeitando o snapshot',
+        (especie) => {
+            expect(
+                mountFunction({
+                    props: { titulo: 'foo', texto: 'bar', especie: especie },
+                }).html()
+            ).toMatchSnapshot();
+        }
+    );
+});
