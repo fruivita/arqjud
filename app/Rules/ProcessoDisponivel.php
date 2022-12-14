5a495e67e2cce30298fd0be38054e0142d88c0b6
FCNO <fcno.dev@gmail.com>
2022-12-14 03:01:52 -0300

    feat(validation): add ProcessoDisponivel rule

---
+++ app/Rules/ProcessoDisponivel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Processo;
+use Illuminate\Contracts\Validation\InvokableRule;
+
+/**
+ * Verifica se o processo informado está disponível para solicitação, ou seja,
+ * se não possui nenhuma solicitação ativa (solicitado ou entregue).
+ *
+ * @see https://laravel.com/docs/9.x/validation#custom-validation-rules
+ */
+class ProcessoDisponivel implements InvokableRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value número do processo com ou sem a máscara
+     * @param  \Closure  $fail
+     * @return void
+     */
+    public function __invoke($attribute, $value, $fail)
+    {
+        $processo = Processo::doesntHave('solicitacoesAtivas')
+            ->firstWhere('numero', apenasNumeros($value));
+
+        if (!$processo) {
+            $fail('validation.solicitacao.ativa')->translate();
+        }
+    }
+}
---
+++ tests/Feature/App/Rules/ProcessoDisponivelTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Models\Processo;
+use App\Models\Solicitacao;
+use App\Rules\ProcessoDisponivel;
+use Illuminate\Support\Facades\Validator;
+
+// Caminho feliz
+test('processo já solicitada está indisponível para nova solicitação', function () {
+    $processo = Processo::factory()->create();
+    Solicitacao::factory()->for($processo, 'processo')->solicitada()->create();
+
+    $validator = Validator::make(['numero' => $processo->numero], [
+        'numero' => [new ProcessoDisponivel()],
+    ]);
+
+    expect($validator->passes())->toBeFalse();
+});
+
+test('processo já entregue está indisponível para nova solicitação', function () {
+    $processo = Processo::factory()->create();
+    Solicitacao::factory()->for($processo, 'processo')->entregue()->create();
+
+    $validator = Validator::make(['numero' => $processo->numero], [
+        'numero' => [new ProcessoDisponivel()],
+    ]);
+
+    expect($validator->passes())->toBeFalse();
+});
+
+test('processo já devolvido está disponível para nova solicitação', function () {
+    $processo = Processo::factory()->create();
+    Solicitacao::factory()->for($processo, 'processo')->devolvida()->create();
+
+    $validator = Validator::make(['numero' => $processo->numero], [
+        'numero' => [new ProcessoDisponivel()],
+    ]);
+
+    expect($validator->passes())->toBeTrue();
+});
+
+test('processo sem solicitação está disponível para solicitação', function (string $numero) {
+    Processo::factory()->create(['numero' => '02393484420224003909']);
+
+    $validator = Validator::make(['numero' => $numero], [
+        'numero' => [new ProcessoDisponivel()],
+    ]);
+
+    expect($validator->passes())->toBeTrue();
+})->with([
+    '02393484420224003909',
+    '0239348-44.2022.4.00.3909', // máscara é irrelevante
+]);
