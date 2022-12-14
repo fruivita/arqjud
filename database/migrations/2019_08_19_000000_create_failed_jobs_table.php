d584d276c4dd52e1b0d8857d3cf88d8643206b66
FCNO <fcno.dev@gmail.com>
2022-12-14 02:38:49 -0300

    docs(database): add php docblock

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * @see https://laravel.com/docs/9.x/queues#dealing-with-failed-jobs
+ */
 return new class extends Migration
 {
     /**
