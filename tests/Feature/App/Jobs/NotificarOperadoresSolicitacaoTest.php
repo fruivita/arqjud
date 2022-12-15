d6480947aed1887f3196eb196b0124c107bb6195
FCNO <fcno.dev@gmail.com>
2022-12-15 03:10:32 -0300

    refactor: move parte comum para o before

--- tests/Feature/App/Jobs/NotificarOperadoresSolicitacaoTest.php
+++ tests/Feature/App/Jobs/NotificarOperadoresSolicitacaoTest.php
@@ -18,50 +18,41 @@ use function Spatie\PestPluginTestTime\testTime;
 
 beforeEach(function () {
     $this->seed([PerfilSeeder::class]);
-
     testTime()->freeze();
+
+    $this->perfil_operador = Perfil::firstWhere('slug', Perfil::OPERADOR);
+    $this->operadores = Usuario::factory(3)->for($this->perfil_operador, 'perfil')->create();
+
+    $this->solicitacao = new \stdClass();
+    $this->solicitacao->processos = ['1111', '2222'];
+    $this->solicitacao->solicitante = Usuario::factory()->comNome()->create();
 });
 
 // Caminho feliz
 test('job NotificarOperadoresSolicitacao envia notificação a todos os usuários de perfil operador', function () {
-    $perfil_operador = Perfil::firstWhere('slug', Perfil::OPERADOR);
-
     $nao_operadores = Usuario::factory(2)->create();
-    $operadores = Usuario::factory(3)->for($perfil_operador, 'perfil')->create();
-
-    $solicitacao = new \stdClass();
-    $solicitacao->processos = ['1111', '2222'];
-    $solicitacao->solicitante = Usuario::factory()->comNome()->create();
 
     Notification::fake();
 
-    NotificarOperadoresSolicitacao::dispatchSync($solicitacao);
+    NotificarOperadoresSolicitacao::dispatchSync($this->solicitacao);
 
-    Notification::assertSentTo($operadores, ProcessoSolicitado::class);
+    Notification::assertSentTo($this->operadores, ProcessoSolicitado::class);
     Notification::assertNotSentTo($nao_operadores, ProcessoSolicitado::class);
 });
 
 test('job NotificarOperadoresSolicitacao cria a notificação com todos os parâmetros e canal esperados', function () {
-    $perfil_operador = Perfil::firstWhere('slug', Perfil::OPERADOR);
-
-    $operadores = Usuario::factory(3)->for($perfil_operador, 'perfil')->create();
-
-    $solicitacao = new \stdClass();
-    $solicitacao->processos = ['1111', '2222'];
-    $solicitacao->solicitante = Usuario::factory()->comNome()->create();
-
     Notification::fake();
 
-    NotificarOperadoresSolicitacao::dispatchSync($solicitacao);
+    NotificarOperadoresSolicitacao::dispatchSync($this->solicitacao);
 
     Notification::assertSentTo(
-        $operadores,
+        $this->operadores,
         ProcessoSolicitado::class,
-        function (ProcessoSolicitado $notification, $channels) use ($solicitacao) {
+        function (ProcessoSolicitado $notification, $channels) {
             expect($notification->toArray(null))->toBe([
-                'processos' => $solicitacao->processos,
-                'solicitante' => $solicitacao->solicitante->nome,
-                'lotacao_destinataria' => $solicitacao->solicitante->lotacao->nome,
+                'processos' => $this->solicitacao->processos,
+                'solicitante' => $this->solicitacao->solicitante->nome,
+                'lotacao_destinataria' => $this->solicitacao->solicitante->lotacao->nome,
                 'solicitada_em' => now()->tz(config('app.tz'))->format('d-m-Y H:i:s'),
                 'url' => 'rota',
             ])->and($channels)->toBe(['mail']);
@@ -72,19 +63,11 @@ test('job NotificarOperadoresSolicitacao cria a notificação com todos os parâ
 });
 
 test('job NotificarOperadoresSolicitacao envia para a queue de prioridade baixa a execução da notificação', function () {
-    $operador = Perfil::firstWhere('slug', Perfil::OPERADOR);
-
-    Usuario::factory(3)->for($operador, 'perfil')->create();
-
-    $solicitacao = new \stdClass();
-    $solicitacao->processos = ['1111', '2222'];
-    $solicitacao->solicitante = Usuario::factory()->create();
-
     Queue::fake()->except([
         NotificarOperadoresSolicitacao::class,
     ]);
 
-    NotificarOperadoresSolicitacao::dispatchSync($solicitacao);
+    NotificarOperadoresSolicitacao::dispatchSync($this->solicitacao);
 
     Queue::assertPushedOn(
         EQueue::Baixa->value,
