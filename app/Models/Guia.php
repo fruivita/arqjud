7321b404bb1208682f2f95a1c293bd9c9a8f9b48
FCNO <fcno.dev@gmail.com>
2022-12-17 19:40:38 -0300

    refactor(model): add próximo número da guia e casts

--- app/Models/Guia.php
+++ app/Models/Guia.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Casts\AsCollection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -22,9 +23,10 @@ class Guia extends Model
      * {@inheritdoc}
      */
     protected $casts = [
-        'remetente' => AsCollection::class,
-        'recebedor' => AsCollection::class,
-        'lotacao_destinataria' => AsCollection::class,
+        'gerada_em' => 'datetime',
+        'remetente' => AsArrayObject::class,
+        'recebedor' => AsArrayObject::class,
+        'lotacao_destinataria' => AsArrayObject::class,
         'processos' => AsCollection::class,
     ];
 
@@ -38,5 +40,16 @@ class Guia extends Model
     public function solicitacoes()
     {
         return $this->hasMany(Solicitacao::class, 'guia_id', 'id');
+    }
+
+    /**
+     * Próximo número da guia disponível para o ano informado ou para o atual.
+     *
+     * @param  int|null  $ano
+     * @return int
+     */
+    public static function proximoNumero(int $ano = null)
+    {
+        return intval(Guia::where('ano', $ano ?? now()->year)->max('numero')) + 1;
     }
 }
---
+++ tests/Feature/App/Models/GuiaTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Models\Guia;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Str;
+
+// Exceptions
+test('lança exception ao tentar criar guias duplicadas, isto é, com mesmo ano e número', function () {
+    expect(
+        fn () => Guia::factory(2)->create([
+            'numero' => 100,
+            'ano' => 2020,
+        ])
+    )->toThrow(QueryException::class, 'Duplicate entry');
+});
+
+test('lança exception ao tentar criar guia com campo inválido', function ($campo, $valor, $mensagem) {
+    expect(
+        fn () => Guia::factory()->create([$campo => $valor])
+    )->toThrow(QueryException::class, $mensagem);
+})->with([
+    ['numero',    -1,               'Out of range'],             // min 0
+    ['numero',    4294967296,       'Out of range'],             // max 4294967295
+    ['numero',    'foo',            'Incorrect integer value'],  // não conversível em inteiro
+    ['ano',       -1,               'Out of range value'],       // min 0
+    ['ano',       65536,            'Out of range value'],       // max 65536
+    ['ano',       'foo',            'Incorrect integer value'],  // não conversível em inteiro
+    ['descricao', Str::random(256), 'Data too long for column'], // máximo 255 caracteres
+]);
+
+// Caminho feliz
+test('aceita campos em seus tamanhos máximos', function () {
+    Guia::factory()->create([
+        'numero' => 4294967295,
+        'ano' => 65535,
+        'descricao' => Str::random(255),
+    ]);
+
+    expect(Guia::count())->toBe(1);
+});
+
+test('campos opcionais estão definidos', function () {
+    Guia::factory()->create([
+        'descricao' => null,
+    ]);
+
+    expect(Guia::count())->toBe(1);
+});
+
+test('uma guia refere-se a muitas solicitações de processo', function () {
+    Guia::factory()->hasSolicitacoes(3)->create();
+
+    $guia = Guia::with('solicitacoes')->first();
+
+    expect($guia->solicitacoes)->toHaveCount(3);
+});
+
+test('gera o próximo número da guia (MAX + 1) de acordo com o ano informado', function () {
+    Guia::factory()->create(['numero' => 100, 'ano' => 2020]);
+    Guia::factory()->create(['numero' => 200, 'ano' => now()->year]);
+
+    expect(Guia::proximoNumero(2020))->toBe(101)
+        ->and(Guia::proximoNumero())->toBe(201)
+        ->and(Guia::proximoNumero(2000))->toBe(1);
+});
