6d7f826f17dbd08a768574a3a7f1117e739528bb
FCNO <fcno.dev@gmail.com>
2022-12-17 00:38:24 -0300

    feat(controller): add api ProcessoController

---
+++ app/Http/Controllers/Api/Processo/ProcessoController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api\Processo;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Processo\ShowProcessoRequest;
+use App\Http\Resources\Processo\ProcessoOnlyResource;
+use App\Models\Processo;
+
+class ProcessoController extends Controller
+{
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Http\Requests\Api\Processo\ShowProcessoRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(ShowProcessoRequest $request)
+    {
+        return [
+            'processo' => ProcessoOnlyResource::make(
+                Processo::where('numero', $request->input('numero'))->firstOrFail()
+            )
+        ];
+    }
+}
