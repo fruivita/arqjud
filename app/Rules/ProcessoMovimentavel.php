64e4fe4711ebdfa3d021de73f6ee54551dae5b8d
FCNO <fcno.dev@gmail.com>
2022-12-16 23:51:16 -0300

    feat(validation): add rule ProcessoMovimentavel

---
+++ app/Rules/ProcessoMovimentavel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Processo;
+use Illuminate\Contracts\Validation\InvokableRule;
+
+/**
+ * Verifica se o processo informado está disponível para ser movimentado, isto
+ * é, se ele está sob a guarda do arquivo.
+ *
+ * Processos com solicitação entregue não podem ser movimentados, pois estão
+ * sob a guarda de terceiros.
+ *
+ * @see https://laravel.com/docs/9.x/validation#custom-validation-rules
+ */
+class ProcessoMovimentavel implements InvokableRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value número do processo com ou sem a máscara
+     * @param  \Closure  $fail
+     * @return void
+     */
+    public function __invoke($attribute, $value, $fail)
+    {
+        $processo = Processo::doesntHave('solicitacoesEntregues')
+            ->firstWhere('numero', apenasNumeros($value));
+
+        if (!$processo) {
+            $fail('validation.movimentacao')->translate();
+        }
+    }
+}
---
+++ tests/Feature/App/Rules/ProcessoMovimentavelTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Models\Processo;
+use App\Models\Solicitacao;
+use App\Rules\ProcessoMovimentavel;
+use Illuminate\Support\Facades\Validator;
+
+// Caminho feliz
+test('processo solicitado pode ser movimentado, pois localizado dentro do arquivo', function () {
+    $processo = Processo::factory()->create();
+    Solicitacao::factory()->for($processo, 'processo')->solicitada()->create();
+
+    $validator = Validator::make(['numero' => $processo->numero], [
+        'numero' => [new ProcessoMovimentavel()],
+    ]);
+
+    expect($validator->passes())->toBeTrue();
+});
+
+test('processo entregue NÃO pode ser movimentado, pois localizado fora do arquivo', function () {
+    $processo = Processo::factory()->create();
+    Solicitacao::factory()->for($processo, 'processo')->entregue()->create();
+
+    $validator = Validator::make(['numero' => $processo->numero], [
+        'numero' => [new ProcessoMovimentavel()],
+    ]);
+
+    expect($validator->passes())->toBeFalse();
+});
+
+test('processo devolvido pode ser movimentado, pois localizado dentro do arquivo', function () {
+    $processo = Processo::factory()->create();
+    Solicitacao::factory()->for($processo, 'processo')->devolvida()->create();
+
+    $validator = Validator::make(['numero' => $processo->numero], [
+        'numero' => [new ProcessoMovimentavel()],
+    ]);
+
+    expect($validator->passes())->toBeTrue();
+});
+
+test('processo sem solicitação pode ser movimentado, pois localizado dentro do arquivo', function (string $numero) {
+    Processo::factory()->create(['numero' => $numero]);
+
+    $validator = Validator::make(['numero' => $numero], [
+        'numero' => [new ProcessoMovimentavel()],
+    ]);
+
+    expect($validator->passes())->toBeTrue();
+})->with([
+    '02393484420224003909',
+    '0239348-44.2022.4.00.3909', // máscara é irrelevante
+]);
