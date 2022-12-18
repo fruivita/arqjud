edfa7f41c6fb494c84797965ea08b1985a02246a
FCNO <fcno.dev@gmail.com>
2022-12-18 00:13:05 -0300

    feat(view): add view para gerar guia em PDF

---
+++ resources/views/pdf/guia.blade.php
@@ -0,0 +1,56 @@
+{{--
+    Subview para exibir a guia em formato PDF.
+
+    @see https://laravel.com/docs/9.x/blade
+    @see https://github.com/barryvdh/laravel-dompdf
+    @see https://github.com/dompdf/dompdf
+--}}
+
+@extends('layouts.pdf')
+
+@section('conteudo')
+    <div class="content">
+        <p>{{ __('Número: :attribute', ['attribute' => "{$guia->numero}/{$guia->ano}"]) }}</p>
+
+        <p>{{ __('Remetente: :attribute', ['attribute' => $guia->remetente['nome'] ?? $guia->remetente['username']]) }}</p>
+
+        <p>{{ __('Lotação destinatária: :attribute1 - :attribute2', [ 'attribute1' => str($guia->lotacao_destinataria['sigla'])->upper(), 'attribute2' => $guia->lotacao_destinataria['nome']]) }}</p>
+
+        <table style="table-layout:fixed;">
+            <thead>
+                <tr>
+                    <th>{{ __('Processos') }}</th>
+
+                    <th>{{ __('Volumes') }}</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @forelse ($guia->processos ?? [] as $processo)
+                    <tr>
+                        <td>{{ mascara($processo['numero'], \App\Models\Processo::MASCARA_CNJ) }}</td>
+
+                        <td>{{ $processo['qtd_volumes'] }}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="2">{{ __('Nenhum registro encontrado!') }}</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+
+
+        <div style="margin-top: 2cm">
+            <hr>
+
+            <p style="text-align: center">
+                {{ __('Recebedor(a): :attribute', ['attribute' => $guia->recebedor['nome'] ?? $guia->recebedor['username']]) }}
+            </p>
+
+            <p style="text-align: center">
+                {{ dataCompleta($guia->gerada_em->tz(config('app.tz'))) }}
+            </p>
+        </div>
+    </div>
+@endsection
