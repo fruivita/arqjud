58b282ea76122b74abb741732fc191441915891a
FCNO <fcno.dev@gmail.com>
2022-12-18 00:12:20 -0300

    feat(controller): add GuiaController

---
+++ app/Http/Controllers/Atendimento/GuiaController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Atendimento;
+
+use App\Enums\Policy;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Guia\GuiaCollection;
+use App\Http\Resources\Guia\GuiaResource;
+use App\Http\Traits\ComPaginacaoEmCache;
+use App\Models\Guia;
+use App\Pipes\Guia\Order;
+use App\Pipes\Search;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Inertia\Inertia;
+use MichaelRubel\EnhancedPipeline\Pipeline;
+
+/**
+ * @see https://laravel.com/docs/controllers
+ * @see https://inertiajs.com/server-side-setup
+ */
+class GuiaController extends Controller
+{
+    use ComPaginacaoEmCache;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Inertia\Response
+     */
+    public function index()
+    {
+        return Inertia::render('Atendimento/Guia/Index', [
+            'guias' => fn () => GuiaCollection::make(
+                Pipeline::make()
+                    ->send(Guia::query())
+                    ->through([Order::class, Search::class])
+                    ->thenReturn()
+                    ->paginate($this->perPage())
+            )->additional(['meta' => [
+                'termo' => request()->query('termo'),
+                'order' => request()->query('order'),
+            ]])->preserveQuery(),
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Guia  $guia
+     * @return \Inertia\Response
+     */
+    public function show(Guia $guia)
+    {
+        $this->authorize(Policy::View->value, Guia::class);
+
+        return Inertia::render('Atendimento/Guia/Show', [
+            'guia' => GuiaResource::make($guia),
+        ]);
+    }
+
+    /**
+     * Display (PDF) the specified resource.
+     *
+     * @param  \App\Models\Guia  $guia
+     * @return \Illuminate\Http\Response
+     */
+    public function pdf(Guia $guia)
+    {
+        $this->authorize(Policy::View->value, Guia::class);
+
+        $pdf = Pdf::loadView('pdf.guia', [
+            'cabecalho' => __('Guia de remessa de processos'),
+            'guia' => $guia,
+        ]);
+        $pdf->render();
+        injetarTotalPagina($pdf);
+
+        return $pdf->stream('guia.pdf');
+    }
+}
---
+++ tests/Feature/App/Http/Controllers/Atendimento/GuiaControllerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Http\Controllers\Atendimento\GuiaController;
+use App\Models\Guia;
+use App\Models\Permissao;
+use Database\Seeders\PerfilSeeder;
+use Inertia\Testing\AssertableInertia as Assert;
+use function Pest\Laravel\get;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $this->guia = Guia::factory()->create();
+
+    login();
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Autorização
+test('usuário sem permissão não consegue gerar a guia em PDF', function () {
+    get(route('atendimento.guia.pdf', ['guia' => $this->guia]))->assertForbidden();
+});
+
+// Caminho feliz
+test('action index compartilha os dados esperados com a view/componente correto', function (bool $permissao) {
+    Guia::factory(2)->create();
+
+    concederPermissao(Permissao::GUIA_VIEW_ANY);
+
+    get(route('atendimento.guia.index'))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Atendimento/Guia/Index')
+                ->has('guias.data', 3) // 2 + 1
+                ->has('guias.meta.termo')
+                ->has('guias.meta.order')
+        );
+})->with([
+    false,
+    true,
+]);
+
+test('action show compartilha os dados esperados com a view/componente correto', function () {
+    concederPermissao(Permissao::GUIA_VIEW);
+
+    get(route('atendimento.guia.show', $this->guia))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Atendimento/Guia/Show')
+                ->where('guia.data', guiaApi($this->guia) + ['links' => [
+                    'view' => route('atendimento.guia.show', $this->guia),
+                    'pdf' => route('atendimento.guia.pdf', $this->guia),
+                ]])
+        );
+});
+
+test('usuário com permissão consegue gerar a guia em PDF', function () {
+    concederPermissao(Permissao::GUIA_VIEW);
+
+    get(route('atendimento.guia.pdf', ['guia' => $this->guia]))
+        ->assertOk()
+        ->assertHeader('Content-Type', 'application/pdf')
+        ->assertHeader('Content-Disposition', 'inline; filename="guia.pdf"');
+});
+
+test('RemessaController usa trait', function () {
+    expect(
+        collect(class_uses(GuiaController::class))
+            ->has([
+                \App\Http\Traits\ComPaginacaoEmCache::class,
+            ])
+    )->toBeTrue();
+});
