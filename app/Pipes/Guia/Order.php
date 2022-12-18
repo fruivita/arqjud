f5e8a70e0e85c09fcef79e22fcc7b5b018f7279c
FCNO <fcno.dev@gmail.com>
2022-12-17 21:30:29 -0300

    feat: add pipe Order para a guia

---
+++ app/Pipes/Guia/Order.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Pipes\Guia;
+
+use App\Pipes\OrderBase;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * @see https://www.youtube.com/watch?v=FByQN_d876c
+ */
+class Order extends OrderBase
+{
+    /**
+     * Aplica a ordenação pelo número da guia.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function numero(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('numero', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo ano da guia.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function ano(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('ano', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo data de geração da guia.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function geradaEm(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('gerada_em', $direcao);
+    }
+}
---
+++ tests/Feature/App/Pipes/Guia/OrderTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Models\Guia;
+use App\Pipes\Guia\Order;
+use MichaelRubel\EnhancedPipeline\Pipeline;
+use function Spatie\Snapshots\assertMatchesSnapshot;
+
+// Caminho feliz
+test('sem ordenação válida no request, ordena pelo ID desc', function (string $coluna, string $direcao) {
+    Guia::factory()->create(['id' => 1]);
+    Guia::factory()->create(['id' => 2]);
+
+    request()->merge(['order' => [$coluna, $direcao]]);
+
+    $guias = Pipeline::make()
+        ->send(Guia::query())
+        ->through([Order::class])
+        ->thenReturn()
+        ->pluck('id');
+
+    expect($guias->toArray())->toBe([2, 1]);
+})->with([
+    ['', ''],
+    ['foo', 'asc'],
+]);
+
+test('ordena pelo número', function () {
+    Guia::factory()->create(['id' => 1, 'numero' => 20]);
+    Guia::factory()->create(['id' => 2, 'numero' => 10]);
+
+    request()->merge(['order' => ['numero' => 'desc']]);
+
+    $guias = Pipeline::make()
+        ->send(Guia::query())
+        ->through([Order::class])
+        ->thenReturn()
+        ->pluck('id');
+
+    expect($guias->toArray())->toBe([1, 2]);
+});
+
+test('todos os métodos de ordenação disponíveis são acionados', function (string $campo) {
+    $this->partialMock(Order::class)
+        ->shouldAllowMockingProtectedMethods()
+        ->shouldReceive(str()->camel($campo))
+        ->withSomeOfArgs('desc')
+        ->once();
+
+    request()->merge(['order' => [$campo => 'desc']]);
+
+    Pipeline::make()
+        ->send(Guia::query())
+        ->through([Order::class])
+        ->thenReturn();
+})->with([
+    'numero',
+    'ano',
+    'gerada_em',
+]);
+
+test('todas as ordenações possíveis no request da guia', function () {
+    request()->merge(['order' => [
+        'numero' => 'asc',
+        'ano' => 'asc',
+        'gerada_em' => 'desc',
+    ]]);
+
+    $query = Pipeline::make()
+        ->send(Guia::query())
+        ->through([Order::class])
+        ->thenReturn();
+
+    assertMatchesSnapshot([$query->toSql(), $query->getBindings()]);
+});
