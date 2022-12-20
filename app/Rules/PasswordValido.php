13f866546cc1c2f1cff70c045d68f3547f8f351d
FCNO <fcno.dev@gmail.com>
2022-12-20 03:47:04 -0300

    feat(validation): add rule PasswordValido

---
+++ app/Rules/PasswordValido.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\InvokableRule;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Valida a senha do usuário informado.
+ *
+ * @see https://laravel.com/docs/9.x/validation#custom-validation-rules
+ */
+class PasswordValido implements InvokableRule
+{
+    /**
+     * @var string $username
+     */
+    private $username;
+
+    /**
+     * @param string|null $username
+     */
+    public function __construct(string $username = null)
+    {
+        $this->username = $username;
+    }
+    /**
+     * Run the validation rule.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value senha de rede do usuário
+     * @param  \Closure  $fail
+     * @return void
+     */
+    public function __invoke($attribute, $value, $fail)
+    {
+        $valido = Auth::validate([
+            'samaccountname' => $this->username,
+            'password' => $value,
+        ]);
+
+        if ($valido !== true) {
+            $fail('auth.password')->translate();
+        }
+    }
+}
---
+++ tests/Feature/App/Rules/PasswordValidoTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Rules\PasswordValido;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+// Caminho feliz
+test('identifica credenciais inválidas', function () {
+    Auth::partialMock()
+        ->shouldReceive('validate')
+        ->once()
+        ->andReturnFalse();
+
+    $validator = Validator::make(['password' => 'bar'], [
+        'password' => [new PasswordValido('foo')],
+    ]);
+
+    expect($validator->passes())->toBeFalse();
+});
+
+test('identifica credenciais válidas', function () {
+    Auth::partialMock()
+        ->shouldReceive('validate')
+        ->once()
+        ->andReturnTrue();
+
+    $validator = Validator::make(['password' => 'bar'], [
+        'password' => [new PasswordValido('foo')],
+    ]);
+
+    expect($validator->passes())->toBeTrue();
+});
+
+test('mensagem de falha de validação está definida', function () {
+    Auth::partialMock()
+        ->shouldReceive('validate')
+        ->once()
+        ->andReturnFalse();
+
+    $validator = Validator::make(['password' => 'bar'], [
+        'password' => [new PasswordValido('foo')],
+    ]);
+
+    expect($validator->passes())->toBeFalse()
+        ->and($validator->errors()->first())->toBe(__('auth.password'));
+});
