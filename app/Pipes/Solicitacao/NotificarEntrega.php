550493ef8f4e947ec8fbf50a400992fd447dcf1e
FCNO <fcno.dev@gmail.com>
2022-12-20 18:35:57 -0300

    feat(core): add pipe NotificarEntrega

---
+++ app/Pipes/Solicitacao/NotificarEntrega.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Pipes\Solicitacao;
+
+use App\Enums\Queue;
+use App\Jobs\NotificarEntrega as JobNotificarEntrega;
+
+/**
+ * @see https://www.youtube.com/watch?v=FByQN_d876c
+ */
+class NotificarEntrega
+{
+    /**
+     * Dispara o job responsável por notificar os usuários acerca da entrega de
+     * processos solicitados.
+     *
+     * @param  \stdClass  $entrega
+     * @param  \Closure  $next
+     * @return \stdClass
+     */
+    public function handle(\stdClass $entrega, \Closure $next)
+    {
+        JobNotificarEntrega::dispatch($entrega)->onQueue(Queue::Media->value);
+
+        return $next($entrega);
+    }
+}
---
+++ tests/Feature/App/Pipes/Solicitacao/NotificarEntregaTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Enums\Queue as EnumsQueue;
+use App\Jobs\NotificarEntrega as JobNotificarEntrega;
+use App\Models\Guia;
+use App\Models\Usuario;
+use App\Pipes\Solicitacao\NotificarEntrega;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Queue;
+use MichaelRubel\EnhancedPipeline\Pipeline;
+
+beforeEach(function () {
+    $this->entrega = new stdClass();
+    $this->entrega->recebedor = Usuario::factory()->create();
+    $this->entrega->guia = Guia::factory()->create();
+    $this->entrega->por_guia = true;
+    $this->entrega->email_terceiros = fake()->safeEmail();
+});
+
+// Caminho feliz
+test('pipe NotificarEntrega cria o job NotificarEntrega para notificar os envolvidos na entrega dos processos solicitados', function () {
+    Bus::fake();
+
+    Pipeline::make()
+        ->withTransaction()
+        ->send($this->entrega)
+        ->through([NotificarEntrega::class])
+        ->thenReturn();
+
+    Bus::assertNotDispatchedSync(JobNotificarEntrega::class, 1); // @phpstan-ignore-line
+});
+
+test('pipe NotificarEntrega envia o job para a querue de prioridade baixa', function () {
+    Queue::fake();
+
+    Pipeline::make()
+        ->withTransaction()
+        ->send($this->entrega)
+        ->through([NotificarEntrega::class])
+        ->thenReturn();
+
+    Queue::assertPushedOn(EnumsQueue::Media->value, JobNotificarEntrega::class);
+    Queue::assertPushed(JobNotificarEntrega::class, 1);
+});
