fbb2f7fbee57b31f4a41b18752ea17fc94e596b4
FCNO <fcno.dev@gmail.com>
2022-12-20 19:49:31 -0300

    feat(validation): add StoreEntregarProcessoRequest

---
+++ app/Http/Requests/Atendimento/StoreEntregarProcessoRequest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Requests\Atendimento;
+
+use App\Enums\Policy;
+use App\Models\Solicitacao;
+use App\Rules\RecebedorHabilitado;
+use App\Rules\PasswordValido;
+use App\Rules\SolicitacaoEntregavel;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+/**
+ * @see https://laravel.com/docs/9.x/validation#form-request-validation
+ */
+class StoreEntregarProcessoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->user()->can(Policy::Update->value, Solicitacao::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'recebedor' => [
+                'bail',
+                'required',
+                'string',
+                'between:1,20',
+                Rule::exists('usuarios', 'username'),
+                new RecebedorHabilitado(),
+            ],
+
+            'por_guia' => ['boolean'],
+
+            'password' => [
+                'bail',
+                Rule::excludeIf($this->por_guia === true),
+                'required',
+                'string',
+                'max:50',
+                new PasswordValido($this->recebedor),
+            ],
+
+            'solicitacoes.*' => [
+                'bail',
+                'required',
+                'integer',
+                Rule::exists('solicitacoes', 'id'),
+                new SolicitacaoEntregavel(),
+            ],
+
+            'email_terceiros.*' => ['nullable', 'email:strict'],
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'recebedor' => __('Recebedor'),
+            'por_guia' => __('Entrega por guia'),
+            'password' => __('Senha'),
+            'solicitacoes.*' => __('Solicitação'),
+            'email_terceiros.*' => __('Email'),
+        ];
+    }
+}
---
+++ tests/Feature/App/Http/Requests/Atendimento/StoreEntregarProcessoRequestTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://hofmannsven.com/2020/testing-laravel-form-requests
+ * @see https://github.com/jasonmccreary/laravel-test-assertions
+ */
+
+use App\Http\Requests\Atendimento\StoreEntregarProcessoRequest;
+use App\Models\Permissao;
+use App\Rules\PasswordValido;
+use App\Rules\RecebedorHabilitado;
+use App\Rules\SolicitacaoEntregavel;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Validation\Rule;
+
+beforeEach(function () {
+    $this->request = new StoreEntregarProcessoRequest();
+});
+
+// Autorização
+test('usuário sem autorização não cria o request', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    login();
+
+    expect($this->request->authorize())->toBeFalse();
+});
+
+// Caminho feliz
+test('rules estão definidas no form request', function () {
+    $this->request->por_guia = rand(0, 1) ? true : false;
+    $this->request->recebedor = 'foo';
+
+    $this->assertExactValidationRules([
+        'recebedor' => [
+            'bail',
+            'required',
+            'string',
+            'between:1,20',
+            Rule::exists('usuarios', 'username'),
+            new RecebedorHabilitado(),
+        ],
+
+        'por_guia' => ['boolean'],
+
+        'password' => [
+            'bail',
+            Rule::excludeIf($this->request->por_guia === true),
+            'required',
+            'string',
+            'max:50',
+            new PasswordValido($this->request->recebedor),
+        ],
+
+        'solicitacoes.*' => [
+            'bail',
+            'required',
+            'integer',
+            Rule::exists('solicitacoes', 'id'),
+            new SolicitacaoEntregavel(),
+        ],
+
+        'email_terceiros.*' => ['nullable', 'email:strict'],
+    ], $this->request->rules());
+});
+
+test('attributes estão definidas no form request', function () {
+    $this->assertExactValidationRules([
+        'recebedor' => __('Recebedor'),
+        'por_guia' => __('Entrega por guia'),
+        'password' => __('Senha'),
+        'solicitacoes.*' => __('Solicitação'),
+        'email_terceiros.*' => __('Email'),
+    ], $this->request->attributes());
+});
+
+test('usuário autorizado pode criar o request', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    login();
+
+    concederPermissao(Permissao::SOLICITACAO_UPDATE);
+
+    expect($this->request->authorize())->toBeTrue();
+});
