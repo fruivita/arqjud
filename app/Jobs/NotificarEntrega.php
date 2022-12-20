a75ffca1186dba6712f2bf485cabce1481c2637d
FCNO <fcno.dev@gmail.com>
2022-12-20 20:18:42 -0300

    feat(job): add job NotificarEntrega

---
+++ app/Jobs/NotificarEntrega.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Guia;
+use App\Models\Usuario;
+use App\Notifications\ProcessoEntregue;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Notification;
+
+/**
+ * Notifica os envolvidos na entrega de processos solicitados.
+ *
+ * @see https://laravel.com/docs/9.x/queues
+ */
+class NotificarEntrega implements ShouldQueue, ShouldBeUnique
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Número de tentativas de execução do job.
+     *
+     * @var int
+     */
+    public $tries = 5;
+
+    /**
+     * Número de segundos de espera antes de tentar executar novamente o job.
+     *
+     * @var int[]
+     */
+    public $backoff = [60, 300];
+
+    /**
+     * Usuário recebedor.
+     *
+     * @var \App\Models\Usuario
+     */
+    private Usuario $recebedor;
+
+    /**
+     * Guia de remessas de processo.
+     *
+     * @var \App\Models\Guia
+     */
+    private Guia $guia;
+
+    /**
+     * A entrega foi efetivada por meio de guia de remessa impressa?
+     *
+     * @var bool
+     */
+    private $por_guia;
+
+    /**
+     * Endereços de email de terceiros que devem ser notificados.
+     *
+     * @var string[]
+     */
+    private $email_terceiros;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param  \stdClass  $entrega
+     * @return void
+     */
+    public function __construct(\stdClass $entrega)
+    {
+        $this->recebedor = $entrega->recebedor;
+        $this->guia = $entrega->guia;
+        $this->por_guia = boolval($entrega->por_guia);
+        $this->email_terceiros = $entrega->email_terceiros;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Notification::send(
+            $this->recebedor,
+            new ProcessoEntregue(
+                $this->guia->paraHumano,
+                $this->guia->processos->toArray(),
+                data_get($this->guia->recebedor, 'nome') ?: data_get($this->guia->recebedor, 'username'),
+                $this->guia->lotacao_destinataria['nome'] ?: $this->guia->lotacao_destinataria['sigla'],
+                $this->guia->gerada_em->tz(config('app.tz'))->format('d-m-Y H:i:s'),
+                $this->por_guia,
+                route('solicitacao.index'),
+                $this->email_terceiros
+            )
+        );
+    }
+
+    /**
+     * Get o cache driver usado para determinar o lock para jobs únicos.
+     *
+     * @return \Illuminate\Contracts\Cache\Repository
+     */
+    public function uniqueVia()
+    {
+        return cache()->driver('database');
+    }
+}
---
+++ tests/Feature/App/Jobs/NotificarEntregaTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Enums\Queue as EQueue;
+use App\Jobs\NotificarEntrega;
+use App\Models\Guia;
+use App\Models\Perfil;
+use App\Models\Usuario;
+use App\Notifications\ProcessoEntregue;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Notifications\SendQueuedNotifications;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Queue;
+use function Spatie\PestPluginTestTime\testTime;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+    testTime()->freeze();
+
+    $this->entrega = new \stdClass();
+    $this->entrega->recebedor = Usuario::factory()->comNome()->create();
+    $this->entrega->guia = Guia::factory()->create();
+    $this->entrega->por_guia = true;
+    $this->entrega->email_terceiros = ['foo@bar.com', 'bar@taz.com'];
+});
+
+// Caminho feliz
+test('job NotificarEntrega envia notificação ao usuário recebedor', function () {
+    Notification::fake();
+
+    NotificarEntrega::dispatchSync($this->entrega);
+
+    Notification::assertTimesSent(1, ProcessoEntregue::class);
+    Notification::assertSentTo($this->entrega->recebedor, ProcessoEntregue::class);
+});
+
+test('job NotificarEntrega cria a notificação com todos os parâmetros e canal esperados', function () {
+    Notification::fake();
+
+    NotificarEntrega::dispatchSync($this->entrega);
+
+    Notification::assertSentTo(
+        $this->entrega->recebedor,
+        ProcessoEntregue::class,
+        function (ProcessoEntregue $notification, $channels) {
+            expect($notification->toArray(null))->toMatchArray([
+                'guia_numero' => $this->entrega->guia->paraHumano,
+                'processos' => $this->entrega->guia->processos->toArray(),
+                'recebedor' => $this->entrega->guia->recebedor['nome'],
+                'lotacao_destinataria' => $this->entrega->guia->lotacao_destinataria['nome'],
+                'entregue_em' => $this->entrega->guia->gerada_em->tz(config('app.tz'))->format('d-m-Y H:i:s'),
+                'por_guia' => $this->entrega->por_guia,
+                'url' => route('solicitacao.index'),
+                'email_terceiros' => $this->entrega->email_terceiros,
+            ])->and($channels)->toBe(['mail']);
+
+            return true;
+        }
+    );
+});
+
+test('job NotificarEntrega envia para a queue de prioridade baixa a execução da notificação', function () {
+    Queue::fake()->except([
+        NotificarEntrega::class,
+    ]);
+
+    NotificarEntrega::dispatchSync($this->entrega);
+
+    Queue::assertPushedOn(
+        EQueue::Baixa->value,
+        SendQueuedNotifications::class,
+        fn (SendQueuedNotifications $job) => $job->notification::class === ProcessoEntregue::class
+    );
+
+    Queue::assertPushed(SendQueuedNotifications::class, 1);
+});
