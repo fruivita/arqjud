b4a06a5a682a5a0254e69335337d6e62f9241b25
FCNO <fcno.dev@gmail.com>
2022-12-21 02:21:11 -0300

    feat(controller): add EntregarProcessoController

---
+++ app/Http/Controllers/Atendimento/EntregarProcessoController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Atendimento;
+
+use App\Enums\Policy;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Atendimento\StoreEntregarProcessoRequest;
+use App\Http\Traits\ComFeedback;
+use App\Models\Guia;
+use App\Models\Solicitacao;
+use App\Pipes\Solicitacao\EfetivarEntrega;
+use App\Pipes\Solicitacao\GerarGuiaRemessa;
+use App\Pipes\Solicitacao\NotificarEntrega;
+use Inertia\Inertia;
+use MichaelRubel\EnhancedPipeline\Pipeline;
+
+/**
+ * @see https://laravel.com/docs/controllers
+ * @see https://inertiajs.com/server-side-setup
+ */
+class EntregarProcessoController extends Controller
+{
+    use ComFeedback;
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Inertia\Response
+     */
+    public function create()
+    {
+        $this->authorize(Policy::Update->value, Solicitacao::class);
+
+        return Inertia::render('Atendimento/EntregarProcesso/Create', [
+            'links' => fn () => [
+                'solicitacoes' => route('api.solicitacao.recebedor.show'),
+                'imprimir_ultima_guia' => function () {
+                    $guia = Guia::select('id')->latest()->first();
+
+                    return ($guia)
+                        ? route('atendimento.guia.pdf', $guia->id)
+                        : null;
+                },
+                'entregar' => route('atendimento.entregar-processo.store'),
+            ],
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\Atendimento\StoreEntregarProcessoRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(StoreEntregarProcessoRequest $request)
+    {
+        $entrega = new \stdClass();
+        $entrega->recebedor = $request->input('recebedor');
+        $entrega->por_guia = $request->boolean('por_guia');
+        $entrega->solicitacoes = $request->input('solicitacoes');
+        $entrega->email_terceiros = $request->input('email_terceiros');
+
+        $salvo = Pipeline::make()
+            ->withTransaction()
+            ->send($entrega)
+            ->through([
+                GerarGuiaRemessa::class,
+                EfetivarEntrega::class,
+                NotificarEntrega::class,
+            ])->thenReturn();
+
+        return back()->with($this->feedback($salvo));
+    }
+}
---
+++ tests/Feature/App/Http/Controllers/Atendimento/EntregarProcessoControllerTest.php
@@ -0,0 +1,183 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://inertiajs.com/testing
+ * @see https://hofmannsven.com/2020/testing-laravel-form-requests
+ * @see https://github.com/jasonmccreary/laravel-test-assertions
+ */
+
+use App\Http\Controllers\Atendimento\EntregarProcessoController;
+use App\Http\Requests\Atendimento\StoreEntregarProcessoRequest;
+use App\Jobs\NotificarEntrega;
+use App\Jobs\NotificarOperadoresSolicitacao;
+use App\Models\Guia;
+use App\Models\Permissao;
+use App\Models\Processo;
+use App\Models\Solicitacao;
+use App\Models\Usuario;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Bus;
+use Inertia\Testing\AssertableInertia as Assert;
+use LdapRecord\Laravel\Testing\DirectoryEmulator;
+use LdapRecord\Models\ActiveDirectory\User;
+use Mockery\MockInterface;
+
+use function Pest\Laravel\delete;
+use function Pest\Laravel\get;
+use function Pest\Laravel\post;
+use function Spatie\PestPluginTestTime\testTime;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $this->usuario = login();
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Autorização
+test('usuário sem permissão não consegue exibir formulário de entrega de processos solicitados', function () {
+    get(route('atendimento.entregar-processo.create'))->assertForbidden();
+});
+
+// Caminho feliz
+test('action do controller usa o form request', function (string $action, string $request) {
+    $this->assertActionUsesFormRequest(
+        EntregarProcessoController::class,
+        $action,
+        $request
+    );
+})->with([
+    ['store', StoreEntregarProcessoRequest::class],
+]);
+
+test('action create compartilha os dados esperados com a view/componente correto', function () {
+    testTime()->freeze();
+    Guia::factory()->create();
+    concederPermissao(Permissao::SOLICITACAO_UPDATE);
+
+    testTime()->addMinute();
+    $guia = Guia::factory()->create();
+
+    get(route('atendimento.entregar-processo.create'))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Atendimento/EntregarProcesso/Create')
+                ->whereAll([
+                    'links' => [
+                        'solicitacoes' => route('api.solicitacao.recebedor.show'),
+                        'imprimir_ultima_guia' => route('atendimento.guia.pdf', $guia->id),
+                        'entregar' => route('atendimento.entregar-processo.store'),
+                    ]
+                ])
+        );
+});
+
+test('se não houver última guia, retonará null na rota para a impressão da última guia', function () {
+    concederPermissao(Permissao::SOLICITACAO_UPDATE);
+
+    get(route('atendimento.entregar-processo.create'))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page->where('links.imprimir_ultima_guia', null)
+        );
+});
+
+test('entrega de processo muda o status da solicitação de solicitada para entrega', function () {
+    concederPermissao(Permissao::SOLICITACAO_UPDATE);
+
+    $recebedor = Usuario::factory()->create();
+    $solicitacoes = Solicitacao::factory(2)->solicitada()->create(['lotacao_destinataria_id' => $recebedor->lotacao_id]);
+    Solicitacao::factory()->solicitada()->create();
+
+    post(route('atendimento.entregar-processo.store'), [
+        'recebedor' => $recebedor->username,
+        'por_guia' => true,
+        'solicitacoes' => $solicitacoes->pluck('id')->toArray(),
+        'email_terceiros' => [],
+    ])
+        ->assertRedirect()
+        ->assertSessionHas('feedback.sucesso');
+
+    expect(Solicitacao::solicitadas()->count())->toBe(1)
+        ->and(Solicitacao::entregues()->count())->toBe(2);
+});
+
+test('entrega de processo gera a guia de remessa dos processos solicitados', function () {
+    testTime()->freeze();
+
+    concederPermissao(Permissao::SOLICITACAO_UPDATE);
+
+    $recebedor = Usuario::factory()->create();
+    $solicitacoes = Solicitacao::factory(2)->solicitada()->create(['lotacao_destinataria_id' => $recebedor->lotacao_id]);
+    Solicitacao::factory()->solicitada()->create();
+
+    expect(Guia::count())->toBe(0);
+
+    post(route('atendimento.entregar-processo.store'), [
+        'recebedor' => $recebedor->username,
+        'por_guia' => true,
+        'solicitacoes' => $solicitacoes->pluck('id')->toArray(),
+        'email_terceiros' => [],
+    ])
+        ->assertRedirect()
+        ->assertSessionHas('feedback.sucesso');
+
+    $processos = $solicitacoes->map(function (Solicitacao $solicitacao) {
+        $solicitacao->loadMissing(['processo', 'solicitante']);
+        return [
+            'numero' => apenasNumeros($solicitacao->processo->numero),
+            'qtd_volumes' => $solicitacao->processo->qtd_volumes,
+            'solicitante' => [
+                'username' => $solicitacao->solicitante->username,
+                'nome' => $solicitacao->solicitante->nome,
+            ],
+        ];
+    });
+
+    $guia = Guia::first();
+
+    expect(Guia::count())->toBe(1)
+        ->and($guia->numero)->toBe(1)
+        ->and($guia->ano)->toBe(now()->year)
+        ->and($guia->gerada_em->toString())->toBe(now()->toString())
+        ->and($guia->remetente)->toMatchArray($this->usuario->only(['nome', 'username']))
+        ->and($guia->recebedor)->toMatchArray($recebedor->only(['nome', 'username']))
+        ->and($guia->lotacao_destinataria)->toMatchArray($recebedor->lotacao->only(['nome', 'sigla']))
+        ->and($guia->processos)->toMatchArray($processos);
+});
+
+test('dispara o job NotificarEntrega quando o usuário faz a entrega dos processos solicitados', function () {
+    Bus::fake();
+
+    concederPermissao(Permissao::SOLICITACAO_UPDATE);
+
+    $recebedor = Usuario::factory()->create();
+    $solicitacoes = Solicitacao::factory(2)->solicitada()->create(['lotacao_destinataria_id' => $recebedor->lotacao_id]);
+
+    post(route('atendimento.entregar-processo.store'), [
+        'recebedor' => $recebedor->username,
+        'por_guia' => true,
+        'solicitacoes' => $solicitacoes->pluck('id')->toArray(),
+        'email_terceiros' => [],
+    ])
+        ->assertRedirect()
+        ->assertSessionHas('feedback.sucesso');
+
+    Bus::assertNotDispatchedSync(NotificarEntrega::class);
+    Bus::assertDispatchedTimes(NotificarEntrega::class, 1);
+});
+
+test('EntregarProcessoController usa trait', function () {
+    expect(
+        collect(class_uses(EntregarProcessoController::class))
+            ->has([
+                \App\Http\Traits\ComFeedback::class,
+            ])
+    )->toBeTrue();
+});
