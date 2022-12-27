588f2015609b61c2d99775474ddbb816c8530997
FCNO <fcno.dev@gmail.com>
2022-12-27 16:34:20 -0300

    feat(job): add job NotificarSolicitanteSolicitacao

---
+++ app/Jobs/NotificarSolicitanteSolicitacao.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Lotacao;
+use App\Models\Usuario;
+use App\Notifications\ProcessoSolicitado;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Notification;
+
+/**
+ * Notifica o solicitante acerca de solicitação de processos feita em seu nome.
+ *
+ * @see https://laravel.com/docs/9.x/queues
+ */
+class NotificarSolicitanteSolicitacao implements ShouldQueue, ShouldBeUnique
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Número de tentativas de execução do job.
+     *
+     * @var int
+     */
+    public $tries = 5;
+
+    /**
+     * Número de segundos de espera antes de tentar executar novamente o job.
+     *
+     * @var int[]
+     */
+    public $backoff = [60, 300];
+
+    /**
+     * Número dos processos solicitados
+     *
+     * @var string[]
+     */
+    private $processos;
+
+    /**
+     * Usuário solicitante.
+     *
+     * @var \App\Models\Usuario
+     */
+    private Usuario $solicitante;
+
+    /**
+     * Destino dos processos solicitados.
+     *
+     * @var \App\Models\Lotaçcao
+     */
+    private Lotacao $destino;
+
+    /**
+     * Data e hora da solicitação
+     *
+     * @var \Illuminate\Support\Carbon
+     */
+    private Carbon $solicitada_em;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param  \stdClass  $solicitacao
+     * @return void
+     */
+    public function __construct(\stdClass $solicitacao)
+    {
+        $this->processos = $solicitacao->processos;
+        $this->solicitante = $solicitacao->solicitante->withoutRelations();
+        $this->destino = $solicitacao->destino->withoutRelations();
+        $this->solicitada_em = now();
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Notification::send(
+            $this->solicitante,
+            new ProcessoSolicitado(
+                $this->processos,
+                $this->solicitante->nome ?? $this->solicitante->username,
+                $this->destino->nome ?? $this->destino->sigla,
+                $this->solicitada_em->tz(config('app.tz'))->format('d-m-Y H:i:s'),
+                route('solicitacao.index')
+            )
+        );
+    }
+
+    /**
+     * Get o cache driver usado para determinar o lock para jobs únicos.
+     *
+     * @return \Illuminate\Contracts\Cache\Repository
+     */
+    public function uniqueVia()
+    {
+        return cache()->driver('database');
+    }
+}
---
+++ tests/Feature/App/Jobs/NotificarSolicitanteSolicitacaoTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Enums\Queue as EQueue;
+use App\Jobs\NotificarSolicitanteSolicitacao;
+use App\Models\Lotacao;
+use App\Models\Perfil;
+use App\Models\Usuario;
+use App\Notifications\ProcessoSolicitado;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Notifications\SendQueuedNotifications;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Queue;
+use function Spatie\PestPluginTestTime\testTime;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+    testTime()->freeze();
+
+    $this->solicitacao = new \stdClass();
+    $this->solicitacao->processos = ['1111', '2222'];
+    $this->solicitacao->solicitante = Usuario::factory()->comNome()->create();
+    $this->solicitacao->destino = Lotacao::factory()->create();
+});
+
+// Caminho feliz
+test('job NotificarSolicitanteSolicitacao envia notificação a ao solicitante', function () {
+    Notification::fake();
+
+    NotificarSolicitanteSolicitacao::dispatchSync($this->solicitacao);
+
+    Notification::assertSentTo($this->solicitacao->solicitante, ProcessoSolicitado::class);
+});
+
+test('job NotificarSolicitanteSolicitacao cria a notificação com todos os parâmetros e canal esperados', function () {
+    Notification::fake();
+
+    NotificarSolicitanteSolicitacao::dispatchSync($this->solicitacao);
+
+    Notification::assertSentTo(
+        $this->solicitacao->solicitante,
+        ProcessoSolicitado::class,
+        function (ProcessoSolicitado $notification, $channels) {
+            expect($notification->toArray(null))->toMatchArray([
+                'processos' => $this->solicitacao->processos,
+                'solicitante' => $this->solicitacao->solicitante->nome,
+                'lotacao_destinataria' => $this->solicitacao->destino->nome,
+                'solicitada_em' => now()->tz(config('app.tz'))->format('d-m-Y H:i:s'),
+                'url' => route('solicitacao.index'),
+            ])->and($channels)->toMatchArray(['mail']);
+
+            return true;
+        }
+    );
+});
+
+test('job NotificarSolicitanteSolicitacao envia para a queue de prioridade baixa a execução da notificação', function () {
+    Queue::fake()->except([
+        NotificarSolicitanteSolicitacao::class,
+    ]);
+
+    NotificarSolicitanteSolicitacao::dispatchSync($this->solicitacao);
+
+    Queue::assertPushedOn(
+        EQueue::Baixa->value,
+        SendQueuedNotifications::class,
+        fn (SendQueuedNotifications $job) => $job->notification::class === ProcessoSolicitado::class
+    );
+
+    Queue::assertPushed(SendQueuedNotifications::class, 1); // 1 por usuário
+});
