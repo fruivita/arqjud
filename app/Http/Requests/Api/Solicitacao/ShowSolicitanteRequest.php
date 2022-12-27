89b43f50e26074566acbae71ca1a4b574bab321c
FCNO <fcno.dev@gmail.com>
2022-12-27 15:29:29 -0300

    feat(validation): add ShowSolicitanteRequest

---
+++ app/Http/Requests/Api/Solicitacao/ShowSolicitanteRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests\Api\Solicitacao;
+
+use App\Enums\Policy;
+use App\Models\Solicitacao;
+use App\Rules\RecebedorHabilitado;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+/**
+ * @see https://laravel.com/docs/9.x/validation#form-request-validation
+ * @see https://www.aaronsaray.com/2022/be-careful-with-prepareforvalidation
+ */
+class ShowSolicitanteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->user()->can(Policy::Create->value, Solicitacao::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'solicitante' => [
+                'bail',
+                'required',
+                'string',
+                'between:1,20',
+                Rule::exists('usuarios', 'username'),
+                new RecebedorHabilitado(),
+            ],
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'solicitante' => __('Solicitante'),
+        ];
+    }
+}
---
+++ tests/Feature/App/Http/Requests/Api/Solicitacao/ShowSolicitanteRequestTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://hofmannsven.com/2020/testing-laravel-form-requests
+ * @see https://github.com/jasonmccreary/laravel-test-assertions
+ */
+
+use App\Http\Requests\Api\Solicitacao\ShowSolicitanteRequest;
+use App\Models\Permissao;
+use App\Rules\RecebedorHabilitado;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Validation\Rule;
+
+beforeEach(function () {
+    $this->request = new ShowSolicitanteRequest();
+});
+
+// Caminho feliz
+test('usuário sem autorização não cria o request', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    login();
+
+    expect($this->request->authorize())->toBeFalse();
+});
+
+test('rules estão definidas no form request para a exibição do solicitante', function () {
+    $this->assertExactValidationRules([
+        'solicitante' => [
+            'bail',
+            'required',
+            'string',
+            'between:1,20',
+            Rule::exists('usuarios', 'username'),
+            new RecebedorHabilitado(),
+        ],
+    ], $this->request->rules());
+});
+
+test('attributes estão definidas no form request', function () {
+    $this->assertExactValidationRules([
+        'solicitante' => __('Solicitante'),
+    ], $this->request->attributes());
+});
+
+test('usuário autorizado pode criar o request', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    login();
+
+    concederPermissao(Permissao::SOLICITACAO_CREATE);
+
+    expect($this->request->authorize())->toBeTrue();
+});
