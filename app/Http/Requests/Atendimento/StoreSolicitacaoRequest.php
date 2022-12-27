71886d1f0ccc34028c6575dcb9bdae6c46125e92
FCNO <fcno.dev@gmail.com>
2022-12-27 15:59:28 -0300

    feat(validation): add StoreSolicitacaoRequest

---
+++ app/Http/Requests/Atendimento/StoreSolicitacaoRequest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Requests\Atendimento;
+
+use App\Enums\Policy;
+use App\Models\Solicitacao;
+use App\Rules\NumeroProcessoCNJ;
+use App\Rules\ProcessoDisponivel;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Arr;
+use Illuminate\Validation\Rule;
+
+/**
+ * @see https://laravel.com/docs/9.x/validation#form-request-validation
+ * @see https://www.aaronsaray.com/2022/be-careful-with-prepareforvalidation
+ */
+class StoreSolicitacaoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->user()->can(Policy::Create->value, Solicitacao::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'solicitante_id' => [
+                'bail',
+                'required',
+                'integer',
+                'exists:usuarios,id',
+            ],
+
+            'destino_id' => [
+                'bail',
+                'required',
+                'integer',
+                'min:1',
+                'exists:lotacoes,id',
+            ],
+
+            'processos.*.numero' => [
+                'bail',
+                'required',
+                'string',
+                'max:25',
+                new NumeroProcessoCNJ(),
+                Rule::exists('processos', 'numero'),
+                new ProcessoDisponivel(),
+            ],
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'solicitante_id' => __('Solicitante'),
+            'destino_id' => __('Destino'),
+            'processos.*.numero' => __('Número do processo'),
+        ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $merge = collect($this->get('processos'))
+            ->map(function ($item) {
+                return ['numero' => apenasNumeros(Arr::get($item, 'numero'))];
+            })->toArray();
+
+        $this->merge([
+            'processos' => $merge,
+        ]);
+    }
+}
---
+++ tests/Feature/App/Http/Requests/Atendimento/StoreSolicitacaoRequestTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://hofmannsven.com/2020/testing-laravel-form-requests
+ * @see https://github.com/jasonmccreary/laravel-test-assertions
+ */
+
+use App\Http\Requests\Atendimento\StoreSolicitacaoRequest;
+use App\Models\Permissao;
+use App\Rules\NumeroProcessoCNJ;
+use App\Rules\ProcessoDisponivel;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Validation\Rule;
+
+beforeEach(function () {
+    $this->request = new StoreSolicitacaoRequest();
+});
+
+// Autorização
+test('usuário sem autorização não cria o request', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    login();
+
+    expect($this->request->authorize())->toBeFalse();
+});
+
+// Caminho feliz
+test('rules estão definidas no form request', function () {
+    $this->assertExactValidationRules([
+        'solicitante_id' => [
+            'bail',
+            'required',
+            'integer',
+            'exists:usuarios,id',
+        ],
+        'destino_id' => [
+            'bail',
+            'required',
+            'integer',
+            'min:1',
+            'exists:lotacoes,id',
+        ],
+        'processos.*.numero' => [
+            'bail',
+            'required',
+            'string',
+            'max:25',
+            new NumeroProcessoCNJ(),
+            Rule::exists('processos', 'numero'),
+            new ProcessoDisponivel(),
+        ],
+    ], $this->request->rules());
+});
+
+test('attributes estão definidas no form request', function () {
+    $this->assertExactValidationRules([
+        'solicitante_id' => __('Solicitante'),
+        'destino_id' => __('Destino'),
+        'processos.*.numero' => __('Número do processo'),
+    ], $this->request->attributes());
+});
+
+test('usuário autorizado pode criar o request', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    login();
+
+    concederPermissao(Permissao::SOLICITACAO_CREATE);
+
+    expect($this->request->authorize())->toBeTrue();
+});
