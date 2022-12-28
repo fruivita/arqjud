6a007341373b40fd783b1ea46d7b8585b40ba23b
FCNO <fcno.dev@gmail.com>
2022-12-28 19:19:53 -0300

    feat(validation): add StoreDevolverProcessoRequest

---
+++ app/Http/Requests/Atendimento/StoreDevolverProcessoRequest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Requests\Atendimento;
+
+use App\Enums\Policy;
+use App\Models\Solicitacao;
+use App\Rules\NumeroProcessoCNJ;
+use App\Rules\ProcessoRetornavel;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+/**
+ * @see https://laravel.com/docs/9.x/validation#form-request-validation
+ */
+class StoreDevolverProcessoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->user()->can(Policy::Update->value, Solicitacao::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'numero' => [
+                'bail',
+                'required',
+                'string',
+                'max:25',
+                new NumeroProcessoCNJ(),
+                Rule::exists('processos', 'numero'),
+                new ProcessoRetornavel(),
+            ],
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'numero' => __('Número do processo'),
+        ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $merge = [];
+
+        if ($this->has('numero')) {
+            $merge['numero'] = apenasNumeros($this->get('numero'));
+        }
+
+        $this->merge($merge);
+    }
+}
---
+++ tests/Feature/App/Http/Requests/Atendimento/StoreDevolverProcessoRequestTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://hofmannsven.com/2020/testing-laravel-form-requests
+ * @see https://github.com/jasonmccreary/laravel-test-assertions
+ */
+
+use App\Http\Requests\Atendimento\StoreDevolverProcessoRequest;
+use App\Models\Permissao;
+use App\Models\Usuario;
+use App\Rules\NumeroProcessoCNJ;
+use App\Rules\ProcessoRetornavel;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+beforeEach(function () {
+    $this->request = new StoreDevolverProcessoRequest();
+});
+
+// Autorização
+test('usuário sem autorização não cria o request', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    Auth::login(Usuario::factory()->create());
+
+    expect($this->request->authorize())->toBeFalse();
+});
+
+// Caminho feliz
+test('rules estão definidas no form request', function () {
+    $this->assertExactValidationRules([
+        'numero' => [
+            'bail',
+            'required',
+            'string',
+            'max:25',
+            new NumeroProcessoCNJ(),
+            Rule::exists('processos', 'numero'),
+            new ProcessoRetornavel(),
+        ],
+    ], $this->request->rules());
+});
+
+test('attributes estão definidas no form request', function () {
+    $this->assertExactValidationRules([
+        'numero' => __('Número do processo'),
+    ], $this->request->attributes());
+});
+
+test('usuário autorizado pode criar o request', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    Auth::login(Usuario::factory()->create());
+
+    concederPermissao(Permissao::SOLICITACAO_UPDATE);
+
+    expect($this->request->authorize())->toBeTrue();
+});
