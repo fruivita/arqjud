d7f60f1323e7be4ffa27d83580c502618b53ab66
FCNO <fcno.dev@gmail.com>
2022-12-28 19:38:47 -0300

    feat(job): add NotificarSolicitanteDevolucao

---
+++ app/Jobs/NotificarSolicitanteDevolucao.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Usuario;
+use App\Notifications\ProcessoDevolvido;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Notification;
+
+/**
+ * Notifica o solicitante sobre a devolução ao arquivo de processo por ele
+ * solicitado.
+ *
+ * @see https://laravel.com/docs/9.x/queues
+ */
+class NotificarSolicitanteDevolucao implements ShouldQueue, ShouldBeUnique
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Número de tentativas de execução do job.
+     *
+     * @var int
+     */
+    public $tries = 5;
+
+    /**
+     * Número de segundos de espera antes de tentar executar novamente o job.
+     *
+     * @var int[]
+     */
+    public $backoff = [60, 300];
+
+    /**
+     * Número dos processo devolvido
+     *
+     * @var string
+     */
+    private $processo;
+
+    /**
+     * Data e hora da devolução
+     *
+     * @var \Illuminate\Support\Carbon
+     */
+    private Carbon $devolvido_em;
+
+    /**
+     * Usuário solicitante.
+     *
+     * @var \App\Models\Usuario
+     */
+    private Usuario $solicitante;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param  \stdClass  $devolucao
+     * @return void
+     */
+    public function __construct(\stdClass $devolucao)
+    {
+        $this->processo = $devolucao->processo;
+        $this->devolvido_em = $devolucao->devolvido_em;
+        $this->solicitante = $devolucao->solicitante->withoutRelations();
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Notification::send(
+            $this->solicitante,
+            new ProcessoDevolvido(
+                $this->processo,
+                $this->devolvido_em->tz(config('app.tz'))->format('d-m-Y H:i:s'),
+                route('solicitacao.index')
+            )
+        );
+    }
+
+    /**
+     * Get o cache driver usado para determinar o lock para jobs únicos.
+     *
+     * @return \Illuminate\Contracts\Cache\Repository
+     */
+    public function uniqueVia()
+    {
+        return cache()->driver('database');
+    }
+}
---
+++ tests/Feature/App/Jobs/NotificarSolicitanteDevolucaoTest.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Enums\Queue as EQueue;
+use App\Jobs\NotificarSolicitanteDevolucao;
+use App\Models\Lotacao;
+use App\Models\Usuario;
+use App\Notifications\ProcessoDevolvido;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Notifications\SendQueuedNotifications;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Queue;
+use function Spatie\PestPluginTestTime\testTime;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+    testTime()->freeze();
+
+    $this->devolucao = new \stdClass();
+    $this->devolucao->processo = '1111';
+    $this->devolucao->devolvido_em = now();
+    $this->devolucao->solicitante = Usuario::factory()->create();
+});
+
+// Caminho feliz
+test('job NotificarSolicitanteDevolucao envia notificação ao solicitante', function () {
+    Notification::fake();
+
+    NotificarSolicitanteDevolucao::dispatchSync($this->devolucao);
+
+    Notification::assertSentTo($this->devolucao->solicitante, ProcessoDevolvido::class);
+});
+
+test('job NotificarSolicitanteDevolucao cria a notificação com todos os parâmetros e canal esperados', function () {
+    Notification::fake();
+
+    NotificarSolicitanteDevolucao::dispatchSync($this->devolucao);
+
+    Notification::assertSentTo(
+        $this->devolucao->solicitante,
+        ProcessoDevolvido::class,
+        function (ProcessoDevolvido $notification, $channels) {
+            expect($notification->toArray(null))->toMatchArray([
+                'processo' => $this->devolucao->processo,
+                'devolvido_em' => now()->tz(config('app.tz'))->format('d-m-Y H:i:s'),
+                'url' => route('solicitacao.index'),
+            ])->and($channels)->toMatchArray(['mail']);
+
+            return true;
+        }
+    );
+});
+
+test('job NotificarSolicitanteDevolucao envia para a queue de prioridade baixa a execução da notificação', function () {
+    Queue::fake()->except([
+        NotificarSolicitanteDevolucao::class,
+    ]);
+
+    NotificarSolicitanteDevolucao::dispatchSync($this->devolucao);
+
+    Queue::assertPushedOn(
+        EQueue::Baixa->value,
+        SendQueuedNotifications::class,
+        fn (SendQueuedNotifications $job) => $job->notification::class === ProcessoDevolvido::class
+    );
+
+    Queue::assertPushed(SendQueuedNotifications::class, 1); // 1 por usuário
+});
