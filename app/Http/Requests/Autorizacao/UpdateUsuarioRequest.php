e45bcf7f3495705f1010fffdf0cfa485fc35a41e
FCNO <fcno.dev@gmail.com>
2022-12-30 14:01:30 -0300

    feat(validation): add UpdateUsuarioRequest

---
+++ app/Http/Requests/Autorizacao/UpdateUsuarioRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests\Autorizacao;
+
+use App\Enums\Policy;
+use App\Models\Usuario;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+/**
+ * @see https://laravel.com/docs/9.x/validation#form-request-validation
+ */
+class UpdateUsuarioRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->user()->can(Policy::Update->value, Usuario::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'perfil_id' => [
+                'bail',
+                'required',
+                'integer',
+                Rule::exists('perfis', 'id'),
+            ],
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'perfil_id' => __('Perfil'),
+        ];
+    }
+}
---
+++ tests/Feature/App/Http/Requests/Autorizacao/UpdateUsuarioRequestTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://hofmannsven.com/2020/testing-laravel-form-requests
+ * @see https://github.com/jasonmccreary/laravel-test-assertions
+ */
+
+use App\Http\Requests\Autorizacao\UpdateUsuarioRequest;
+use App\Models\Permissao;
+use App\Models\Usuario;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+beforeEach(function () {
+    $this->request = new UpdateUsuarioRequest();
+});
+
+// Autorização
+test('usuário sem autorização não cria o request', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    Auth::login(Usuario::factory()->create());
+
+    expect($this->request->authorize())->toBeFalse();
+});
+
+// Caminho feliz
+test('rules estão definidas no form request', function () {
+    $this->assertExactValidationRules([
+        'perfil_id' => [
+            'bail',
+            'required',
+            'integer',
+            Rule::exists('perfis', 'id'),
+        ],
+    ], $this->request->rules());
+});
+
+test('attributes estão definidas no form request', function () {
+    $this->assertExactValidationRules([
+        'perfil_id' => __('Perfil'),
+    ], $this->request->attributes());
+});
+
+test('usuário autorizado pode criar o request', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    Auth::login(Usuario::factory()->create());
+
+    concederPermissao(Permissao::USUARIO_UPDATE);
+
+    expect($this->request->authorize())->toBeTrue();
+});
