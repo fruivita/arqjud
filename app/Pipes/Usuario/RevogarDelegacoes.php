9a5c097f1ba0ce08d844c1657e18395d8d570957
FCNO <fcno.dev@gmail.com>
2022-12-30 15:52:38 -0300

    feat(core): add pipe RevogarDelegacoes

---
+++ app/Pipes/Usuario/RevogarDelegacoes.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Pipes\Usuario;
+
+use App\Models\Usuario;
+
+/**
+ * @see https://www.youtube.com/watch?v=FByQN_d876c
+ */
+class RevogarDelegacoes
+{
+    /**
+     * Revoga por pipe todas as delegações feitas pelo usuário restaurando o
+     * perfil antigo de cada usuário.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @param  \Closure  $next
+     * @return \App\Models\Usuario
+     */
+    public function handle(Usuario $usuario, \Closure $next)
+    {
+        $usuario
+            ->delegados()
+            ->get()
+            ->each(function (Usuario $delegado) {
+                $delegado
+                    ->perfil()->associate($delegado->antigo_perfil_id)
+                    ->perfilAntigo()->dissociate()
+                    ->delegante()->dissociate()
+                    ->save();
+            });
+
+        return $next($usuario);
+    }
+}
