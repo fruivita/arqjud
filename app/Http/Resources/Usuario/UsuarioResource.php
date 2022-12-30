d5dc23cac12444618584e3d42b08a333feee26ef
FCNO <fcno.dev@gmail.com>
2022-12-30 14:35:01 -0300

    feat(api): ADD UsuarioResource

---
+++ app/Http/Resources/Usuario/UsuarioResource.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Resources\Usuario;
+
+use App\Enums\Policy;
+use App\Http\Resources\Cargo\CargoOnlyResource;
+use App\Http\Resources\Funcao\FuncaoOnlyResource;
+use App\Http\Resources\Lotacao\LotacaoOnlyResource;
+use App\Http\Resources\Perfil\PerfilOnlyResource;
+use App\Models\Usuario;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @see https://laravel.com/docs/9.x/eloquent-resources
+ */
+class UsuarioResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return ($this->resource)
+            ? [
+                'id' => $this->id,
+                'matricula' => $this->matricula,
+                'username' => $this->username,
+                'nome' => $this->nome,
+                'email' => $this->email,
+                'status' => $this->habilitado() ? __('completo') : __('incompleto'),
+                'lotacao_id' => $this->lotacao_id,
+                'cargo_id' => $this->cargo_id,
+                'funcao_confianca_id' => $this->funcao_confianca_id,
+                'perfil_id' => $this->perfil_id,
+                'perfil_concedido_por' => $this->perfil_concedido_por,
+                'antigo_perfil_id' => $this->antigo_perfil_id,
+                'lotacao' => LotacaoOnlyResource::make($this->whenLoaded('lotacao')),
+                'cargo' => CargoOnlyResource::make($this->whenLoaded('cargo')),
+                'funcao' => FuncaoOnlyResource::make($this->whenLoaded('funcaoConfianca')),
+                'perfil' => PerfilOnlyResource::make($this->whenLoaded('perfil')),
+                'delegante' => UsuarioOnlyResource::make($this->whenLoaded('delegante')),
+                'perfil_antigo' => PerfilOnlyResource::make($this->whenLoaded('perfilAntigo')),
+                'links' => [
+                    'view' => $this->when(
+                        auth()->user()->can(Policy::ViewOrUpdate->value, Usuario::class),
+                        route('autorizacao.usuario.edit', $this->id),
+                    ),
+                    'update' => $this->when(
+                        auth()->user()->can(Policy::Update->value, Usuario::class),
+                        route('autorizacao.usuario.update', $this->id),
+                    ),
+                ],
+            ]
+            : [];
+    }
+}
---
+++ tests/Feature/App/Http/Resources/Usuario/UsuarioResourceTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://inertiajs.com/testing
+ */
+
+use App\Http\Resources\Cargo\CargoOnlyResource;
+use App\Http\Resources\Funcao\FuncaoOnlyResource;
+use App\Http\Resources\Perfil\PerfilOnlyResource;
+use App\Http\Resources\Usuario\UsuarioOnlyResource;
+use App\Http\Resources\Usuario\UsuarioResource;
+use App\Models\Permissao;
+use App\Models\Usuario;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Support\Facades\Auth;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $usuario = Usuario::factory()->create();
+    Auth::login($usuario);
+
+    $this->usuario = Usuario::factory()->completo()->create();
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Caminho feliz
+test('retorna os campos principais e as rotas autorizadas do modelo', function () {
+    concederPermissao([Permissao::USUARIO_UPDATE]);
+
+    $resource = UsuarioResource::make($this->usuario);
+
+    expect($resource->response()->getData(true))->toMatchArray([
+        'data' => usuarioApi($this->usuario)
+            + [
+                'links' => [
+                    'view' => route('autorizacao.usuario.edit', $this->usuario),
+                    'update' => route('autorizacao.usuario.update', $this->usuario),
+                ],
+            ],
+    ]);
+});
+
+test('retorna o relacionamento se houver o eager load da propriedade e sem os links se não houver rota autorizada', function () {
+    $resource = UsuarioResource::make($this->usuario->load(['lotacao', 'cargo', 'funcaoConfianca', 'perfil', 'delegante', 'perfilAntigo']));
+
+    expect($resource->response()->getData(true))->toMatchArray([
+        'data' => usuarioApi($this->usuario)
+            + ['lotacao' => lotacaoApi($this->usuario->lotacao)]
+            + ['cargo' => CargoOnlyResource::make($this->usuario->cargo)->resolve()]
+            + ['funcao' => FuncaoOnlyResource::make($this->usuario->funcaoConfianca)->resolve()]
+            + ['perfil' => PerfilOnlyResource::make($this->usuario->perfil)->resolve()]
+            + ['delegante' => UsuarioOnlyResource::make($this->usuario->delegante)->resolve()]
+            + ['perfil_antigo' => PerfilOnlyResource::make($this->usuario->perfilAntigo)->resolve()]
+            + ['links' => []],
+    ]);
+});
+
+test('retorna o resource vazio se o modelo for nulo', function () {
+    expect(UsuarioResource::make(null)->resolve())->toBeEmpty();
+});
