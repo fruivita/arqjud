50d227215ce08f79f5aa4e3e13835e1b791d38d7
FCNO <fcno.dev@gmail.com>
2022-12-30 15:31:10 -0300

    feat(core): add pipe Order do usuário

---
+++ app/Pipes/Usuario/Order.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Pipes\Usuario;
+
+use App\Pipes\OrderBase;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Pressupõe join com as tabelas pais se o critério de ordenação for por elas.
+ *
+ * @see https://www.youtube.com/watch?v=FByQN_d876c
+ */
+class Order extends OrderBase
+{
+    /**
+     * Aplica a ordenação pelo matricula.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function matricula(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('matricula', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo username.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function username(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('username', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo email.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function email(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('email', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo nome.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function nome(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('nome', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pela sigla da lotação.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function lotacaoSigla(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('lotacoes.sigla', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo nome do cargo.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function cargoNome(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('cargos.nome', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo nome da função de confiança.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function funcaoNome(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('funcoes_confianca.nome', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo nome do perfil.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function perfilNome(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('perfis.nome', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo username do delegante.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function deleganteUsername(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('delegantes.username', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação pelo nome do perfil antigo.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function perfilAntigoNome(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('perfis_antigos.nome', $direcao);
+    }
+}
---
+++ tests/Feature/App/Pipes/Usuario/OrderTest.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Models\Usuario;
+use App\Pipes\Usuario\JoinAll;
+use App\Pipes\Usuario\Order;
+use MichaelRubel\EnhancedPipeline\Pipeline;
+use function Spatie\Snapshots\assertMatchesSnapshot;
+
+// Caminho feliz
+test('ordena pelo nome do usuário', function () {
+    Usuario::factory()->create(['id' => 1, 'nome' => 'bbbb']);
+    Usuario::factory()->create(['id' => 2, 'nome' => 'aaaa']);
+
+    request()->merge(['order' => ['nome' => 'desc']]);
+
+    $usuarios = Pipeline::make()
+        ->send(Usuario::query())
+        ->through([Order::class])
+        ->thenReturn()
+        ->pluck('id');
+
+    expect($usuarios->toArray())->toMatchArray([1, 2]);
+});
+
+test('todos os métodos de ordenação disponíveis são acionados', function (string $campo) {
+    $this->partialMock(Order::class)
+        ->shouldAllowMockingProtectedMethods()
+        ->shouldReceive(str()->camel($campo))
+        ->withSomeOfArgs('desc')
+        ->once();
+
+    request()->merge(['order' => [$campo => 'desc']]);
+
+    Pipeline::make()
+        ->send(Usuario::query())
+        ->through([Order::class])
+        ->thenReturn();
+})->with([
+    'nome',
+    'matricula',
+    'username',
+    'email',
+    'lotacao_sigla',
+    'cargo_nome',
+    'funcao_nome',
+    'perfil_nome',
+    'delegante_username',
+    'perfil_antigo_nome',
+]);
+
+test('todas as ordenações possíveis no request do usuário', function () {
+    request()->merge(['order' => [
+        'nome' => 'asc',
+        'matricula' => 'asc',
+        'username' => 'asc',
+        'email' => 'asc',
+        'lotacao_sigla' => 'desc',
+        'cargo_nome' => 'desc',
+        'funcao_nome' => 'desc',
+        'perfil_nome' => 'desc',
+        'delegante_username' => 'desc',
+        'perfil_antigo_nome' => 'desc',
+    ]]);
+
+    $query = Pipeline::make()
+        ->send(Usuario::query())
+        ->through([JoinAll::class, Order::class])
+        ->thenReturn();
+
+    assertMatchesSnapshot([$query->toSql(), $query->getBindings()]);
+});
