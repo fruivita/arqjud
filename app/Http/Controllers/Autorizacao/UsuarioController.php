fad3e728945582748620ba2d1c5a932d73bf92b5
FCNO <fcno.dev@gmail.com>
2022-12-30 16:47:12 -0300

    feat(controller): add UsuarioController

---
+++ app/Http/Controllers/Autorizacao/UsuarioController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Autorizacao;
+
+use App\Enums\Policy;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Autorizacao\UpdateUsuarioRequest;
+use App\Http\Resources\Perfil\PerfilOnlyResource;
+use App\Http\Resources\Usuario\UsuarioCollection;
+use App\Http\Resources\Usuario\UsuarioResource;
+use App\Http\Traits\ComFeedback;
+use App\Http\Traits\ComPaginacaoEmCache;
+use App\Models\Perfil;
+use App\Models\Usuario;
+use App\Pipes\Search;
+use App\Pipes\Usuario\AlterarPerfil;
+use App\Pipes\Usuario\JoinAll;
+use App\Pipes\Usuario\Order;
+use App\Pipes\Usuario\RevogarDelegacoes;
+use Inertia\Inertia;
+use MichaelRubel\EnhancedPipeline\Pipeline;
+
+/**
+ * @see https://laravel.com/docs/9.x/controllers
+ * @see https://laravel.com/docs/9.x/requests
+ * @see https://laravel.com/docs/9.x/responses
+ * @see https://inertiajs.com/
+ */
+class UsuarioController extends Controller
+{
+    use ComFeedback;
+    use ComPaginacaoEmCache;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Inertia\Response
+     */
+    public function index()
+    {
+        $this->authorize(Policy::ViewAny->value, Usuario::class);
+
+        return Inertia::render('Autorizacao/Usuario/Index', [
+            'usuarios' => fn () => UsuarioCollection::make(
+                Pipeline::make()
+                    ->send(Usuario::select('usuarios.*')->with(['lotacao', 'cargo', 'funcaoConfianca', 'perfil', 'perfilAntigo', 'delegante']))
+                    ->through([JoinAll::class, Order::class, Search::class])
+                    ->thenReturn()
+                    ->paginate($this->perPage())
+            )->additional(['meta' => [
+                'termo' => request()->query('termo'),
+                'order' => request()->query('order'),
+            ]])->preserveQuery(),
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Inertia\Response
+     */
+    public function edit(Usuario $usuario)
+    {
+        $this->authorize(Policy::ViewOrUpdate->value, Usuario::class);
+
+        return Inertia::render('Autorizacao/Usuario/Edit', [
+            'usuario' => fn () => UsuarioResource::make(
+                $usuario->load(['lotacao', 'cargo', 'funcaoConfianca', 'perfil', 'perfilAntigo', 'delegante'])
+            ),
+            'perfis' => fn () => PerfilOnlyResource::collection(Perfil::disponiveisParaAtribuicao()->orderBy('poder', 'desc')->get()),
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\Autorizacao\UpdateUsuarioRequest  $request
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UpdateUsuarioRequest $request, Usuario $usuario)
+    {
+        $salvo = Pipeline::make()
+            ->withTransaction()
+            ->send($usuario)
+            ->through([
+                AlterarPerfil::class . ':' . $request->integer('perfil_id'),
+                RevogarDelegacoes::class,
+            ])
+            ->onFailure(fn () => false)
+            ->then(fn () => true);
+
+        return back()->with($this->feedback($salvo));
+    }
+}
---
+++ tests/Feature/App/Http/Controllers/Autorizacao/UsuarioControllerTest.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://inertiajs.com/testing
+ * @see https://hofmannsven.com/2020/testing-laravel-form-requests
+ * @see https://github.com/jasonmccreary/laravel-test-assertions
+ */
+
+use App\Http\Controllers\Autorizacao\UsuarioController;
+use App\Http\Requests\Autorizacao\UpdateUsuarioRequest;
+use App\Http\Resources\Usuario\UsuarioResource;
+use App\Models\Perfil;
+use App\Models\Permissao;
+use App\Models\Usuario;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Testing\AssertableInertia as Assert;
+use function Pest\Laravel\get;
+use function Pest\Laravel\patch;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $this->usuario = Usuario::factory()->create();
+    Auth::login($this->usuario);
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Autorização
+test('usuário sem permissão não consegue listar os usuários', function () {
+    get(route('autorizacao.usuario.index'))->assertForbidden();
+});
+
+test('usuário sem permissão não consegue visualizar um usuário', function () {
+    $usuario = Usuario::factory()->create();
+
+    get(route('autorizacao.usuario.edit', $usuario))->assertForbidden();
+});
+
+// Caminho feliz
+test('action do controller usa o form request', function (string $action, string $request) {
+    $this->assertActionUsesFormRequest(
+        UsuarioController::class,
+        $action,
+        $request
+    );
+})->with([
+    ['update', UpdateUsuarioRequest::class],
+]);
+
+test('action index compartilha os dados esperados com a view/componente correto', function () {
+    Usuario::factory(2)->create();
+
+    concederPermissao([Permissao::USUARIO_VIEW_ANY]);
+
+    get(route('autorizacao.usuario.index'))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Autorizacao/Usuario/Index')
+                ->has('usuarios.data', 3)
+                ->has('usuarios.meta.termo')
+                ->has('usuarios.meta.order')
+        );
+});
+
+test('action edit compartilha os dados esperados com a view/componente correto', function () {
+    concederPermissao(Permissao::USUARIO_UPDATE);
+
+    $usuario = Usuario::factory()->completo()->create();
+
+    get(route('autorizacao.usuario.edit', $usuario))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Autorizacao/Usuario/Edit')
+                ->where('usuario', UsuarioResource::make($usuario->load(['lotacao', 'cargo', 'funcaoConfianca', 'perfil', 'delegante', 'perfilAntigo']))->response()->getData(true))
+                ->has('perfis.data', Perfil::disponiveisParaAtribuicao()->count())
+        );
+});
+
+test('action edit também é executável com permissão de visualização', function () {
+    concederPermissao(Permissao::USUARIO_VIEW);
+
+    $usuario = Usuario::factory()->completo()->create();
+
+    get(route('autorizacao.usuario.edit', $usuario))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page->component('Autorizacao/Usuario/Edit')
+        );
+});
+
+test('atualiza um usuário e remove suas delegações', function () {
+    $perfis = Perfil::all();
+
+    $this->usuario->perfil_id = $perfis->firstWhere('slug', Perfil::GERENTE_NEGOCIO)->id;
+    concederPermissao(Permissao::USUARIO_UPDATE);
+
+    $usuario = Usuario::factory()->create(['perfil_id' => $perfis->firstWhere('slug', Perfil::PADRAO)->id]);
+
+    $delegado = Usuario::factory()->create([
+        'perfil_id' => $perfis->firstWhere('slug', Perfil::GERENTE_NEGOCIO)->id,
+        'perfil_concedido_por' => $usuario->id,
+        'antigo_perfil_id' => $perfis->firstWhere('slug', Perfil::OPERADOR)->id,
+    ]);
+
+    patch(route('autorizacao.usuario.update', $usuario), [
+        'perfil_id' => $perfis->firstWhere('slug', Perfil::OPERADOR)->id
+    ])
+        ->assertRedirect()
+        ->assertSessionHas('feedback.sucesso');
+
+    $usuario->refresh();
+    $delegado->refresh();
+
+    expect($usuario->perfil_id)->toBe($perfis->firstWhere('slug', Perfil::OPERADOR)->id)
+        ->and($usuario->perfil_id)->toBe($perfis->firstWhere('slug', Perfil::OPERADOR)->id)
+        ->and($usuario->perfil_concedido_por)->toBeNull()
+        ->and($usuario->antigo_perfil_id)->toBeNull();
+});
+
+test('UsuarioController usa trait', function () {
+    expect(
+        collect(class_uses(UsuarioController::class))
+            ->has([
+                \App\Http\Traits\ComPaginacaoEmCache::class,
+                \App\Http\Traits\ComFeedback::class,
+            ])
+    )->toBeTrue();
+});
