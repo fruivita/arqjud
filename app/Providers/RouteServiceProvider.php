74e68a56ace1fe5e9fd0b02cb1d938f1fd3d7334
FCNO <fcno.dev@gmail.com>
2022-12-31 18:42:30 -0300

    feat(route): regex global no parâmetro log nas rotas

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,6 +36,8 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
+
+        Route::pattern('log', 'arqjud(-\d{4}-\d{2}-\d{2})?.log');
     }
 
     /**
