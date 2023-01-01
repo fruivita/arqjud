338a2159fb56df21f80d3428bb4a53a7200b2da4
FCNO <fcno.dev@gmail.com>
2023-01-01 15:49:46 -0300

    feat(api): add LogResource

---
+++ app/Http/Resources/Log/LogResource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources\Log;
+
+use App\Enums\Policy;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @see https://laravel.com/docs/9.x/eloquent-resources
+ */
+class LogResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return ($this->resource)
+            ? [
+                'nome' => $this->getFilename(),
+                'links' => [
+                    'view' => $this->when(
+                        auth()->user()->can(Policy::LogView->value),
+                        route('administracao.log.show', $this->getFilename())
+                    ),
+                    'download' => $this->when(
+                        auth()->user()->can(Policy::LogView->value),
+                        route('administracao.log.download', $this->getFilename())
+                    ),
+                    'delete' => $this->when(
+                        auth()->user()->can(Policy::LogDelete->value),
+                        route('administracao.log.destroy', $this->getFilename())
+                    ),
+                ]
+            ]
+            : [];
+    }
+}
---
+++ tests/Feature/App/Http/Resources/Log/LogResourceTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://inertiajs.com/testing
+ */
+
+use App\Http\Resources\Log\LogResource;
+use App\Models\Permissao;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+    login();
+
+    $this->filename = 'arqjud.log';
+
+    $this->storage = Storage::fake('log-aplicacao');
+
+    $this->storage->put($this->filename, 'Contents');
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Caminho feliz
+test('retorna os campos principais e as rotas autorizadas', function () {
+    concederPermissao([Permissao::LOG_VIEW, Permissao::LOG_DELETE]);
+
+    $resource = LogResource::make(File::allFiles($this->storage->path(''))[0]);
+
+    expect($resource->response()->getData(true))->toMatchArray([
+        'data' => [
+            'nome' => $this->filename,
+            'links' => [
+                'view' => route('administracao.log.show', $this->filename),
+                'download' => route('administracao.log.download', $this->filename),
+                'delete' => route('administracao.log.destroy', $this->filename),
+            ],
+        ],
+    ]);
+});
+
+test('retorna apenas os campos principais se não houver rota autorizada para o modelo', function () {
+    $resource = LogResource::make(File::allFiles($this->storage->path(''))[0]);
+
+    expect($resource->response()->getData(true))->toMatchArray([
+        'data' => [
+            'nome' => $this->filename,
+            'links' => [],
+        ],
+    ]);
+});
+
+test('retorna o resource vazio se o modelo for nulo', function () {
+    expect(LogResource::make(null)->resolve())->toBeEmpty();
+});
