2d2700f02b51e521f0c66122881f5cc0417e7351
FCNO <fcno.dev@gmail.com>
2023-01-02 18:41:37 -0300

    feat(core): add pipe Order da Permissão

---
+++ app/Pipes/Permissao/Order.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Pipes\Permissao;
+
+use App\Pipes\OrderBase;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * @see https://www.youtube.com/watch?v=FByQN_d876c
+ */
+class Order extends OrderBase
+{
+    /**
+     * Aplica a ordenação por nome da permissão.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function nome(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('nome', $direcao);
+    }
+
+    /**
+     * Aplica a ordenação por slug da permissão.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direcao asc ou desc
+     * @return void
+     */
+    protected function slug(Builder $query, string $direcao)
+    {
+        $direcao = ascOrDesc($direcao);
+
+        $query->orderBy('slug', $direcao);
+    }
+}
---
+++ tests/Feature/App/Pipes/Permissao/OrderTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Models\Permissao;
+use App\Pipes\Permissao\Order;
+use MichaelRubel\EnhancedPipeline\Pipeline;
+use function Spatie\Snapshots\assertMatchesSnapshot;
+
+// Caminho feliz
+test('sem ordenação válida no request, ordena pelo id desc', function (string $coluna, string $direcao) {
+    Permissao::factory()->create(['id' => 1]);
+    Permissao::factory()->create(['id' => 2]);
+
+    request()->merge(['order' => [$coluna, $direcao]]);
+
+    $perfis = Pipeline::make()
+        ->send(Permissao::query())
+        ->through([Order::class])
+        ->thenReturn()->pluck('id');
+
+    expect($perfis->toArray())->toMatchArray([2, 1]);
+})->with([
+    ['', ''],
+    ['foo', 'asc'],
+]);
+
+test('ordena pelo nome', function () {
+    Permissao::factory()->create(['id' => 1, 'nome' => 'bbb']);
+    Permissao::factory()->create(['id' => 2, 'nome' => 'aaa']);
+
+    request()->merge(['order' => ['nome' => 'desc']]);
+
+    $perfis = Pipeline::make()
+        ->send(Permissao::query())
+        ->through([Order::class])
+        ->thenReturn()->pluck('id');
+
+    expect($perfis->toArray())->toMatchArray([1, 2]);
+});
+
+test('com todas as ordenações específicas na permissão', function () {
+    request()->merge(['order' => [
+        'nome' => 'asc',
+        'slug' => 'asc',
+    ]]);
+
+    $query = Pipeline::make()
+        ->send(Permissao::query())
+        ->through([Order::class])
+        ->thenReturn();
+
+    assertMatchesSnapshot([$query->toSql(), $query->getBindings()]);
+});
