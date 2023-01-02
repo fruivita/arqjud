33c8d5e39453c544b377e9fff572e47026b105a5
FCNO <fcno.dev@gmail.com>
2023-01-01 22:11:47 -0300

    feat(api): add PerfilResource

---
+++ tests/Feature/App/Http/Resources/Perfil/PerfilResourceTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://inertiajs.com/testing
+ */
+
+use App\Http\Resources\Perfil\PerfilResource;
+use App\Models\Perfil;
+use App\Models\Permissao;
+use Database\Seeders\PerfilSeeder;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+    login();
+
+    $this->perfil = Perfil::factory()->create();
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Caminho feliz
+test('retorna os campos principais e as rotas autorizadas do modelo', function () {
+    concederPermissao([Permissao::PERFIL_DELETE, Permissao::PERFIL_VIEW, Permissao::PERFIL_UPDATE]);
+
+    $resource = PerfilResource::make($this->perfil);
+
+    expect($resource->response()->getData(true))->toMatchArray([
+        'data' => $this->perfil->only(['id', 'nome', 'slug', 'poder', 'descricao'])
+            + [
+                'links' => [
+                    'view' => route('administracao.perfil.edit', $this->perfil),
+                    'update' => route('administracao.perfil.update', $this->perfil),
+                    'delete' => route('administracao.perfil.destroy', $this->perfil),
+                ],
+            ],
+    ]);
+});
+
+test('retorna a quantidade de filhos se houver o eager load da propriedade', function () {
+    $resource = PerfilResource::make($this->perfil->loadCount(['usuarios', 'delegados']));
+
+    expect($resource->response()->getData(true))->toMatchArray([
+        'data' => $this->perfil->only(['id', 'nome', 'slug', 'poder', 'descricao', 'usuarios_count', 'delegados_count'])
+            + ['links' => []],
+    ]);
+});
+
+test('retorna apenas os campos principais se não houver rota autorizada para o modelo', function () {
+    $resource = PerfilResource::make($this->perfil);
+
+    expect($resource->response()->getData(true))->toMatchArray([
+        'data' => $this->perfil->only(['id', 'nome', 'slug', 'poder', 'descricao'])
+            + ['links' => []],
+    ]);
+});
+
+test('retorna o resource vazio se o modelo for nulo', function () {
+    expect(PerfilResource::make(null)->resolve())->toBeEmpty();
+});
