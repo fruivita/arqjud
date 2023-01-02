089d2726e3aefba2bf835fa574df17b02f2bca71
FCNO <fcno.dev@gmail.com>
2023-01-01 22:16:21 -0300

    feat(api): add PerfilEditResource

---
+++ app/Http/Resources/Perfil/PerfilEditResource.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Resources\Perfil;
+
+use App\Enums\Policy;
+use App\Models\Perfil;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @see https://laravel.com/docs/9.x/eloquent-resources
+ */
+class PerfilEditResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return ($this->resource)
+            ? [
+                'id' => $this->id,
+                'nome' => $this->nome,
+                'slug' => $this->slug,
+                'poder' => $this->poder,
+                'descricao' => $this->descricao,
+                'usuarios_count' => $this->whenCounted('usuarios'),
+                'delegados_count' => $this->whenCounted('delegados'),
+                'links' => [
+                    'view' => $this->when(
+                        auth()->user()->can(Policy::ViewOrUpdate->value, Perfil::class),
+                        route('administracao.perfil.edit', $this->id),
+                    ),
+                    'update' => $this->when(
+                        auth()->user()->can(Policy::Update->value, Perfil::class),
+                        route('administracao.perfil.update', $this->id),
+                    ),
+                ],
+            ]
+            : [];
+    }
+}
---
+++ tests/Feature/App/Http/Resources/Perfil/PerfilEditResourceTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://inertiajs.com/testing
+ */
+
+use App\Http\Resources\Perfil\PerfilEditResource;
+use App\Models\Perfil;
+use App\Models\Permissao;
+use Database\Seeders\PerfilSeeder;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+    login();
+
+    $this->perfil = Perfil::factory()->create();
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Caminho feliz
+test('retorna os campos principais e as rotas autorizadas do modelo', function () {
+    concederPermissao([Permissao::PERFIL_DELETE, Permissao::PERFIL_VIEW, Permissao::PERFIL_UPDATE]);
+
+    $resource = PerfilEditResource::make($this->perfil);
+
+    expect($resource->response()->getData(true))->toMatchArray([
+        'data' => $this->perfil->only(['id', 'nome', 'slug', 'poder', 'descricao'])
+            + [
+                'links' => [
+                    'view' => route('administracao.perfil.edit', $this->perfil),
+                    'update' => route('administracao.perfil.update', $this->perfil),
+                ],
+            ],
+    ]);
+});
+
+test('retorna a quantidade de filhos se houver o eager load da propriedade', function () {
+    $resource = PerfilEditResource::make($this->perfil->loadCount(['usuarios', 'delegados']));
+
+    expect($resource->response()->getData(true))->toMatchArray([
+        'data' => $this->perfil->only(['id', 'nome', 'slug', 'poder', 'descricao', 'usuarios_count', 'delegados_count'])
+            + ['links' => []],
+    ]);
+});
+
+test('retorna apenas os campos principais se não houver rota autorizada para o modelo', function () {
+    $resource = PerfilEditResource::make($this->perfil);
+
+    expect($resource->response()->getData(true))->toMatchArray([
+        'data' => $this->perfil->only(['id', 'nome', 'slug', 'poder', 'descricao'])
+            + ['links' => []],
+    ]);
+});
+
+test('retorna o resource vazio se o modelo for nulo', function () {
+    expect(PerfilEditResource::make(null)->resolve())->toBeEmpty();
+});
