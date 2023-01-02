3b5739262a60969bb441b0a8bb43fb919d73f692
FCNO <fcno.dev@gmail.com>
2023-01-02 18:05:15 -0300

    feat(validation): addd UpdatePerfilRequest

---
+++ app/Http/Requests/Administracao/UpdatePerfilRequest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Requests\Administracao;
+
+use App\Enums\Policy;
+use App\Models\Perfil;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+/**
+ * @see https://laravel.com/docs/9.x/validation#form-request-validation
+ * @see https://www.aaronsaray.com/2022/be-careful-with-prepareforvalidation
+ */
+class UpdatePerfilRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->user()->can(Policy::Update->value, Perfil::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'nome' => [
+                'bail',
+                'required',
+                'string',
+                'between:1,50',
+                Rule::unique('perfis', 'nome')
+                    ->ignore($this->perfil),
+            ],
+
+            'slug' => [
+                'bail',
+                'required',
+                'string',
+                'between:1,50',
+                Rule::unique('perfis', 'slug')
+                    ->ignore($this->perfil),
+            ],
+
+            'descricao' => [
+                'bail',
+                'nullable',
+                'string',
+                'between:1,255',
+            ],
+
+            'permissao_id' => [
+                'bail',
+                'nullable',
+                'integer',
+                Rule::exists('permissoes', 'id'),
+            ],
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'nome' => __('Nome'),
+            'slug' => __('Slug'),
+            'descricao' => __('Descrição'),
+            'perfil_id' => __('Perfil'),
+        ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $merge = [];
+
+        if ($this->has('nome')) {
+            $merge['slug'] = str($this->get('nome'))->slug()->toString();
+        }
+
+        $this->merge($merge);
+    }
+}
---
+++ tests/Feature/App/Http/Requests/Administracao/UpdatePerfilRequestTest.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://hofmannsven.com/2020/testing-laravel-form-requests
+ * @see https://github.com/jasonmccreary/laravel-test-assertions
+ */
+
+use App\Http\Requests\Administracao\UpdatePerfilRequest;
+use App\Models\Perfil;
+use App\Models\Permissao;
+use App\Models\Usuario;
+use App\Rules\NumeroProcessoCNJ;
+use App\Rules\ProcessoDisponivel;
+use Database\Seeders\PerfilSeeder;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+beforeEach(function () {
+    $this->request = new UpdatePerfilRequest();
+    $this->perfil = Perfil::factory()->create();
+
+    $this->request->perfil = $this->perfil;
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Autorização
+test('usuário sem autorização não cria o request', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    login();
+
+    expect($this->request->authorize())->toBeFalse();
+});
+
+// Caminho feliz
+test('rules estão definidas no form request', function () {
+    $this->assertExactValidationRules([
+        'nome' => [
+            'bail',
+            'required',
+            'string',
+            'between:1,50',
+            Rule::unique('perfis', 'nome')
+                ->ignore($this->perfil),
+        ],
+        'slug' => [
+            'bail',
+            'required',
+            'string',
+            'between:1,50',
+            Rule::unique('perfis', 'slug')
+                ->ignore($this->perfil),
+        ],
+        'descricao' => [
+            'bail',
+            'nullable',
+            'string',
+            'between:1,255',
+        ],
+        'permissao_id' => [
+            'bail',
+            'nullable',
+            'integer',
+            Rule::exists('permissoes', 'id'),
+        ],
+    ], $this->request->rules());
+});
+
+test('attributes estão definidas no form request', function () {
+    $this->assertExactValidationRules([
+        'nome' => __('Nome'),
+        'slug' => __('Slug'),
+        'descricao' => __('Descrição'),
+        'perfil_id' => __('Perfil'),
+    ], $this->request->attributes());
+});
+
+test('usuário autorizado pode criar o request', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    login();
+
+    concederPermissao(Permissao::PERFIL_UPDATE);
+
+    expect($this->request->authorize())->toBeTrue();
+});
