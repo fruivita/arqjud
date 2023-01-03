ae348b6b1ebd2b94b717a0d8e547079a69860572
FCNO <fcno.dev@gmail.com>
2023-01-02 23:52:08 -0300

    feat(controller): add PerfilController

---
+++ app/Http/Controllers/Administracao/PerfilController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Http\Controllers\Administracao;
+
+use App\Enums\Policy;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Administracao\StorePerfilRequest;
+use App\Http\Requests\Administracao\UpdatePerfilRequest;
+use App\Http\Resources\Perfil\PerfilCollection;
+use App\Http\Resources\Perfil\PerfilEditResource;
+use App\Http\Resources\Permissao\PermissaoCollection;
+use App\Http\Traits\ComFeedback;
+use App\Http\Traits\ComPaginacaoEmCache;
+use App\Models\Perfil;
+use App\Models\Permissao;
+use App\Pipes\Perfil\Order;
+use App\Pipes\Permissao\Order as PermissaoOrder;
+use App\Pipes\Search;
+use Inertia\Inertia;
+use MichaelRubel\EnhancedPipeline\Pipeline;
+
+/**
+ * @see https://laravel.com/docs/9.x/controllers
+ * @see https://laravel.com/docs/9.x/requests
+ * @see https://laravel.com/docs/9.x/responses
+ * @see https://inertiajs.com/
+ */
+class PerfilController extends Controller
+{
+    use ComFeedback;
+    use ComPaginacaoEmCache;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Inertia\Response
+     */
+    public function index()
+    {
+        $this->authorize(Policy::ViewAny->value, Perfil::class);
+
+        return Inertia::render('Administracao/Perfil/Index', [
+            'perfis' => fn () => PerfilCollection::make(
+                Pipeline::make()
+                    ->send(Perfil::withCount(['usuarios', 'delegados']))
+                    ->through([Order::class, Search::class])
+                    ->thenReturn()
+                    ->paginate($this->perPage())
+            )->additional(['meta' => [
+                'termo' => request()->query('termo'),
+                'order' => request()->query('order'),
+            ]])->preserveQuery(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Inertia\Response
+     */
+    public function create()
+    {
+        $this->authorize(Policy::Create->value, Perfil::class);
+
+        return Inertia::render('Administracao/Perfil/Create', [
+            'ultima_insercao' => fn () => PerfilEditResource::make(Perfil::latest()->first()),
+            'links' => fn () => ['store' => route('administracao.perfil.store')],
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\Administracao\StorePerfilRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(StorePerfilRequest $request)
+    {
+        $perfil = new Perfil();
+
+        $perfil->nome = $request->input('nome');
+        $perfil->slug = $request->input('slug');
+        $perfil->poder = $request->integer('poder');
+        $perfil->descricao = $request->input('descricao');
+
+        $salvo = $perfil->save();
+
+        return back()->with($this->feedback($salvo));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Perfil  $perfil
+     * @return \Inertia\Response
+     */
+    public function edit(Perfil $perfil)
+    {
+        $this->authorize(Policy::ViewOrUpdate->value, Perfil::class);
+
+        return Inertia::render('Administracao/Perfil/Edit', [
+            'perfil' => fn () => PerfilEditResource::make($perfil),
+            'permissoes' => fn () => PermissaoCollection::make(
+                Pipeline::make()
+                    ->send(Permissao::with(['Perfis' => fn ($query) => $query->select(['id'])->where('perfis.id', $perfil->id)]))
+                    ->through([PermissaoOrder::class])
+                    ->thenReturn()
+                    ->paginate($this->perPage())
+            )->additional(['meta' => [
+                'order' => request()->query('order'),
+            ]])->preserveQuery(),
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\Administracao\UpdatePerfilRequest  $request
+     * @param  \App\Models\Perfil  $perfil
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UpdatePerfilRequest $request, Perfil $perfil)
+    {
+        $salvo = $request->whenFilled(
+            'permissao_id',
+            fn () => $perfil->permissoes()->toggle([$request->integer('permissao_id')]),
+            function () use ($request, $perfil) {
+                $perfil->nome = $request->input('nome');
+                $perfil->slug = $request->input('slug');
+                $perfil->descricao = $request->input('descricao');
+
+                return $perfil->save();
+            }
+        );
+
+        return back()->with($this->feedback($salvo));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Perfil  $perfil
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(Perfil $perfil)
+    {
+        $this->authorize(Policy::Delete->value, $perfil);
+
+        $excluido = $perfil->delete();
+
+        return back()->with($this->feedback($excluido));
+    }
+}
---
+++ tests/Feature/App/Http/Controllers/Administracao/PerfilControllerTest.php
@@ -0,0 +1,225 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://inertiajs.com/testing
+ * @see https://hofmannsven.com/2020/testing-laravel-form-requests
+ * @see https://github.com/jasonmccreary/laravel-test-assertions
+ */
+
+use App\Http\Controllers\Administracao\PerfilController;
+use App\Http\Requests\Administracao\StorePerfilRequest;
+use App\Http\Requests\Administracao\UpdatePerfilRequest;
+use App\Http\Resources\Perfil\PerfilEditResource;
+use App\Http\Resources\Perfil\PerfilResource;
+use App\Models\Perfil;
+use App\Models\Permissao;
+use Database\Seeders\PerfilSeeder;
+use Inertia\Testing\AssertableInertia as Assert;
+use function Pest\Laravel\delete;
+use function Pest\Laravel\get;
+use function Pest\Laravel\patch;
+use function Pest\Laravel\post;
+use function Spatie\PestPluginTestTime\testTime;
+
+beforeEach(function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $this->dados = [
+        'nome' => 'Loren Ipsun',
+        'slug' => 'foo', // será ignorado pois o slug é gerado com base no nome
+        'poder' => 2000, // será ignorado na atualização
+        'descricao' => 'foo bar',
+    ];
+
+    login();
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Autorização
+test('usuário sem permissão não consegue excluir um perfil', function () {
+    $id_perfil = Perfil::factory()->create()->id;
+
+    expect(Perfil::where('id', $id_perfil)->exists())->toBeTrue();
+
+    delete(route('administracao.perfil.destroy', $id_perfil))->assertForbidden();
+
+    expect(Perfil::where('id', $id_perfil)->exists())->toBeTrue();
+});
+
+test('usuário sem permissão não consegue exibir formulário de criação do perfil', function () {
+    get(route('administracao.perfil.create'))->assertForbidden();
+});
+
+test('usuário sem permissão não consegue exibir formulário de edição do perfil', function () {
+    get(route('administracao.perfil.edit', Perfil::factory()->create()))->assertForbidden();
+});
+
+// Caminho feliz
+test('action do controller usa o form request', function (string $action, string $request) {
+    $this->assertActionUsesFormRequest(
+        PerfilController::class,
+        $action,
+        $request
+    );
+})->with([
+    ['store', StorePerfilRequest::class],
+    ['update', UpdatePerfilRequest::class],
+]);
+
+test('action index compartilha os dados esperados com a view/componente correto', function () {
+    concederPermissao(Permissao::PERFIL_VIEW_ANY);
+
+    get(route('administracao.perfil.index'))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Administracao/Perfil/Index')
+                ->has('perfis.data', 5)
+                ->has('perfis.meta.termo')
+                ->has('perfis.meta.order')
+        );
+});
+
+test('action create compartilha os dados esperados com a view/componente correto', function () {
+    Perfil::factory()->create();
+
+    $this->travel(1)->seconds();
+
+    $ultimo_perfil_criado = Perfil::factory()->create();
+
+    concederPermissao(Permissao::PERFIL_CREATE);
+
+    get(route('administracao.perfil.create'))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Administracao/Perfil/Create')
+                ->whereAll([
+                    'ultima_insercao.data' => PerfilEditResource::make($ultimo_perfil_criado)->resolve(),
+                    'links' => ['store' => route('administracao.perfil.store')],
+                ])
+        );
+});
+
+test('cria um novo perfil com o slug baseado no nome', function () {
+    concederPermissao(Permissao::PERFIL_CREATE);
+
+    testTime()->freeze();
+
+    expect(Perfil::count())->toBe(5);
+
+    testTime()->addMinute();
+
+    post(route('administracao.perfil.store', $this->dados))
+        ->assertRedirect()
+        ->assertSessionHas('feedback.sucesso');
+
+    expect(Perfil::count())->toBe(6)
+        ->and(Perfil::latest()->first()->only(array_keys($this->dados)))
+        ->toMatchArray(['slug' => 'loren-ipsun'] + $this->dados);
+});
+
+test('action edit compartilha os dados esperados com a view/componente correto', function () {
+    concederPermissao(Permissao::PERFIL_UPDATE);
+
+    $perfil = Perfil::factory()->hasPermissoes(3)->create();
+
+    get(route('administracao.perfil.edit', $perfil))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Administracao/Perfil/Edit')
+                ->where('perfil.data', PerfilEditResource::make($perfil)->resolve())
+                ->has('permissoes.data', 3 + 1) //todos os perfis serão retornados
+                ->has('permissoes.meta.order')
+        );
+});
+
+test('action edit também é executável com permissão de visualização', function () {
+    concederPermissao(Permissao::PERFIL_VIEW);
+
+    $perfil = Perfil::factory()->create();
+
+    get(route('administracao.perfil.edit', $perfil))
+        ->assertOk()
+        ->assertInertia(
+            fn (Assert $page) => $page->component('Administracao/Perfil/Edit')
+        );
+});
+
+test('atualiza um perfil', function () {
+    concederPermissao(Permissao::PERFIL_UPDATE);
+
+    $perfil_antes = Perfil::factory()->create();
+
+    patch(route('administracao.perfil.update', $perfil_antes), $this->dados)
+        ->assertRedirect()
+        ->assertSessionHas('feedback.sucesso');
+
+    $perfil_depois = Perfil::find($perfil_antes->id);
+
+    expect($perfil_depois->nome)->toBe($this->dados['nome'])
+        ->and($perfil_depois->slug)->toBe('loren-ipsun')
+        ->and($perfil_depois->poder)->toBe($perfil_antes->poder)
+        ->and($perfil_depois->descricao)->toBe($this->dados['descricao']);
+});
+
+test('faz o toggle da permissão se ela for informada sem alterar os demais atributos do perfil', function () {
+    concederPermissao(Permissao::PERFIL_UPDATE);
+    $permissao = Permissao::factory()->create();
+    $perfil_antes = Perfil::factory()->create();
+
+    // adiciona a permissão (toggle)
+    patch(route('administracao.perfil.update', $perfil_antes), $this->dados + ['permissao_id' => $permissao->id])
+        ->assertRedirect()
+        ->assertSessionHas('feedback.sucesso');
+
+    $perfil_depois = Perfil::with('Permissoes')->find($perfil_antes->id);
+
+    expect($perfil_depois->nome)->toBe($perfil_antes->nome)
+        ->and($perfil_depois->slug)->toBe($perfil_antes->slug)
+        ->and($perfil_depois->poder)->toBe($perfil_antes->poder)
+        ->and($perfil_depois->descricao)->toBe($perfil_antes->descricao)
+        ->and($perfil_depois->permissoes->first()->id)->toBe($permissao->id);
+
+    // remove a permissão (toggle)
+    patch(route('administracao.perfil.update', $perfil_antes), $this->dados + ['permissao_id' => $permissao->id])
+        ->assertRedirect()
+        ->assertSessionHas('feedback.sucesso');
+
+    $perfil_depois = Perfil::with('Permissoes')->find($perfil_antes->id);
+
+    expect($perfil_depois->nome)->toBe($perfil_antes->nome)
+        ->and($perfil_depois->slug)->toBe($perfil_antes->slug)
+        ->and($perfil_depois->poder)->toBe($perfil_antes->poder)
+        ->and($perfil_depois->descricao)->toBe($perfil_antes->descricao)
+        ->and($perfil_depois->permissoes)->toBeEmpty();
+});
+
+test('exclui o perfil informado', function () {
+    $id_perfil = Perfil::factory()->create()->id;
+
+    concederPermissao(Permissao::PERFIL_DELETE);
+
+    expect(Perfil::where('id', $id_perfil)->exists())->toBeTrue();
+
+    delete(route('administracao.perfil.destroy', $id_perfil))
+        ->assertRedirect()
+        ->assertSessionHas('feedback.sucesso');
+
+    expect(Perfil::where('id', $id_perfil)->exists())->toBeFalse();
+});
+
+test('PerfilController usa trait', function () {
+    expect(
+        collect(class_uses(PerfilController::class))
+            ->has([
+                \App\Http\Traits\ComPaginacaoEmCache::class,
+                \App\Http\Traits\ComFeedback::class,
+            ])
+    )->toBeTrue();
+});
