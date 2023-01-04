d8baee7179463918022a93ccf9baa51c37b6ec70
FCNO <fcno.dev@gmail.com>
2023-01-04 13:03:41 -0300

    feat(core): add pipe ExcluirSolicitacao

---
+++ app/Pipes/Solicitacao/ExcluirSolicitacao.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Pipes\Solicitacao;
+
+/**
+ * @see https://www.youtube.com/watch?v=FByQN_d876c
+ */
+class ExcluirSolicitacao
+{
+    /**
+     * Exclui por pipe uma solicitação
+     *
+     * @param  \stdClass  $solicitacao
+     * @param  \Closure  $next
+     * @return \stdClass
+     */
+    public function handle(\stdClass $solicitacao, \Closure $next)
+    {
+        $solicitacao->processo = $solicitacao->model->processo->numero;
+        $solicitacao->solicitante = $solicitacao->model->solicitante;
+        $solicitacao->destino = $solicitacao->model->lotacaoDestinataria;
+        $solicitacao->solicitada_em = $solicitacao->model->solicitada_em;
+        $solicitacao->operador = auth()->user();
+        $solicitacao->cancelada_em = now();
+
+        $solicitacao->model->delete();
+
+        return $next($solicitacao);
+    }
+}
---
+++ tests/Feature/App/Pipes/Solicitacao/ExcluirSolicitacaoTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Models\Guia;
+use App\Models\Solicitacao;
+use App\Models\Usuario;
+use App\Pipes\Solicitacao\ExcluirSolicitacao;
+use Database\Seeders\PerfilSeeder;
+use MichaelRubel\EnhancedPipeline\Pipeline;
+use function Spatie\PestPluginTestTime\testTime;
+
+// Caminho feliz
+test('pipe ExcluirSolicitacao exclui a solicitação informada mas mantém suas propriedades para uso no próximo pipe', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    login();
+    testTime()->freeze();
+
+    $solicitacao = Solicitacao::factory()->solicitada()->create();
+
+    $std = new \stdClass();
+    $std->model = $solicitacao;
+
+    expect(Solicitacao::count())->toBe(1);
+
+    $new_std = Pipeline::make()
+        ->withTransaction()
+        ->send($std)
+        ->through([ExcluirSolicitacao::class])
+        ->thenReturn();
+
+    expect(Solicitacao::count())->toBe(0)
+        ->and($new_std->processo)->toBe($solicitacao->processo->numero)
+        ->and($new_std->solicitante->is($solicitacao->solicitante))->toBeTrue()
+        ->and($new_std->destino->is($solicitacao->lotacaoDestinataria))->toBeTrue()
+        ->and($new_std->solicitada_em->is($solicitacao->solicitada_em))->toBeTrue()
+        ->and($new_std->operador->is(auth()->user()))->toBeTrue()
+        ->and($new_std->cancelada_em->is(now()))->toBeTrue();
+});
