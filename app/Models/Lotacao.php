a7db6e902f474e252da3b2d9729719fdf319e9be
FCNO <fcno.dev@gmail.com>
2023-01-04 23:26:59 -0300

    feat(model): add escopo search na lotação

--- app/Models/Lotacao.php
+++ app/Models/Lotacao.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use FruiVita\Corporativo\Models\Lotacao as LotacaoCorporativo;
+use Illuminate\Database\Eloquent\Builder;
 
 /**
  * @see https://laravel.com/docs/9.x/eloquent
@@ -26,5 +27,25 @@ class Lotacao extends LotacaoCorporativo
     public function solicitacoes()
     {
         return $this->hasMany(Solicitacao::class, 'lotacao_destinataria_id', 'id');
+    }
+
+    /**
+     * Pesquisa utilizando o termo informado com o operador like no seguinte
+     * formato: `termo%`
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string|null  $termo
+     * @return void
+     */
+    public function scopeSearch(Builder $query, string $termo = null)
+    {
+        $termo = "{$termo}%";
+
+        $query->where(function (Builder $query) use ($termo) {
+            $query->where('lotacoes.sigla', 'like', $termo)
+                ->orWhere('lotacoes.nome', 'like', $termo)
+                ->orWhere('lotacoes_pai.sigla', 'like', $termo)
+                ->orWhere('lotacoes_pai.nome', 'like', $termo);
+        });
     }
 }
--- tests/Feature/App/Models/LotacaoTest.php
+++ tests/Feature/App/Models/LotacaoTest.php
@@ -5,7 +5,9 @@
  */
 
 use App\Models\Lotacao;
+use App\Pipes\Lotacao\JoinAll;
 use Illuminate\Database\QueryException;
+use MichaelRubel\EnhancedPipeline\Pipeline;
 
 // Exceptions
 test('lança exception ao tentar criar solicitação com campo inválido', function (string $campo, mixed $valor, string $mensagem) {
@@ -25,3 +27,37 @@ test('uma lotação pode ser destinatária de várias solicitações de processo
 
     expect($lotacao->solicitacoes)->toHaveCount(3);
 });
+
+test('retorna as lotações pelo escopo search que busca a partir do início do texto na sigla ou nome da lotação', function (string $termo, int $quantidade) {
+    Lotacao::factory()->create(['sigla' => 'aaaabbbb', 'nome' => 'ggggffff']);
+    Lotacao::factory()->create(['sigla' => 'ccccdddd', 'nome' => 'gggggggg']);
+    Lotacao::factory()->create(['sigla' => 'cccccccc', 'nome' => 'aaaajjjj']);
+    Lotacao::factory()->create(['sigla' => 'ggggdddd', 'nome' => 'hhhhhhhh']);
+
+    $query = Pipeline::make()
+        ->send(Lotacao::query())
+        ->through([JoinAll::class])
+        ->thenReturn();
+
+    expect($query->search($termo)->count())->toBe($quantidade);
+})->with([
+    ['', 4],
+    ['aaaa', 2],
+    ['gggg', 3],
+]);
+
+test('retorna as lotações pelo escopo search que busca a partir do início do texto na sigla ou nome da lotação pai', function (string $termo, int $quantidade) {
+    Lotacao::factory(2)->for(Lotacao::factory(['sigla' => 'aaaabbbb', 'nome' => 'eeeeffff']), 'lotacaoPai')->create();
+    Lotacao::factory(3)->for(Lotacao::factory(['sigla' => 'ccccdddd', 'nome' => 'gggghhhh']), 'lotacaoPai')->create();
+
+    $query = Pipeline::make()
+        ->send(Lotacao::query())
+        ->through([JoinAll::class])
+        ->thenReturn();
+
+    expect($query->search($termo)->count())->toBe($quantidade);
+})->with([
+    ['', 7],
+    ['aaaa', 2 + 1],
+    ['gggg', 3 + 1],
+]);
