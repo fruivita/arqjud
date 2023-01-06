ba43eb8cc334689ded3b5159a4dc09b68f6ab1d2
FCNO <fcno.dev@gmail.com>
2023-01-05 23:07:56 -0300

    test: corrige testes face a lotação administrável

--- tests/Feature/App/Pipes/Lotacao/JoinAllTest.php
+++ tests/Feature/App/Pipes/Lotacao/JoinAllTest.php
@@ -10,14 +10,16 @@ use MichaelRubel\EnhancedPipeline\Pipeline;
 
 // Caminho feliz
 test('join da tabela lotacoes_pai', function () {
+    \Spatie\Once\Cache::getInstance()->disable();
+
     $lotacao_pai = Lotacao::factory()->create();
-    $lotacao = Lotacao::factory()->for($lotacao_pai, 'lotacaoPai')->create();
+    $lotacao = Lotacao::factory()->create(['lotacao_pai' => $lotacao_pai->id]);
 
     $lotacao = Pipeline::make()
         ->send(Lotacao::query())
         ->through([JoinAll::class])
         ->thenReturn()
-        ->pluck('lotacoes_pai.nome');
+        ->pluck('lotacoes_pai.sigla');
 
-    expect($lotacao->first())->toBe($lotacao_pai->nome);
+    expect($lotacao->first())->toBe($lotacao_pai->sigla);
 });
