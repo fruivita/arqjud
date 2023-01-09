fb83a25c6ec3844325a5694b90d506c3f27900b5
FCNO <fcno.dev@gmail.com>
2023-01-09 00:24:18 -0300

    docs(auth): add php doc block

--- app/Ldap/PerfilAttributeHandler.php
+++ app/Ldap/PerfilAttributeHandler.php
@@ -6,6 +6,9 @@ use App\Models\Perfil;
 use App\Models\Usuario;
 use LdapRecord\Models\ActiveDirectory\User as LdapUser;
 
+/**
+ * @see https://ldaprecord.com/docs/laravel/v2/auth/database/configuration/#attribute-handlers
+ */
 class PerfilAttributeHandler
 {
     /**
