5b0ea9a25b0c719373f854882263f4889b5401dc
FCNO <fcno.dev@gmail.com>
2023-01-10 22:44:57 -0300

    feat(config): add configurações de importação

---
+++ config/corporativo.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    /*
+    | ------------------------------------------------------------------------
+    | Max Upsert
+    | ------------------------------------------------------------------------
+    |
+    | Número de registros que serão persistidos no banco de dados por query.
+    |
+    | Se menor ou igual a zero, utilizará o valor padrão internamente definido.
+    |
+    */
+    'max_upsert' => 500,
+
+    /*
+    | ------------------------------------------------------------------------
+    | Padrão matrícula
+    | ------------------------------------------------------------------------
+    |
+    | Valor a ser, eventualmente, adicionado ao início da matrícula caso.
+    |
+    | necessário. Ex.: ES99999
+    |
+    */
+    'matricula' => 'ES',
+
+    /*
+    | ------------------------------------------------------------------------
+    | Emitir eventos
+    | ------------------------------------------------------------------------
+    |
+    | Se o processo de importação deve emitir os eventos de início e fim da
+    | importação e de alteração da lotação, função ou cargo do usuário.
+    |
+    */
+    'eventos' => true,
+];
