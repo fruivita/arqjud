8f541d47c1d1e48a0997c37c01ccbb2857d9f8b9
FCNO <fcno.dev@gmail.com>
2023-01-11 15:08:01 -0300

    feat(event): add ImportacaoDadosRHEventSubscriber

---
+++ app/Listeners/ImportacaoDadosRHEventSubscriber.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Enums\Queue;
+use App\Models\Usuario;
+use FruiVita\Corporativo\Events\CargoUsuarioAlterado;
+use FruiVita\Corporativo\Events\FuncaoConfiancaUsuarioAlterada;
+use FruiVita\Corporativo\Events\ImportacaoConcluida;
+use FruiVita\Corporativo\Events\ImportacaoIniciada;
+use FruiVita\Corporativo\Events\LotacaoUsuarioAlterada;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Log;
+
+/**
+ * @link https://laravel.com/docs/9.x/events
+ */
+class ImportacaoDadosRHEventSubscriber implements ShouldQueue
+{
+    /**
+     * Número de tentativas de execução do job.
+     *
+     * @var int
+     */
+    public $tries = 5;
+
+    /**
+     * Número de segundos de espera antes de tentar executar novamente o job.
+     *
+     * @var int[]
+     */
+    public $backoff = [60, 300];
+
+    /**
+     * Handle ImportacaoIniciada events.
+     *
+     * @param \FruiVita\Corporativo\Events\ImportacaoIniciada $event
+     * @return void
+     */
+    public function handleImportacaoIniciada(ImportacaoIniciada $event)
+    {
+        Log::notice(
+            __('Importação dos dados corporativos iniciada'),
+            [
+                'iniciado_em' => $event->iniciado_em->tz(config('app.tz'))->format('d-m-Y H:i:s'),
+                'arquivo' => $event->arquivo,
+            ]
+        );
+    }
+
+    /**
+     * Handle ImportacaoConcluida events.
+     *
+     * @param \FruiVita\Corporativo\Events\ImportacaoConcluida $event
+     * @return void
+     */
+    public function handleImportacaoConcluida(ImportacaoConcluida $event)
+    {
+        Log::notice(
+            __('Importação dos dados corporativos concluída'),
+            [
+                'concluido_em' => $event->concluido_em->tz(config('app.tz'))->format('d-m-Y H:i:s'),
+                'arquivo' => $event->arquivo,
+            ]
+        );
+    }
+
+    /**
+     * Handle CargoUsuarioAlterado events.
+     *
+     * @param \FruiVita\Corporativo\Events\CargoUsuarioAlterado $event
+     * @return void
+     */
+    public function handleCargoUsuarioAlterado(CargoUsuarioAlterado $event)
+    {
+        Usuario::resetarPerfil($event->usuario);
+    }
+
+    /**
+     * Handle FuncaoConfiancaUsuarioAlterada events.
+     *
+     * @param \FruiVita\Corporativo\Events\FuncaoConfiancaUsuarioAlterada $event
+     * @return void
+     */
+    public function handleFuncaoConfiancaUsuarioAlterada(FuncaoConfiancaUsuarioAlterada $event)
+    {
+        Usuario::resetarPerfil($event->usuario);
+    }
+
+    /**
+     * Handle LotacaoUsuarioAlterada events.
+     *
+     * @param \FruiVita\Corporativo\Events\LotacaoUsuarioAlterada $event
+     * @return void
+     */
+    public function handleLotacaoUsuarioAlterada(LotacaoUsuarioAlterada $event)
+    {
+        Usuario::resetarPerfil($event->usuario);
+    }
+
+    /**
+     * Register the listeners for the subscriber.
+     *
+     * @param  \Illuminate\Events\Dispatcher  $events
+     * @return array
+     */
+    public function subscribe($events)
+    {
+        return [
+            ImportacaoIniciada::class => 'handleImportacaoIniciada',
+            ImportacaoConcluida::class => 'handleImportacaoConcluida',
+            CargoUsuarioAlterado::class => 'handleCargoUsuarioAlterado',
+            FuncaoConfiancaUsuarioAlterada::class => 'handleFuncaoConfiancaUsuarioAlterada',
+            LotacaoUsuarioAlterada::class => 'handleLotacaoUsuarioAlterada',
+        ];
+    }
+
+    /**
+     * Get the name of the listener's queue.
+     *
+     * @return string
+     */
+    public function viaQueue()
+    {
+        return Queue::Baixa->value;
+    }
+}
---
+++ tests/Feature/App/Listeners/ImportacaoDadosRHEventSubscriberTest.php
@@ -0,0 +1,168 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ * @see https://inertiajs.com/testing
+ * @see https://hofmannsven.com/2020/testing-laravel-form-requests
+ * @see https://github.com/jasonmccreary/laravel-test-assertions
+ */
+
+use App\Enums\Queue as EQueue;
+use App\Jobs\ImportarDadosRH;
+use App\Listeners\ImportacaoDadosRHEventSubscriber;
+use App\Models\Cargo;
+use App\Models\FuncaoConfianca;
+use App\Models\Lotacao;
+use App\Models\Perfil;
+use App\Models\Usuario;
+use Database\Seeders\PerfilSeeder;
+use FruiVita\Corporativo\Events\ImportacaoIniciada;
+use Illuminate\Events\CallQueuedListener;
+use Illuminate\Notifications\SendQueuedNotifications;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Queue;
+
+use function Spatie\PestPluginTestTime\testTime;
+
+beforeAll(fn () => \Spatie\Once\Cache::getInstance()->disable());
+
+// Caminho feliz
+test('registra o log de início e fim da importação', function () {
+    testTime()->freeze();
+
+    Log::spy();
+
+    ImportarDadosRH::dispatchSync();
+
+    Log::shouldHaveReceived('notice')
+        ->withArgs(function ($message, $data) {
+            return $message === __('Importação dos dados corporativos iniciada')
+                && $data === [
+                    'iniciado_em' => now()->tz(config('app.tz'))->format('d-m-Y H:i:s'),
+                    'arquivo' => 'tests/template/Corporativo.xml',
+                ];
+        })->once();
+
+    Log::shouldHaveReceived('notice')
+        ->withArgs(function ($message, $data) {
+            return $message === __('Importação dos dados corporativos concluída')
+                && $data === [
+                    'concluido_em' => now()->tz(config('app.tz'))->format('d-m-Y H:i:s'),
+                    'arquivo' => 'tests/template/Corporativo.xml',
+                ];
+        })->once();
+});
+
+test('reseta o perfil do usuário ao mudar seu cargo', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $usuario = Usuario::factory()->for(Cargo::factory(['id' => 1000]), 'cargo')->create(['matricula' => 'ES44444']);
+
+    expect($usuario->perfil_id)->not->toBe(Perfil::padrao()->id);
+
+    ImportarDadosRH::dispatchSync();
+
+    $usuario->refresh();
+
+    expect($usuario->perfil_id)->toBe(Perfil::padrao()->id)
+        ->and($usuario->cargo_id)->not->toBe(1000);
+});
+
+test('reseta o perfil do usuário ao mudar sua lotação', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $usuario = Usuario::factory()->for(Lotacao::factory(['id' => 1000]), 'lotacao')->create(['matricula' => 'ES44444']);
+
+    expect($usuario->perfil_id)->not->toBe(Perfil::padrao()->id);
+
+    ImportarDadosRH::dispatchSync();
+
+    $usuario->refresh();
+
+    expect($usuario->perfil_id)->toBe(Perfil::padrao()->id)
+        ->and($usuario->lotacao_id)->not->toBe(1000);
+});
+
+test('reseta o perfil do usuário ao mudar sua função de confiança', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $usuario = Usuario::factory()->for(FuncaoConfianca::factory(['id' => 1000]), 'funcaoConfianca')->create(['matricula' => 'ES44444']);
+
+    expect($usuario->perfil_id)->not->toBe(Perfil::padrao()->id);
+
+    ImportarDadosRH::dispatchSync();
+
+    $usuario->refresh();
+
+    expect($usuario->perfil_id)->toBe(Perfil::padrao()->id)
+        ->and($usuario->funcao_confianca_id)->toBeEmpty();
+});
+
+test('usuário com perfil administrador não é resetado, mesmo havendo mudança de cargo, lotação ou função', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    $usuario = Usuario::factory()
+        ->for(Cargo::factory(['id' => 1000]), 'cargo')
+        ->for(Lotacao::factory(['id' => 1000]), 'lotacao')
+        ->for(FuncaoConfianca::factory(['id' => 1000]), 'funcaoConfianca')
+        ->for(Perfil::administrador(), 'perfil')
+        ->create(['matricula' => 'ES44444']);
+
+    expect($usuario->perfil_id)->toBe(Perfil::administrador()->id);
+
+    ImportarDadosRH::dispatchSync();
+
+    $usuario->refresh();
+
+    expect($usuario->perfil_id)->toBe(Perfil::administrador()->id)
+        ->and($usuario->cargo_id)->not->toBe(1000)
+        ->and($usuario->lotacao_id)->not->toBe(1000)
+        ->and($usuario->funcao_confianca_id)->toBeEmpty();
+});
+
+test('envia a tratativa dos eventos para a queue', function () {
+    $this->seed([PerfilSeeder::class]);
+
+    Queue::fake()->except([
+        ImportarDadosRH::class,
+    ]);
+
+    Usuario::factory()
+        ->for(Cargo::factory(['id' => 1000]), 'cargo')
+        ->for(Lotacao::factory(['id' => 1000]), 'lotacao')
+        ->for(FuncaoConfianca::factory(['id' => 1000]), 'funcaoConfianca')
+        ->for(Perfil::administrador(), 'perfil')
+        ->create(['matricula' => 'ES44444']);
+
+    ImportarDadosRH::dispatchSync();
+
+    Queue::assertPushedOn(
+        EQueue::Baixa->value,
+        CallQueuedListener::class,
+        fn (CallQueuedListener $listener) => $listener->class === ImportacaoDadosRHEventSubscriber::class && $listener->method === 'handleImportacaoIniciada'
+    );
+
+    Queue::assertPushedOn(
+        EQueue::Baixa->value,
+        CallQueuedListener::class,
+        fn (CallQueuedListener $listener) => $listener->class === ImportacaoDadosRHEventSubscriber::class && $listener->method === 'handleImportacaoConcluida'
+    );
+
+    Queue::assertPushedOn(
+        EQueue::Baixa->value,
+        CallQueuedListener::class,
+        fn (CallQueuedListener $listener) => $listener->class === ImportacaoDadosRHEventSubscriber::class && $listener->method === 'handleCargoUsuarioAlterado'
+    );
+
+    Queue::assertPushedOn(
+        EQueue::Baixa->value,
+        CallQueuedListener::class,
+        fn (CallQueuedListener $listener) => $listener->class === ImportacaoDadosRHEventSubscriber::class && $listener->method === 'handleFuncaoConfiancaUsuarioAlterada'
+    );
+
+    Queue::assertPushedOn(
+        EQueue::Baixa->value,
+        CallQueuedListener::class,
+        fn (CallQueuedListener $listener) => $listener->class === ImportacaoDadosRHEventSubscriber::class && $listener->method === 'handleLotacaoUsuarioAlterada'
+    );
+});
